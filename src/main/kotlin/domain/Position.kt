3264e989c34a354bad560bf2bc2ad7860dae1c62
lowoon <sangbo0229@gmail.com>
2020-06-01 17:52:07 +0900

    refactor : 불필요한 코드 제거
    - InputView 중복 코드 메서드 추출
    - lambda 구문 수정

--- src/main/kotlin/domain/Position.kt
+++ src/main/kotlin/domain/Position.kt
@@ -5,7 +5,7 @@ private const val INCREASE_VALUE = 1
 
 data class Position(private val position: Int = MIN_VALUE) {
     init {
-        require(position >= MIN_VALUE, { "음수는 허용되지 않습니다." })
+        require(position >= MIN_VALUE) { "음수는 허용되지 않습니다." }
     }
 
     fun move() = Position(position.plus(INCREASE_VALUE))
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,15 +5,17 @@ private const val MIN_COUNT = 1
 object InputView {
     fun receiveCountOfCar(): Int {
         println("자동차 대수는 몇 대 인가요?")
-        val count = readLine()!!.toInt()
-        require(count >= MIN_COUNT, { "자동차의 대수는 1대 이상이어야 합니다." })
-        return count
+        return receiveCount("자동차의 대수는 1대 이상이어야 합니다.")
     }
 
     fun receiveCountOfTry(): Int {
         println("시도할 회수는 몇 회 인가요?")
+        return receiveCount("시도할 회수는 1회 이상이어야 합니다.");
+    }
+
+    private fun receiveCount(message: String): Int {
         val count = readLine()!!.toInt()
-        require(count >= MIN_COUNT, { "시도할 회수는 1회 이상이어야 합니다." })
+        require(count >= MIN_COUNT) { message }
         return count
     }
 }
