58a85ae90a0d3458ae933f005cfad531e49bf8e0
lalize <printf@kakao.com>
2020-06-01 17:27:10 +0900

    feat : 사용자 입력 유효성 검사 추가

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,13 +1,19 @@
 package view
 
+private const val MIN_COUNT = 1
+
 object InputView {
     fun receiveCountOfCar(): Int {
         println("자동차 대수는 몇 대 인가요?")
-        return readLine()!!.toInt()
+        val count = readLine()!!.toInt()
+        require(count >= MIN_COUNT, { "자동차의 대수는 1대 이상이어야 합니다." })
+        return count
     }
 
     fun receiveCountOfTry(): Int {
         println("시도할 회수는 몇 회 인가요?")
-        return readLine()!!.toInt()
+        val count = readLine()!!.toInt()
+        require(count >= MIN_COUNT, { "시도할 회수는 1회 이상이어야 합니다." })
+        return count
     }
 }
