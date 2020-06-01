c0525cece73ac774c80d695d763cc952d8825896
lowoon <sangbo0229@gmail.com>
2020-06-01 17:52:17 +0900

    refactor : Car의 이동 테스트 케이스 추가

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -3,6 +3,9 @@ package domain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class CarTest {
     @Test
@@ -11,13 +14,22 @@ internal class CarTest {
         assertThat(Car(Position()) { true }).isNotNull
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("자동차 이동")
-    internal fun move() {
-        val car = Car(Position()) { true }
+    @MethodSource("createStrategyAndPosition")
+    fun move(condition: Boolean, value: Int) {
+        val car = Car(Position()) { condition }
         val startPosition = car.position()
         car.move()
 
-        assertThat(car.position()).isEqualTo(startPosition + 1)
+        assertThat(car.position()).isEqualTo(startPosition + value)
+    }
+
+    companion object {
+        @JvmStatic
+        fun createStrategyAndPosition() = listOf(
+                Arguments.of(true, 1),
+                Arguments.of(false, 0)
+        )
     }
 }
