3ef1abb26ddaa8d2675162954935d4c6704a636f
lalize <printf@kakao.com>
2020-06-01 17:30:15 +0900

    refactor : Cars 일급 컬렉션 리팩토링
    - java.util.* 의존성 제거

--- src/main/kotlin/domain/Cars.kt
+++ src/main/kotlin/domain/Cars.kt
@@ -1,10 +1,7 @@
 package domain
 
-import java.util.*
-import kotlin.collections.ArrayList
-
-class Cars(cars: List<Car> = Collections.emptyList()) {
-    private val cars: List<Car> = Collections.unmodifiableList(ArrayList(cars))
+class Cars(cars: List<Car> = emptyList()) {
+    private val cars: List<Car> = listOf(*cars.toTypedArray())
 
     fun move() {
         for (car in cars) {
--- src/test/kotlin/domain/CarsTest.kt
+++ src/test/kotlin/domain/CarsTest.kt
@@ -1,6 +1,7 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.util.Lists
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -24,5 +25,19 @@ internal class CarsTest {
         assertThat(car1.position()).isEqualTo(1)
         assertThat(car2.position()).isEqualTo(1)
         assertThat(car3.position()).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("일급 컬렉션 불변 테스트")
+    internal fun immutable() {
+        val car1 = Car(Position()) { true }
+        val car2 = Car(Position()) { true }
+        val car3 = Car(Position()) { true }
+        val init = Lists.newArrayList(car1, car2, car3)
+        val cars = Cars(init)
+
+        init.remove(car1)
+
+        assertThat(cars.values().contains(car1)).isTrue()
     }
 }
