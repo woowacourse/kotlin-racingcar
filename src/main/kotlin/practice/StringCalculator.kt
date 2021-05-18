0fe3d3e5abdc127889850baa358d7f3fa47162f7
sehwan-jang <whilter08@gmail.com>
2021-05-18 16:51:27 +0900

    feat : 잘못된 수식 입력 시 예외처리 기능 구현 및 테스트 작성

--- src/main/kotlin/practice/StringCalculator.kt
+++ src/main/kotlin/practice/StringCalculator.kt
@@ -1,20 +1,38 @@
 package practice
 
-import java.lang.IllegalArgumentException
+import java.util.*
 import java.util.regex.Pattern
 
 class StringCalculator {
     private val pattern: Pattern = Pattern.compile("^[0-9+-/* ]*$")
 
-    fun calculate(input: String): Int  {
-        val expression = input.trim()
-        validateInput(expression)
-        return 0
+    fun calculate(input: String): Int {
+        validateInput(input)
+
+        val elements = input.split(" ".toRegex()).toTypedArray()
+        val expressionElements: Queue<String> = LinkedList(listOf(*elements))
+        return calculateByQueue(expressionElements)
     }
 
     private fun validateInput(expression: String) {
         if (expression.isEmpty() || !pattern.matcher(expression).matches()) {
             throw IllegalArgumentException("올바르지 않은 입력값입니다.")
+        }
+    }
+
+    private fun calculateByQueue(expressionElements: Queue<String>): Int {
+        try {
+            var result = expressionElements.poll()
+                .toInt()
+            while (expressionElements.isNotEmpty()) {
+                val operator = expressionElements.poll()
+                val nextValue = expressionElements.poll()
+                    .toInt()
+                // calculate
+            }
+            return result
+        } catch (e: Exception) {
+            throw IllegalArgumentException("잘못된 수식입니다.")
         }
     }
 }
--- src/test/kotlin/practice/StringCalculatorTest.kt
+++ src/test/kotlin/practice/StringCalculatorTest.kt
@@ -2,18 +2,29 @@ package practice
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 internal class StringCalculatorTest {
 
+    private val calculator = StringCalculator()
+
     @DisplayName("잘못된 입력값 예외처리")
     @ParameterizedTest
     @ValueSource(strings = ["", "  ", "1 : 3", "1 + a"])
-    internal fun calculate(input: String?) {
-        val calculator = StringCalculator()
+    internal fun validateInput(input: String?) {
         Assertions.assertThatThrownBy { calculator.calculate(input!!) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("올바르지 않은 입력값입니다.")
+    }
+
+    @DisplayName("잘못된 수식인 경우 예외처리")
+    @ParameterizedTest
+    @ValueSource(strings = ["1 2", "+", "1 +", "+ 1"])
+    internal fun validateExpression(input: String?) {
+        Assertions.assertThatThrownBy { calculator.calculate(input!!) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("잘못된 수식입니다.")
     }
 }
