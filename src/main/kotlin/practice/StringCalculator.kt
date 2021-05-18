f56cbc5063b8a9af2316a34345b444571b6fa584
sehwan-jang <whilter08@gmail.com>
2021-05-18 17:55:36 +0900

    feat : 계산 기능 구현 및 테스트 작성

--- src/main/kotlin/practice/StringCalculator.kt
+++ src/main/kotlin/practice/StringCalculator.kt
@@ -1,38 +1,63 @@
 package practice
 
 import java.util.*
+import java.util.function.IntBinaryOperator
 import java.util.regex.Pattern
+import kotlin.IllegalArgumentException as IllegalArgumentException1
 
 class StringCalculator {
     private val pattern: Pattern = Pattern.compile("^[0-9+-/* ]*$")
 
     fun calculate(input: String): Int {
-        validateInput(input)
+        validateInput(input.trim())
 
         val elements = input.split(" ".toRegex()).toTypedArray()
         val expressionElements: Queue<String> = LinkedList(listOf(*elements))
-        return calculateByQueue(expressionElements)
+        return validateExpressionAndCalculate(expressionElements)
     }
 
     private fun validateInput(expression: String) {
         if (expression.isEmpty() || !pattern.matcher(expression).matches()) {
-            throw IllegalArgumentException("올바르지 않은 입력값입니다.")
+            throw IllegalArgumentException1("올바르지 않은 입력값입니다.")
         }
     }
 
-    private fun calculateByQueue(expressionElements: Queue<String>): Int {
+    private fun validateExpressionAndCalculate(expressionElements: Queue<String>): Int {
         try {
-            var result = expressionElements.poll()
-                .toInt()
-            while (expressionElements.isNotEmpty()) {
-                val operator = expressionElements.poll()
-                val nextValue = expressionElements.poll()
-                    .toInt()
-                // calculate
-            }
-            return result
+            return calculateBySequence(expressionElements)
         } catch (e: Exception) {
-            throw IllegalArgumentException("잘못된 수식입니다.")
+            throw IllegalArgumentException1("잘못된 수식입니다.")
+        }
+    }
+
+    private fun calculateBySequence(expressionElements: Queue<String>): Int {
+        var result = expressionElements.poll()
+            .toInt()
+        while (expressionElements.isNotEmpty()) {
+            val operator = expressionElements.poll()
+            val nextValue = expressionElements.poll()
+                .toInt()
+            result = Operator.calculate(result, nextValue, operator).orElseThrow()
+        }
+        return result
+    }
+}
+
+enum class Operator(
+    private val signature: String,
+    private val function: IntBinaryOperator
+) {
+    PLUS("+", IntBinaryOperator { a: Int, b: Int -> a + b }),
+    MINUS("-", IntBinaryOperator { a: Int, b: Int -> a - b }),
+    TIMES("*", IntBinaryOperator { a: Int, b: Int -> a * b }),
+    DIVIDE("/", IntBinaryOperator { a: Int, b: Int -> a / b });
+
+    companion object {
+        fun calculate(a: Int, b: Int, operator: String): Optional<Int> {
+            return Arrays.stream(values())
+                .filter { calc: Operator -> calc.signature == operator }
+                .map { calc: Operator -> calc.function.applyAsInt(a, b) }
+                .findFirst()
         }
     }
 }
--- src/test/kotlin/practice/StringCalculatorTest.kt
+++ src/test/kotlin/practice/StringCalculatorTest.kt
@@ -3,6 +3,7 @@ package practice
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -21,10 +22,20 @@ internal class StringCalculatorTest {
 
     @DisplayName("잘못된 수식인 경우 예외처리")
     @ParameterizedTest
-    @ValueSource(strings = ["1 2", "+", "1 +", "+ 1"])
+    @ValueSource(strings = ["1 2", "+", "1 +", "+ 1", "1+2"])
     internal fun validateExpression(input: String?) {
         Assertions.assertThatThrownBy { calculator.calculate(input!!) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("잘못된 수식입니다.")
+    }
+
+    @DisplayName("계산 기능")
+    @Test
+    internal fun calculate() {
+        assertAll(
+            { Assertions.assertThat(calculator.calculate("1 + 2")).isEqualTo(3) },
+            { Assertions.assertThat(calculator.calculate("8 + 2 / 2")).isEqualTo(5) },
+            { Assertions.assertThat(calculator.calculate("4 / 2 + 2 * 3")).isEqualTo(12) }
+        )
     }
 }
