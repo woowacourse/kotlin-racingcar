f11956642c19981b6cfbaa2d9a8b24aeecb93851
naeunkim <knae11@naver.com>
2021-05-19 15:03:02 +0900

    feat: Cars race 기능 구현

--- src/test/kotlin/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/racingcar/domain/CarsTest.kt
@@ -1,14 +1,37 @@
 package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class CarsTest{
+internal class CarsTest {
 
     @Test
-    internal fun race() {
+    internal fun create() {
         val cars = Cars(3)
         assertThat(cars.cars()).hasSize(3)
     }
+
+    @Test
+    internal fun race() {
+        val moveStrategy: MoveStrategy = object : MoveStrategy {
+            override fun isMovable(): Boolean {
+                return true
+            }
+        }
+        val stopStrategy: MoveStrategy = object : MoveStrategy {
+            override fun isMovable(): Boolean {
+                return false
+            }
+        }
+        val betterCar = Car(moveStrategy)
+        val aaronCar = Car(stopStrategy)
+
+        val cars = Cars(arrayListOf(betterCar, aaronCar))
+        cars.race()
+
+        assertThat(betterCar.isSamePosition(1)).isTrue()
+        assertThat(aaronCar.isSamePosition(0)).isTrue()
+    }
+
+
 }
