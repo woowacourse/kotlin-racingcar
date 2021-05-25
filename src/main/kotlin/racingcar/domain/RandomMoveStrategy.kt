1bc498742fb62dae4abfe8370e6d5174e9a124fa
naeunkim <knae11@naver.com>
2021-05-25 14:52:09 +0900

    fix: functional Interface 적용 수정

--- src/main/kotlin/racingcar/domain/RandomMoveStrategy.kt
+++ src/main/kotlin/racingcar/domain/RandomMoveStrategy.kt
@@ -1,9 +1,9 @@
 package racingcar.domain
 
-val randomMoveStrategy: () -> Boolean = {
+val randomMoveStrategy: () -> Boolean = fun(): Boolean {
     val randomNumber = Math.random() * 10 + 1
     if (randomNumber >= 4) {
-        true
+        return true
     }
-    false
+    return false
 }
