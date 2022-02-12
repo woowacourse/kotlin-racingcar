7e2fc341d08b6c312d133b411fd37e87e4c61236
osandra <okhw97@gmail.com>
2022-02-12 15:30:06 +0900

    feat: Car 클래스 및 RandomGenerator 클래스 코드 추가

---
+++ src/main/kotlin/water/racingcar/domain/Car.kt
@@ -0,0 +1,22 @@
+package water.racingcar.domain
+
+class Car(val name: String) {
+    var movedValue: Int = 0
+
+    fun move(randomNumber: Int) {
+        if (randomNumber >= 4)
+            movedValue++
+    }
+
+    override fun toString(): String {
+        val output = StringBuilder()
+        var moved = movedValue
+
+        output.append("$name : ")
+        while (moved > 0) {
+            output.append("-")
+            moved--
+        }
+        return output.toString()
+    }
+}
---
+++ src/main/kotlin/water/racingcar/util/RandomGenerator.kt
@@ -0,0 +1,5 @@
+package water.racingcar.util
+
+class RandomGenerator {
+    fun generateRandomNumber(): Int = (0..9).random()
+}
