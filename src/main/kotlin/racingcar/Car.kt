0861970d0627b7cd1e95e31912a066f48aa91162
jayjaehunchoi <wogns0108@naver.com>
2022-04-28 09:21:50 +0900

    feat : 이름이 입력되지 않은 경우 에러를 발생한다

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,12 +2,19 @@ package racingcar
 
 class Car(val name: String, var position : Int = 0) {
     init {
+        validateNoName(name)
         validateNameSpace(name)
     }
 
     private fun validateNameSpace(name : String) {
         if (name != name.replace(" ", "")) {
             throw IllegalArgumentException("[ERROR] 이름에 공백이 존재합니다.")
+        }
+    }
+
+    private fun validateNoName(name : String) {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("[ERROR] 이름을 입력하지 않았습니다.")
         }
     }
 }
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import io.kotest.matchers.throwable.haveMessage
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,6 +12,16 @@ class CarTest {
     fun nameSpaceException() {
         assertThrows<IllegalArgumentException> {
             Car(" huni")
+            haveMessage("[ERROR] 이름에 공백이 존재합니다.")
+        }
+    }
+
+    @DisplayName("이름을 입력하지 않은 경우 에러가 발생한다")
+    @Test
+    fun nameNotInsert() {
+        assertThrows<IllegalArgumentException> {
+            Car("")
+            haveMessage("[ERROR] 이름을 입력하지 않았습니다.")
         }
     }
 }
