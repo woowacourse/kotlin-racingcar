c98a1a92639570d891f1f08717c4d4ddf40b6c1b
kth990303 <kth990303@naver.com>
2022-04-28 14:50:26 +0900

    feat: 널값을 검증하는 메서드 추가

--- src/main/kotlin/racingcar/validator/KCarNameValidator.kt
+++ src/main/kotlin/racingcar/validator/KCarNameValidator.kt
@@ -2,10 +2,12 @@ package racingcar.validator
 
 private const val EMPTY_NAME_ERROR_MESSAGE = "이름을 입력하지 않았습니다. 재입력해주세요."
 private const val MAX_NAME_LENGTH_ERROR_MESSAGE = "이름을 5글자 이하로 입력해주세요."
+private const val NULL_ERROR_MESSAGE = "널값을 입력할 수 없습니다."
 private const val MAX_LENGTH = 5
 
 fun validateCarName(name: String) {
     require(!isEmptyName(name)) { EMPTY_NAME_ERROR_MESSAGE }
+    require(!isNull(name)) { NULL_ERROR_MESSAGE }
     require(!isIllegalLength(name)) { MAX_NAME_LENGTH_ERROR_MESSAGE }
 }
 
--- src/main/kotlin/racingcar/validator/KRoundNumberValidator.kt
+++ src/main/kotlin/racingcar/validator/KRoundNumberValidator.kt
@@ -1,9 +1,11 @@
 package racingcar.validator
 
 private const val INVALID_ROUND_ERROR_MESSAGE = "자연수로 입력해 주세요."
+private const val NULL_ERROR_MESSAGE = "널값을 입력할 수 없습니다."
 private val POSITIVE_INTEGER_REGEX = Regex("^([1-9][0-9]*)$")
 
 fun validateRoundNumber(round: String) {
+    require(!isNull(round)) { NULL_ERROR_MESSAGE }
     require(isPositiveNumber(round)) { INVALID_ROUND_ERROR_MESSAGE }
 }
 
