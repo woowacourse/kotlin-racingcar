3f1ac73cbe7fb6f90e7f01db74be03ba9c44630c
BeomsooKim <kbsat@naver.com>
2022-05-01 16:32:14 +0900

    feat: 문자열 계산기 구현

---
+++ src/test/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,49 @@
+package calculator
+
+import kotlin.IllegalArgumentException
+
+class StringCalculator {
+
+    fun add(input: String?): Int {
+        if (input === null || input.isEmpty()) {
+            return 0
+        }
+        val regex = Regex("//(.)\n(.*)")
+        val matchResult = regex.matchEntire(input)
+        if (matchResult != null) {
+            return calcSumWithCustomDelimiter(matchResult)
+        }
+        return calcSumNoCustomDelimiter(input)
+    }
+
+    private fun calcSumWithCustomDelimiter(matchResult: MatchResult): Int {
+        val groupValues = matchResult.groupValues
+        val customDelimiter = groupValues[1]
+        val tokens = groupValues[2].split(customDelimiter)
+        return calcSum(tokens)
+    }
+
+    private fun calcSumNoCustomDelimiter(input: String): Int {
+        val split = input.split(",", ":")
+        if (split.size == 1) {
+            return input.toInt()
+        }
+
+        return calcSum(split)
+    }
+
+    private fun calcSum(strings: List<String>): Int {
+        var sum = 0
+        for (number in strings) {
+            validatePositive(number.toInt())
+            sum += number.toInt()
+        }
+        return sum
+    }
+
+    private fun validatePositive(number: Int) {
+        if ( number < 0) {
+            throw IllegalArgumentException("음수를 입력하면 안됩니다.");
+        }
+    }
+}
---
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -0,0 +1,51 @@
+package calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
+
+class StringCalculatorTest {
+
+    private val calculator = StringCalculator()
+
+    @NullAndEmptySource
+    fun `Null 혹은 Empty가 들어올 경우 0이 반환된다`(string: String?) {
+        assertThat(calculator.add(string)).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `숫자 하나만 들어올 경우 그 숫자를 반환한다`(input: String) {
+        assertThat(calculator.add(input)).isEqualTo(input.toInt())
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["'1,2', 3", "'2,4', 6"])
+    fun `숫자 두개가 컴마로 구분해 들어올 경우 두 숫자의 합을 반환한다`(input: String, result: Int) {
+        assertThat(calculator.add(input)).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["'1,2:3', 6", "'3:3,6', 12"])
+    fun `구분자는 컴마 외에 콜론도 이용할 수 있다`(input: String, result: Int) {
+        assertThat(calculator.add(input)).isEqualTo(result)
+    }
+
+    @Test
+    fun `커스텀 구분자를 만들어 사용할 수 있다`() {
+        assertThat(calculator.add("//;\n1;2;3")).isEqualTo(6)
+    }
+
+    @Test
+    fun `음수가 존재할 경우 예외가 발생한다`() {
+        assertThatThrownBy { calculator.add("-1,2,3") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("음수를 입력하면 안됩니다.")
+    }
+}
