a4ad967e9062c818c00f08f0f2b641a21cb1d9fd
asebn1 <asebn1@khu.ac.kr>
2022-05-02 03:50:48 +0900

    feat: 계산기 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,4 +1,31 @@
 package calculator
 
-class StringCalculator {
+object StringCalculator {
+
+    fun calculate(input: String): Int {
+        val inputSplit = input.split(" ")
+        var result = inputSplit[0].toInt()
+        for (index in (1 until inputSplit.size) step 2) {
+            val operator = inputSplit[index]
+            val second = inputSplit[index + 1].toInt()
+            result = operate(result, second, operator)
+        }
+        return result
+    }
+
+    private fun operate(first: Int, second: Int, operator: String): Int {
+        return OPERATORS.getOrElse(operator)
+        { throw IllegalArgumentException("연산이 없습니다.") }(first, second)
+    }
+
+    private val OPERATORS =
+        mapOf(
+            "+" to { first: Int, second: Int -> first + second },
+            "-" to { first: Int, second: Int -> first - second },
+            "*" to { first: Int, second: Int -> first * second },
+            "/" to { first: Int, second: Int ->
+                require(second != 0) { "0으로 나눌 수 없습니다." }
+                first / second
+            }
+        )
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -1,5 +1,39 @@
 package calculator
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-internal class StringCalculatorTest
+class StringCalculatorTest {
+
+    @Test
+    fun plus() {
+        assertThat(StringCalculator.calculate("1 + 2")).isEqualTo(3)
+    }
+
+    @Test
+    fun minus() {
+        assertThat(StringCalculator.calculate("6 - 3")).isEqualTo(3)
+    }
+
+    @Test
+    fun multiply() {
+        assertThat(StringCalculator.calculate("2 * 3")).isEqualTo(6)
+    }
+
+    @Test
+    fun divide() {
+        assertThat(StringCalculator.calculate("4 / 2")).isEqualTo(2)
+    }
+
+    @Test
+    fun multiOperators() {
+        assertThat(StringCalculator.calculate("3 + 5 - 2")).isEqualTo(6)
+    }
+
+    @Test
+    fun divideZero() {
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { StringCalculator.calculate("10 / 0") }
+    }
+}
