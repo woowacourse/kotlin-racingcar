bbf286415bb5d75ee3a8a8b2118d3cecbc65bc2b
cjlee38 <cjlee38@github.com>
2022-05-01 15:41:20 +0900

    refactor(Strategy): 이동 전략 기본값 추가

--- src/main/kotlin/racingcar/domain/RandomMovingStrategy.kt
+++ src/main/kotlin/racingcar/domain/RandomMovingStrategy.kt
@@ -1,14 +1,16 @@
 package racingcar.domain
 
 
-class RandomMovingStrategy(private val range: IntRange, private val threshold: Int) : MovingStrategy{
+class RandomMovingStrategy(
+    private val range: IntRange = (RANDOM_MIN_VALUE..RANDOM_MAX_VALUE),
+    private val threshold: Int = THRESHOLD
+) : MovingStrategy {
 
-//
-//    companion object {
-//        const val THRESHOLD = 4
-//        const val RANDOM_MIN_VALUE = 0
-//        const val RANDOM_MAX_VALUE = 9
-//    }
+    companion object {
+        const val THRESHOLD = 4
+        const val RANDOM_MIN_VALUE = 0
+        const val RANDOM_MAX_VALUE = 9
+    }
 
     override fun isMovable(): Boolean {
         return range.random() >= threshold
