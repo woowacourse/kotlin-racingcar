e58163087f4e6e63a74cc597b7a8d9357d029e4f
summerlunaa <summerlunaa9@gmail.com>
2022-05-02 17:56:41 +0900

    refactor: stream 삭제

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -1,18 +1,6 @@
 package racingcar.domain
 
-import java.util.stream.Collectors
-
 class Cars(val cars: List<Car>, private val strategy: MovingStrategy = RandomMovingStrategy()) {
-
-    companion object {
-        fun from(carNames: List<String>): Cars {
-            return Cars(
-                carNames.stream()
-                    .map { Car(it) }
-                    .collect(Collectors.toList()),
-            )
-        }
-    }
 
     init {
         require(cars.toSet().size == cars.size) { "자동차 이름에 중복이 있습니다." }
@@ -20,24 +8,24 @@ class Cars(val cars: List<Car>, private val strategy: MovingStrategy = RandomMov
 
     fun move(): Cars {
         return Cars(
-            cars.stream()
-                .map { it.moveForward(strategy) }
-                .collect(Collectors.toList()),
+            cars.map { it.moveForward(strategy) },
             strategy
         )
     }
 
     fun findWinners(): List<Car> {
-        return cars.stream()
-            .filter { it.isSamePosition(findMaxPosition()) }
-            .collect(Collectors.toList())
+        return cars.filter { it.isSamePosition(findMaxPosition()) }
     }
 
     private fun findMaxPosition(): Position {
-        return cars.stream()
-            .map { it.position }
-            .sorted()
-            .findFirst()
-            .orElseThrow { IllegalStateException("최대 위치를 찾을 수 없습니다.") }
+        return cars.maxOfOrNull { it.position } ?: throw IllegalArgumentException("최대 위치를 찾을 수 없습니다.")
+    }
+
+    companion object {
+        fun from(carNames: List<String>): Cars {
+            return Cars(
+                carNames.map { Car(it) }
+            )
+        }
     }
 }
