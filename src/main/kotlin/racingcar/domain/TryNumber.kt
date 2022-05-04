c63d3a4850d744dd1ff3963e3ad0d7c6b6352fc0
cjlee38 <cjlee38@github.com>
2022-05-04 10:54:01 +0900

    refactor(TryNumber): 엘비스 연산자 활용

--- src/main/kotlin/racingcar/domain/TryNumber.kt
+++ src/main/kotlin/racingcar/domain/TryNumber.kt
@@ -1,9 +1,8 @@
 package racingcar.domain
 
-import racingcar.util.parseInt
-
 data class TryNumber(private var value: Int) {
-    constructor(input: String) : this(parseInt(input))
+    constructor(input: String) : this(input.toIntOrNull() ?:
+    throw IllegalArgumentException("$input 을 숫자로 변환할 수 없습니다."))
 
     init {
         require(value > 0) { "이동횟수는 양수여야 합니다." }
