87768c167df2c19179838f902b31c7c8a3f222a2
cjlee38 <cjlee38@github.com>
2022-05-04 10:55:22 +0900

    refactor(Position): compareTo 순서 변경

--- src/main/kotlin/racingcar/domain/Position.kt
+++ src/main/kotlin/racingcar/domain/Position.kt
@@ -6,13 +6,12 @@ data class Position(val distance: Int = 0): Comparable<Position> {
     }
 
     fun increase(strategy: MovingStrategy): Position {
-        if (strategy.isMovable()) {
+        return if (strategy.isMovable()) {
             return Position(distance + 1)
-        }
-        return Position(distance)
+        } else Position(distance)
     }
 
     override fun compareTo(other: Position): Int {
-        return other.distance.compareTo(this.distance)
+        return this.distance.compareTo(other.distance)
     }
 }
