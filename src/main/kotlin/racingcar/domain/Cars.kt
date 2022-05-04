6f736873db55df57c4ff1d1db7ff43a18648bcf7
cjlee38 <cjlee38@github.com>
2022-05-04 10:52:34 +0900

    refactor(Car): 스트림 제거 및 컬렉션 API 사용

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -1,39 +1,27 @@
 package racingcar.domain
 
-import java.util.stream.Collectors
-
 class Cars(val cars: List<Car>, private val strategy: MovingStrategy = RandomMovingStrategy()) {
     companion object {
         fun from(carNames: List<String>): Cars {
-            return Cars(
-                carNames.stream()
-                    .map { Car(it) }
-                    .collect(Collectors.toList()),
-            )
+            return Cars(carNames.map { Car(it) })
         }
     }
 
     init {
-        require(cars.toSet().size == cars.size) { "자동차 이름에 중복이 있습니다." }
+        require(cars.distinct().size == cars.size) { "자동차 이름에 중복이 있습니다." }
     }
 
     fun move(): Cars {
-        return Cars(cars.stream()
-            .map { it.moveForward(strategy) }
-            .collect(Collectors.toList()), strategy)
+        return Cars(cars.map { it.moveForward(strategy) })
     }
 
     fun findWinners(): List<Car> {
-        return cars.stream()
-            .filter { it.isSamePosition(findMaxPosition()) }
-            .collect(Collectors.toList())
+        return cars.filter { it.isSamePosition(findMaxPosition()) }
     }
 
     private fun findMaxPosition(): Position {
-        return cars.stream()
+        return cars
             .map { it.position }
-            .sorted()
-            .findFirst()
-            .orElseThrow { IllegalStateException("최대 위치를 찾을 수 없습니다.") }
+            .maxOrNull() ?: throw IllegalStateException("최대 위치를 찾을 수 없습니다.")
     }
 }
