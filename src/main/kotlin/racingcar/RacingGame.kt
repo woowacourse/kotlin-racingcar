06ff420ba90e5b5f821a76dbdaa689b36319c658
hyewoncc <hyewoncc@gmail.com>
2022-05-24 21:23:34 +0900

    feat: 게임 진행 기능

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,31 @@
+package racingcar
+
+fun main() {
+    RacingGame().play()
+}
+
+class RacingGame {
+    fun play() {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val input = readln()
+        val names = input.split(",").map { it.trim() }
+        val values = mutableListOf<Car>()
+        for (name in names) {
+            values.add(Car(name))
+        }
+        val cars = Cars(values)
+
+        println("시도할 회수는 몇회인가요?")
+        var count: Int = readln().toInt()
+        for (i in 1..count) {
+            cars.moveAll()
+            for (car in cars.values) {
+                println("${car.name} : " + "-".repeat(car.position))
+            }
+            println()
+        }
+
+        val winners = cars.findWinners()
+        println("${winners.joinToString { car -> car.name }}가 최종 우승했습니다.")
+    }
+}
