7db9edafc7eb891b650d6b9cb768f4dc21fa23e9
hyewoncc <hyewoncc@gmail.com>
2022-05-24 20:49:28 +0900

    feat: 랜덤값으로 자동차 이동

--- src/main/kotlin/racingcar/Cars.kt
+++ src/main/kotlin/racingcar/Cars.kt
@@ -5,4 +5,10 @@ class Cars(val values: List<Car>) {
         val maxPosition = values.maxOf { it.position }
         return values.filter { it.position == maxPosition }
     }
+
+    fun moveAll() {
+        for (value in values) {
+            value.move((1..9).random())
+        }
+    }
 }
