0da93637128b8abd8fc47fb83a6b3f3fe9bb15de
Ohzzi <jihoon970712@gmail.com>
2022-12-19 21:09:20 +0900

    feat: 이름을 나타내는 값 타입 구현

---
+++ src/main/kotlin/Name.kt
@@ -0,0 +1,9 @@
+class Name(
+    val value: String
+) {
+    init {
+        require(value.isNotEmpty() && value.length <= 5) {
+            "자동차 이름은 1자 이상, 5자 이하여야 합니다."
+        }
+    }
+}
---
+++ src/test/kotlin/NameTest.kt
@@ -0,0 +1,18 @@
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.string.match
+
+internal class NameTest : StringSpec({
+
+    "이름이_0자_또는_6자_이상이면_예외를_발생시킨다" {
+        listOf(
+            "", "123456"
+        ).forEach { input: String ->
+            shouldThrow<IllegalArgumentException> {
+                Name(input)
+            }.message should match("자동차 이름은 1자 이상, 5자 이하여야 합니다.")
+        }
+    }
+})
+
