f10149fa53fb5200054de2de13f10c34b9749872
Ohzzi <jihoon970712@gmail.com>
2022-12-22 16:46:43 +0900

    feat: 자동차를 모은 일급 컬렉션 구현

---
+++ src/main/kotlin/domain/Cars.kt
@@ -0,0 +1,19 @@
+package domain
+
+class Cars(
+    val items: List<Car>
+) {
+
+    constructor(vararg names: String) : this(names.map { Car(it) })
+
+    init {
+        val distinctCount = items.map { it.name }
+            .distinct()
+            .count()
+        require(distinctCount == items.count()) { "자동차 이름은 중복될 수 없습니다." }
+    }
+
+    fun move(numberGenerator: NumberGenerator) {
+        items.forEach { it.move(numberGenerator) }
+    }
+}
---
+++ src/test/kotlin/domain/CarsTest.kt
@@ -0,0 +1,23 @@
+package domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.match
+
+internal class CarsTest : StringSpec({
+
+    "중복된 자동차 이름이 있으면 예외를 발생시킨다." {
+        shouldThrow<IllegalArgumentException> { Cars("자동차", "자동차") }
+            .message should match("자동차 이름은 중복될 수 없습니다.")
+    }
+
+    "자동차들을 전진시킬 수 있다." {
+        val cars = Cars("자동차1", "자동차2")
+
+        cars.move { 4 }
+
+        cars.items.forEach { it.position shouldBe Position(1) }
+    }
+})
