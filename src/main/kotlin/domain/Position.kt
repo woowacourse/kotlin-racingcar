e9998e52a3661667b4d44f6bf6bd9208e14a1d30
Ohzzi <jihoon970712@gmail.com>
2022-12-24 17:41:57 +0900

    feat: 포지션 객체가 비교 가능하도록 구현

--- src/main/kotlin/domain/Position.kt
+++ src/main/kotlin/domain/Position.kt
@@ -2,10 +2,12 @@ package domain
 
 data class Position(
     val value: Int = 0
-) {
+) : Comparable<Position> {
     init {
         require(value >= 0) { "위치는 0 이상이어야 합니다." }
     }
 
     fun increase(): Position = Position(value + 1)
+
+    override fun compareTo(other: Position): Int = value.compareTo(other.value)
 }
--- src/test/kotlin/domain/PositionTest.kt
+++ src/test/kotlin/domain/PositionTest.kt
@@ -2,6 +2,8 @@ package domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.should
 import io.kotest.matchers.string.match
@@ -14,5 +16,15 @@ internal class PositionTest : StringSpec({
     "위치값이 0 미만이면 예외를 발생시킨다." {
         shouldThrow<IllegalArgumentException> { Position(-1) }
             .message should match("위치는 0 이상이어야 합니다.")
+    }
+
+    "위치값 간의 대소 비교가 가능하다." {
+        forAll(
+            row(Position(), Position(1), -1),
+            row(Position(), Position(), 0),
+            row(Position(1), Position(), 1)
+        ) { position1, position2, expected ->
+            position1.compareTo(position2) shouldBeExactly expected
+        }
     }
 })
