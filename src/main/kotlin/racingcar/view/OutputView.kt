ece69d482cbc990b603f25e6ca7e8c1b2fce2f81
changhwan <chang5hustle@gmail.com>
2023-02-07 15:40:21 +0900

    feat(racingcar): OutputView 초기 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,3 +1,24 @@
 package racingcar.view
 
-class OutputView
+import racingcar.constant.INPUT_CARS_NAME
+import racingcar.constant.INPUT_MOVING_COUNT
+import racingcar.constant.OUTPUT_RESULT
+import racingcar.constant.OUTPUT_WINNER
+
+class OutputView {
+    fun printGettingCarsName() {
+        println(INPUT_CARS_NAME)
+    }
+
+    fun printGettingMovingCount() {
+        println(INPUT_MOVING_COUNT)
+    }
+
+    fun printResult() {
+        println(OUTPUT_RESULT)
+    }
+
+    fun printWinner() {
+        println(OUTPUT_WINNER)
+    }
+}
