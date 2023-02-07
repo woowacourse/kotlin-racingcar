0dd77392f04ac7bc781b42af4adabc01d37bc87a
EmilyCh0 <emily9803@ewhain.net>
2023-02-07 16:34:08 +0900

    feat: 경주 초기화

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,4 +5,5 @@ import view.OutputView
 
 fun main() {
     val controller = Controller(InputView(Validator()), OutputView())
+    controller.start()
 }
--- src/main/kotlin/controller/Controller.kt
+++ src/main/kotlin/controller/Controller.kt
@@ -1,18 +1,30 @@
 package controller
 
+import domain.Car
+import utils.RepeatInputProcess
 import view.InputView
 import view.OutputView
 
 class Controller(private val inputView: InputView, private val outputView: OutputView) {
+    private var cars = mutableListOf<Car>()
+    private var roundCount: Int = 0
     fun start() {
+        initializeGame()
     }
 
-    fun initializeGame() {
+    private fun initializeGame() {
+        outputView.printCarNamesPrompt()
+        val names = RepeatInputProcess.repeat { inputView.readCarNames() } as List<String>
+        for (name in names) {
+            cars.add(Car(name, 0))
+        }
+        outputView.printRoundCountPrompt()
+        roundCount = RepeatInputProcess.repeat { inputView.readRoundCount() } as Int
     }
 
-    fun playGame() {
+    private fun playGame() {
     }
 
-    fun finishGame() {
+    private fun finishGame() {
     }
 }
