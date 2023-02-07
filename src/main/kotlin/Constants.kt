29ace340bf8451e7e76b72ef9f6431287e90082e
Hyemin Song <hyemdooly@gmail.com>
2023-02-07 15:13:59 +0900

    feat(Constants): Constants 클래스 생성

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,9 @@
+class Constants {
+
+    companion object {
+        const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+        const val INPUT_TRY_NUMBER = "시도할 횟수는 몇 회인가요?"
+        const val OUTPUT_RESULT = "실행 결과"
+        const val OUTPUT_WINNER = "최종 우승자: "
+    }
+}
