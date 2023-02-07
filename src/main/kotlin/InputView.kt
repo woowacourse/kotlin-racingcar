e1aa0d3829bf5dcac0d7280aa62e8c6c1ce0389f
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-07 15:46:19 +0900

    <feat> 자동차 이름 입력받기

---
+++ src/main/kotlin/InputView.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class InputView {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    fun inputCarNames(): String {
+        bw.write("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        bw.flush()
+
+        return br.readLine()
+    }
+}
