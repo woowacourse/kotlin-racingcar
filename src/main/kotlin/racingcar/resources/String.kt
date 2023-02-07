e5401d8e604b4f4ccd1b8852096adc74f3eaeea1
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-07 14:57:39 +0900

    feat (InputView) : 자동차 이름을 입력 받는 기능 구현

---
+++ src/main/kotlin/racingcar/resources/String.kt
@@ -0,0 +1,12 @@
+package racingcar.resources
+
+// FOR MENU RECOMMEND LOGIC
+
+// FOR OUTPUT VIEW
+
+// FOR INPUT VIEW
+const val INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)"
+
+// FOR ERROR MESSAGE
+private const val ERROR_MESSAGE_PREFIX = "[ERROR] "
+const val ERROR_INPUT_NAME_AT_MOST_FIVE = ERROR_MESSAGE_PREFIX + "자동차 이름은 5자를 초과할 수 없습니다."
---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,17 @@
+package racingcar.view
+
+import racingcar.resources.ERROR_INPUT_NAME_AT_MOST_FIVE
+import racingcar.resources.INPUT_CAR_NAMES
+
+class InputView {
+    fun inputCarNames(): List<String> {
+        println(INPUT_CAR_NAMES)
+        val userInput = readLine()?.split(',')?.map { checkCarNameLength(it) }
+        return userInput ?: listOf("")
+    }
+
+    private fun checkCarNameLength(carName: String): String {
+        require(carName.length in 1..5) { println(ERROR_INPUT_NAME_AT_MOST_FIVE) }
+        return carName
+    }
+}
