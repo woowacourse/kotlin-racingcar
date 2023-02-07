70a64cef4ebe628f8ee168a6d9169f4445c724df
Hyemin Song <hyemdooly@gmail.com>
2023-02-07 16:07:42 +0900

    feat(Car): 예외사항 처리 기능 추가

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,4 +1,9 @@
 class Car(private val name: String, private var position: Int) {
+
+    init {
+        Validator().checkName(name)
+    }
+
     fun getInfo(): Pair<String, Int> {
         return Pair(name, position)
     }
