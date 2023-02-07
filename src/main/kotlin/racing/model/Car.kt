583a074dba9c202647e39f61cb4890385033ca81
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-07 15:53:34 +0900

    feat: 자동차의 이동 및 위치 저장기능 구현

--- src/main/kotlin/racing/model/Car.kt
+++ src/main/kotlin/racing/model/Car.kt
@@ -1,3 +1,11 @@
 package racing.model
 
-class Car
+class Car(private val name: String) {
+    private var position = 0
+
+    fun getPosition() = position
+
+    fun move() {
+        position++
+    }
+}
