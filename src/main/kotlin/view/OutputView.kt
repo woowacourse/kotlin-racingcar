26d315f15411b66a2146ab5cf1b97c180d75fe0e
briandr97 <briandr@naver.com>
2023-02-07 16:02:13 +0900

    feat(OutputView): Create basic method in OutputView

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,3 +1,31 @@
 package view
 
-class OutputView
+private const val ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+private const val ENTER_COUNT = "시도할 횟수는 몇 회인가요?"
+private const val RESULT = "실행 결과"
+private const val COLON = " : "
+private const val WINNER = "최종 우승자: "
+private const val SEPARATOR = ", "
+
+class OutputView {
+    fun printEnterCarName() {
+        println(ENTER_CAR_NAME)
+    }
+
+    fun printEnterCount() {
+        println(ENTER_COUNT)
+    }
+
+    fun printResult(result: Map<String, String>) {
+        println(RESULT)
+        result.forEach { entry ->
+            val eachResult = entry.key + COLON + entry.value
+            println(eachResult)
+        }
+        println()
+    }
+
+    fun printWinner(winners: List<String>) {
+        println(WINNER + winners.joinToString(SEPARATOR))
+    }
+}
