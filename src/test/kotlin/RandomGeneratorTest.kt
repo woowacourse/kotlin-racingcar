4934003d72037b516d9b6f2b3cc367ba48cbe61f
Hyemin Song <hyemdooly@gmail.com>
2023-02-07 16:22:52 +0900

    test(RandomGeneratorTest): RandomGenerator 클래스 테스트 생성

---
+++ src/test/kotlin/RandomGeneratorTest.kt
@@ -0,0 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RandomGeneratorTest {
+
+    @Test
+    fun getRandomNumberTest() {
+        var isTrue = true
+        for (i in 1..100) {
+            val number = RandomGenerator().getRandomNumber()
+            if (number < 0 || number > 9) {
+                isTrue = false
+            }
+        }
+        assertThat(isTrue).isTrue
+    }
+}
