9ea9c32ac6d6d9e07b27a476ef942240d3ef1313
EmilyCh0 <emily9803@ewhain.net>
2023-02-07 15:59:47 +0900

    feat: 시도할 횟수 입력받기

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,13 +5,15 @@ import exception.Validator
 class InputView(
     private val validator: Validator
 ) {
+    private val console = System.console()
 
     fun readCarNames(): List<String> {
-        val console = System.console()
         val input = console.readLine()
         return validator.checkCarName(input)
     }
 
-    fun readRoundCount() {
+    fun readRoundCount(): Int {
+        val input = console.readLine()
+        return validator.checkRoundCount(input)
     }
 }
