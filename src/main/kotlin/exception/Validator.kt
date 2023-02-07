ef43b3cc54b3a29b29fd9c4bccb177cf541c57c6
EmilyCh0 <emily9803@ewhain.net>
2023-02-07 16:03:03 +0900

    feat: 안내 문구 출력

--- src/main/kotlin/exception/Validator.kt
+++ src/main/kotlin/exception/Validator.kt
@@ -4,4 +4,8 @@ class Validator {
     fun checkCarName(input: String): List<String> {
         return listOf()
     }
+
+    fun checkRoundCount(input: String): Int {
+        return 0
+    }
 }
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -3,12 +3,15 @@ package view
 class OutputView {
 
     fun printCarNamesPrompt() {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     }
 
     fun printRoundCountPrompt() {
+        println("시도할 횟수는 몇 회인가요?")
     }
 
     fun printRoundResultMessage() {
+        println("실행 결과")
     }
 
     fun printWinners() {
