7b2897f51f19e2d0db2d33d436da5ebf49734ff3
changhwan <chang5hustle@gmail.com>
2023-02-07 15:26:16 +0900

    feat(racingcar): 관련 상수 추가

---
+++ src/main/kotlin/racingcar/constant/Const.kt
@@ -0,0 +1,9 @@
+package racingcar.constant
+
+const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+const val INPUT_MOVING_COUNT = "시도할 횟수는 몇 회인가요?"
+
+const val OUTPUT_RESULT = "\n실행 결과"
+const val OUTPUT_WINNER = "최종 우승자: "
+
+const val STATE_UNIT = "-"
