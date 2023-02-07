2102a96deeb325b1deefd20f493836d450131841
inseonyun <yuninseon@naver.com>
2023-02-07 16:08:28 +0900

    refactor: 이름 입력 시 Null 발생 예외 처리 수정

--- src/main/kotlin/exception/NameException.kt
+++ src/main/kotlin/exception/NameException.kt
@@ -2,13 +2,19 @@ package exception
 
 import message.ExceptionMessage
 
-class NameException(carNames: String) {
+class NameException(carNames: String?) {
     init {
+        checkEmpty(carNames)
         checkNames(carNames)
     }
 
-    private fun checkNames(carNames: String) {
-        splitCarNames(carNames).forEach { name ->
+    private fun checkEmpty(carNames: String?) {
+        if (carNames.isNullOrEmpty())
+            throw IllegalArgumentException(ExceptionMessage.NAME_IS_EMPTY_ERROR_MESSAGE)
+    }
+
+    private fun checkNames(carNames: String?) {
+        splitCarNames(carNames!!).forEach { name ->
             checkNameRange(name)
         }
     }
