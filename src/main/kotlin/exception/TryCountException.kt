8264a28cd66beefff90e4707c67dd43fbafdc329
inseonyun <yuninseon@naver.com>
2023-02-07 15:46:53 +0900

    feat: 시도 횟수가 양의 정수인지 check 하는 함수 구현

--- src/main/kotlin/exception/TryCountException.kt
+++ src/main/kotlin/exception/TryCountException.kt
@@ -5,6 +5,7 @@ import message.ExceptionMessage
 class TryCountException(tryCount: String) {
     init {
         checkTryCountInteger(tryCount)
+        checkTryCountPositive(tryCount)
     }
 
     private fun checkTryCountInteger(tryCount: String) {
@@ -13,5 +14,10 @@ class TryCountException(tryCount: String) {
         } catch (e: IllegalArgumentException) {
             throw IllegalArgumentException(ExceptionMessage.TRY_COUNT_INTEGER_ERROR_MESSAGE)
         }
+    }
+
+    private fun checkTryCountPositive(tryCount: String) {
+        if (tryCount.toInt() < 0)
+            throw IllegalArgumentException(ExceptionMessage.TRY_COUNT_POSITIVE_ERROR_MESSAGE)
     }
 }
--- src/main/kotlin/message/ExceptionMessage.kt
+++ src/main/kotlin/message/ExceptionMessage.kt
@@ -4,5 +4,6 @@ class ExceptionMessage {
     companion object {
         const val NAME_RANGE_ERROR_MASSAGE = "[ERROR] 자동차 이름은 1자 이상 5자 이하로 입력해주세요"
         const val TRY_COUNT_INTEGER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자만 입력해 주세요"
+        const val TRY_COUNT_POSITIVE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양의 정수여야 합니다"
     }
 }
