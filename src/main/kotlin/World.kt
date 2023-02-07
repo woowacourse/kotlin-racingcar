0673d92ab08301da5d09c578c606c743c8cf52fb
Yerin <myheaven_b@naver.com>
2023-02-07 14:41:53 +0900

    feat: 랜덤 숫자 생성

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -1,28 +1,28 @@
+import java.util.Random
+
 class World {
     val cars = mutableListOf<Car>()
     var attemptCount = 0
-    fun init() {
 
+    fun init() {
     }
 
     fun run() {
         for (i in 0 until cars.size) {
-
         }
     }
 
     fun determineWinner() {
-
     }
 
-    fun processStep(car : Car) {
+    fun processStep(car: Car) {
         val number = generateRandom()
-        if (number >= 4){
+        if (number >= 4) {
             car.forward()
         }
     }
 
     fun generateRandom(): Int {
-
+        return Random().nextInt(10)
     }
 }
