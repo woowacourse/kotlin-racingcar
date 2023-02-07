9a4f5222cd6658836e959905c06fc867085c86a9
Hyemin Song <hyemdooly@gmail.com>
2023-02-07 15:44:20 +0900

    fix(InputView): readline 수정

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,7 +1,7 @@
 class InputView {
     fun inputCarNames(): List<Car> {
-        val input = System.console().readLine()
-        val names = input.split(",").toList()
+        val input = readLine()
+        val names = input!!.split(",").toList()
         val cars = mutableListOf<Car>()
         for (name in names) {
             cars.add(Car(name, 0))
@@ -10,6 +10,6 @@ class InputView {
     }
 
     fun inputTryNumber(): Int {
-        return System.console().readLine().toInt()
+        return readLine()!!.toInt()
     }
 }
