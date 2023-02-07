ca51779e5708bf522437f33bdf79f7a80377fdca
tmdgh1592 <tmdgh1592@naver.com>
2023-02-07 16:00:09 +0900

    refactor: 조건 숫자, 에러문구 상수화

---
+++ src/main/kotlin/racingcar/utils/Dimens.kt
@@ -0,0 +1,7 @@
+package racingcar.utils
+
+const val MIN_CAR_NAME_LENGTH = 1
+const val MAX_CAR_NAME_LENGTH = 5
+
+const val MIN_ROUND_COUNT = 1
+const val MAX_ROUND_COUNT = Int.MAX_VALUE
--- src/main/kotlin/racingcar/utils/Validator.kt
+++ src/main/kotlin/racingcar/utils/Validator.kt
@@ -3,33 +3,36 @@ package racingcar.utils
 class Validator {
 
     fun checkCarNames(input: String): List<String> {
-        val names = input.split(",").toMutableList()
+        val names = input.split(CAR_NAME_DELIMITER).toMutableList()
 
         for (i in names.indices) {
             names[i] = names[i].trim()
-
-            if (names[i].length !in 1..4) {
-                throw IllegalArgumentException("error")
-            }
+            checkCarNameLength(names[i])
         }
 
         if (names.toSet().size != names.size) {
-            throw IllegalArgumentException("error")
+            throw IllegalArgumentException(DUPLICATED_CAR_NAME_ERROR_MESSAGE)
         }
 
         return names
     }
 
     fun checkRoundCount(input: String): Int {
-        val roundCount = input.toIntOrNull() ?: throw IllegalArgumentException("error")
+        val roundCount = input.toIntOrNull() ?: throw IllegalArgumentException(NOT_INTEGER_TYPE_ERROR_MESSAGE)
 
-        if (roundCount <= 0) {
-            throw IllegalArgumentException("0보다 작습니다.")
+        if (roundCount < MIN_ROUND_COUNT) {
+            throw IllegalArgumentException(ROUND_COUNT_BOUNDARY_ERROR_MESSAGE)
         }
-        if (roundCount > Int.MAX_VALUE) {
-            throw IllegalArgumentException("너무 숫자가 큼 ㅜㅜ")
+        if (roundCount > MAX_ROUND_COUNT) {
+            throw IllegalArgumentException(ROUND_COUNT_BOUNDARY_ERROR_MESSAGE)
         }
 
         return roundCount
+    }
+
+    private fun checkCarNameLength(name: String) {
+        if (name.length !in MIN_CAR_NAME_LENGTH..MAX_CAR_NAME_LENGTH) {
+            throw IllegalArgumentException(CAR_NAME_BOUNDARY_ERROR_MESSAGE)
+        }
     }
 }
