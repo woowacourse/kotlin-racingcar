16c7ab57e2c44d9a830d9089e3c664af225300b0
inseonyun <yuninseon@naver.com>
2023-02-08 15:37:26 +0900

    feat: 라운드 별 결과를 확인하는 기능 구현

--- src/main/kotlin/service/RacingCarGameService.kt
+++ src/main/kotlin/service/RacingCarGameService.kt
@@ -1,7 +1,28 @@
 package service
 
+import model.Car
+
 class RacingCarGameService {
     fun isPossibleMove(): Boolean = RandomNumberGenerator().generate() >= 4
 
     fun splitCarNames(carNames: String): List<String> = carNames.split(",")
+
+    fun initCarInfo(carNames: List<String>): List<Car> {
+        val carsInfo = mutableListOf<Car>()
+        carNames.forEach { name ->
+            carsInfo.add(Car(name, 0))
+        }
+
+        return carsInfo.toList()
+    }
+
+    fun getRoundResult(carsInfo: List<Car>): List<Car> {
+        carsInfo.forEach { car ->
+            car.position += moveForward(isPossibleMove())
+        }
+
+        return carsInfo
+    }
+
+    fun moveForward(isPossibleMove: Boolean): Int = if (isPossibleMove) 1 else 0
 }
