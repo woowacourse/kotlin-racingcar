2e161d76b8ed4ee3554ff75245d911a7999d100d
Gloria <ippnsj@gmail.com>
2023-02-08 17:18:26 +0900

    feat: 0에서 9 숫자 랜덤 생성 기능 구현

--- src/main/kotlin/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/domain/RandomNumberGenerator.kt
@@ -1,3 +1,12 @@
 package domain
 
-class RandomNumberGenerator
+class RandomNumberGenerator : NumberGenerator {
+    override fun generate(): Int {
+        return (RANDOM_LOWER_INCLUSIVE..RANDOM_UPPER_INCLUSIVE).random()
+    }
+
+    companion object {
+        const val RANDOM_LOWER_INCLUSIVE = 0
+        const val RANDOM_UPPER_INCLUSIVE = 9
+    }
+}
