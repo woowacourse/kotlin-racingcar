a1d54ce59fbc153af367a0769f7a0fcf03bcdd2b
changhwan <chang5hustle@gmail.com>
2023-02-08 13:32:48 +0900

    feat(racingcar): 자동차 이름 입력함수 작성

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,3 +1,22 @@
 package racingcar.view
 
-class InputView
+import racingcar.constant.ERROR_NAME_LENGTH
+import racingcar.domain.Validator
+
+class InputView {
+    fun getCarsName() {
+        val input = readLine()?.trim()
+
+        if (!input.isNullOrBlank()) {
+            checkNameLength(input.split(","))
+        } else {
+            throw IllegalArgumentException(ERROR_NAME_LENGTH)
+        }
+    }
+
+    fun checkNameLength(value: List<String>) {
+        value.forEach {
+            require(Validator.isNameLengthInRange(it)) { ERROR_NAME_LENGTH }
+        }
+    }
+}
