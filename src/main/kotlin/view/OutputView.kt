18b732ccba20934cb80854012e0d1450d4d5450c
Gloria <ippnsj@gmail.com>
2023-02-08 19:57:11 +0900

    feat: 최종 우승자 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,6 +1,7 @@
 package view
 
 import dto.RaceResultDTO
+import dto.WinnersDTO
 
 class OutputView {
     fun printExecutionResult() {
@@ -11,7 +12,13 @@ class OutputView {
         println(raceResultDTO.getResult())
     }
 
+    fun printWinners(winnersDTO: WinnersDTO) {
+        val winners = winnersDTO.getWinners()
+        winners.joinToString { ", " }
+    }
+
     companion object {
         const val EXECUTION_RESULT = "실행 결과"
+        const val FINAL_WINNER = "최종 우승자: "
     }
 }
