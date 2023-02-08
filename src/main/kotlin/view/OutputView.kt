712266ff30329ce5e212f0d22707e82958d183e8
Gloria <ippnsj@gmail.com>
2023-02-08 19:53:16 +0900

    feat: 실행 결과 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,3 +1,17 @@
 package view
 
-class OutputView
+import dto.RaceResultDTO
+
+class OutputView {
+    fun printExecutionResult() {
+        println(EXECUTION_RESULT)
+    }
+
+    fun printRaceResult(raceResultDTO: RaceResultDTO) {
+        println(raceResultDTO.getResult())
+    }
+
+    companion object {
+        const val EXECUTION_RESULT = "실행 결과"
+    }
+}
