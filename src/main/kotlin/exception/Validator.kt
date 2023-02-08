047ce681ebc44ead703629f20f075a0c8fc5f908
EmilyCh0 <emily9803@ewhain.net>
2023-02-08 14:08:50 +0900

    refactor: 에러 문구 상수 분리

--- src/main/kotlin/exception/Validator.kt
+++ src/main/kotlin/exception/Validator.kt
@@ -5,18 +5,26 @@ class Validator {
         val names = input.split(",")
         for (name in names) {
             if (name.length > 5) {
-                throw IllegalArgumentException("[ERROR] 5자를 초과할 수 없습니다.")
+                throw IllegalArgumentException(NAME_LENGTH_ERROR)
             }
             if (name.isEmpty()) {
-                throw IllegalArgumentException("[ERROR] 올바른 입력 형식이 아닙니다.")
+                throw IllegalArgumentException(INVALID_NAME_ERROR)
             }
         }
         return names
     }
 
     fun checkRoundCount(input: String): Int {
-        val num = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 숫자가 아닙니다.")
-        if (num < 0) throw IllegalArgumentException("[ERROR] 시도 횟수는 음수일 수 없습니다.")
+        val num = input.toIntOrNull() ?: throw IllegalArgumentException(NOT_NUMBER_ERROR)
+        if (num < 0) throw IllegalArgumentException(NEGATIVE_NUMBER_ERROR)
         return num
+    }
+
+    companion object {
+        private const val ERROR = "[ERROR]"
+        private const val NAME_LENGTH_ERROR = "$ERROR 5자를 초과할 수 없습니다."
+        private const val INVALID_NAME_ERROR = "$ERROR 올바른 입력 형식이 아닙니다."
+        private const val NOT_NUMBER_ERROR = "$ERROR 숫자가 아닙니다."
+        private const val NEGATIVE_NUMBER_ERROR = "$ERROR 시도 횟수는 음수일 수 없습니다."
     }
 }
