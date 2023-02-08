1ac9dcbe55672fce0961923decca87e50faa16fd
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-08 16:08:18 +0900

    feat: 실행결과 출력하는 기능 구현

--- src/main/kotlin/racing/view/OutputView.kt
+++ src/main/kotlin/racing/view/OutputView.kt
@@ -1,3 +1,27 @@
 package racing.view
 
-class OutputView
+import racing.model.Car
+
+class OutputView {
+    fun printResult(cars: List<Car>, maxMoveCount: Int) {
+        println("실행 결과")
+        repeat(maxMoveCount) {
+            printCurrentPosition(cars)
+        }
+    }
+
+    private fun printCurrentPosition(cars: List<Car>) {
+        for (car in cars) {
+            println("${car.getName()} : ${getDash(car.getPosition())}")
+        }
+        println()
+    }
+
+    private fun getDash(position: Int): String {
+        val result = StringBuilder()
+        repeat(position) {
+            result.append("-")
+        }
+        return result.toString()
+    }
+}
