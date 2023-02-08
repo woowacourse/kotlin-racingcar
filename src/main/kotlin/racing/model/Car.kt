f138aedf4faea0d998f34500fc0044183abafd38
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-08 13:40:11 +0900

    feat: 랜덤 숫자를 받아 전진 조건을 판별하는 메소드 구현

--- src/main/kotlin/racing/model/Car.kt
+++ src/main/kotlin/racing/model/Car.kt
@@ -1,11 +1,19 @@
 package racing.model
 
+import racing.util.NumberGenerator
+
 class Car(private val name: String) {
     private var position = 0
-
+    private val numberGenerator = NumberGenerator()
     fun getPosition() = position
 
     fun move() {
         position++
+    }
+
+    fun checkMove(randomNumber: Int) = randomNumber >= CAN_MOVE_MINIMUM
+
+    companion object {
+        private const val CAN_MOVE_MINIMUM = 4
     }
 }
