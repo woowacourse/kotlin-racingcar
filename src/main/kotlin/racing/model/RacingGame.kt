241d90d25746d15b6c06961bcb620cfd00587818
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-08 16:15:09 +0900

    feat: 최종 우승자를 판별하는 기능 구현

--- src/main/kotlin/racing/model/RacingGame.kt
+++ src/main/kotlin/racing/model/RacingGame.kt
@@ -1,10 +1,27 @@
 package racing.model
 
-class RacingGame(private val carNames: List<String>, private val maxMoveCount: Int) {
-    fun racingStart() {
+class RacingGame {
+    private val cars = mutableListOf<Car>()
 
+    fun getCars() = cars.toList()
+
+    fun racingStart(cars: List<Car>, maxMoveCount: Int) {
+        this.cars.addAll(cars)
+        move(cars)
     }
 
-    fun move() {
+    fun getWinners(): List<String> {
+        cars.sortBy { it.getPosition() }
+        val result = mutableListOf<String>()
+        for (car in cars) {
+            if (car.getPosition() == cars.last().getPosition()) result.add(car.getName())
+        }
+        return result.toList()
+    }
+
+    private fun move(cars: List<Car>) {
+        for (car in cars) {
+            car.move()
+        }
     }
 }
