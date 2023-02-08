d147186bc20d2012e6d1ccca762213da0cbe9a46
Hyemin Song <hyemdooly@gmail.com>
2023-02-08 13:48:00 +0900

    test(CarTest): Car 테스트 클래스 생성

---
+++ src/test/kotlin/CarTest.kt
@@ -0,0 +1,23 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CarTest {
+
+    @Test
+    fun getInfoTest() {
+        assertThat(Car("dool", 0).getInfo()).isEqualTo(Pair("dool", 0))
+        assertThrows<IllegalArgumentException> {
+            assertThat(Car("dooly", 0).getInfo()).isEqualTo(Pair("dooly", 0))
+        }
+    }
+
+    @Test
+    fun moveTest() {
+        val car = Car("dool", 0)
+        car.move(3)
+        assertThat(car.getInfo().second).isEqualTo(0)
+        car.move(4)
+        assertThat(car.getInfo().second).isEqualTo(1)
+    }
+}
