824d3c0c940a77c435a0572659de8247d954e8c0
inseonyun <yuninseon@naver.com>
2023-02-08 14:32:26 +0900

    feat: 라운드 결과 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -6,4 +6,5 @@ class OutputView {
 
     fun printGameStart() = println(PrintMessage.INPUT_CAR_NAMES_MESSAGE)
     fun printTryCount() = println(PrintMessage.INPUT_TRY_COUNT_MESSAGE)
+    fun printRoundResult(roundResult: String) = println(roundResult)
 }
