7534cbe4b593cb0deae8ed7ae8cc9d97e5f50473
Hyemin Song <hyemdooly@gmail.com>
2023-02-08 17:11:09 +0900

    refactor(InputView): inputCarNames 내용 수정

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,12 +1,6 @@
 class InputView {
-    fun inputCarNames(): List<Car> {
-        val input = readLine()
-        val names = input!!.split(",").toList()
-        val cars = mutableListOf<Car>()
-        for (name in names) {
-            cars.add(Car(name, 0))
-        }
-        return cars.toList()
+    fun inputCarNames(): String? {
+        return readLine()
     }
 
     fun inputTryNumber(): String? {
