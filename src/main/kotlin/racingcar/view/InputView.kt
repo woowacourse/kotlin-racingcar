9dcb78c53403a8198c2c24c74afddd0a7d225491
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-08 17:59:57 +0900

    refactor (inputView) : 길이 제한 기능 중복으로 삭제

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,19 +1,12 @@
 package racingcar.view
 
-import racingcar.resources.ERROR_INPUT_NAME_AT_MOST_FIVE
 import racingcar.resources.INPUT_CAR_NAMES
 import racingcar.resources.INPUT_COUNT
 
 class InputView {
     fun inputCarNames(): List<String> {
         println(INPUT_CAR_NAMES)
-        val userInput = readLine()?.split(',')?.map { checkCarNameLength(it) }
-        return userInput ?: listOf("")
-    }
-
-    private fun checkCarNameLength(carName: String): String {
-        require(carName.length in 1..5) { println(ERROR_INPUT_NAME_AT_MOST_FIVE) }
-        return carName
+        return readLine()?.split(',') ?: listOf("")
     }
 
     fun inputRacingCount(): Int {
