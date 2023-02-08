ab9057b50814ecdb065a0c837d7c6d91c864d8a4
briandr97 <briandr@naver.com>
2023-02-08 14:52:48 +0900

    feat(InputView): readLine으로 null 값이 들어왔을 때에 대한 에러 처리

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,13 +1,14 @@
 package view
 
 private const val SEPARATOR = ","
+private const val INPUT_NULL = "[ERROR] null 값이 들어왔습니다."
 
 class InputView {
-    fun enterCarName(): List<String>? {
-        return readLine()?.split(SEPARATOR)
+    fun enterCarName(): List<String> {
+        return (readLine() ?: throw IllegalArgumentException(INPUT_NULL)).split(SEPARATOR)
     }
 
-    fun enterCount(): Int? {
-        return readLine()?.toInt()
+    fun enterTryCount(): Int {
+        return (readLine() ?: throw IllegalArgumentException(INPUT_NULL)).toInt()
     }
 }
