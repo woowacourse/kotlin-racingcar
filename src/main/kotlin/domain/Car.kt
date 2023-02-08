330486bf5dcea914c419ad3151a8901267464d99
briandr97 <briandr@naver.com>
2023-02-08 16:33:00 +0900

    feat(Car): 이름과 이동거리를 갖고 있는 Car 클래스 구현

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,3 +1,11 @@
 package domain
 
-class Car
+class Car(val carName: String, private val driver: Driver) {
+    private var _distance = ""
+    val distance get() = _distance
+
+    fun decideCarMovement() {
+        val decision = driver.decideMovement()
+        _distance += decision.distance
+    }
+}
