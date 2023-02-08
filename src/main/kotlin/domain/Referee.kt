1b88ba71fe3b62ba51a482b13d064961ae8f0bf1
briandr97 <briandr@naver.com>
2023-02-08 16:17:29 +0900

    feat(Referee): 우승자를 판단해줄 Referee 클래스 구현

--- src/main/kotlin/domain/Referee.kt
+++ src/main/kotlin/domain/Referee.kt
@@ -1,3 +1,23 @@
 package domain
 
-class Referee
+object Referee {
+
+    fun judgeWinner(cars: List<Car>): List<String> {
+        val winnersContainer = mutableListOf<String>()
+
+        val maxDistance = cars.maxOf { it.distance.length }
+        findWinners(winnersContainer, maxDistance, cars)
+
+        return winnersContainer
+    }
+
+    private fun findWinners(winnersContainer: MutableList<String>, maxDistance: Int, cars: List<Car>) {
+        cars.forEach {
+            compareDistanceWithMax(winnersContainer, maxDistance, it)
+        }
+    }
+
+    private fun compareDistanceWithMax(winnersContainer: MutableList<String>, maxDistance: Int, car: Car) {
+        if (car.distance.length == maxDistance) winnersContainer.add(car.carName)
+    }
+}
