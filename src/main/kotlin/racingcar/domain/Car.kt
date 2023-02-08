03ca99e4984ae21cb723c80fd890c8f0b84becc7
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-08 17:57:06 +0900

    feat (Car) : car의 이름 길이 제한 기능 추가

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,11 +1,15 @@
 package racingcar.domain
 
 import racingcar.resources.ERROR_INPUT_COUNT
+import racingcar.resources.ERROR_NAME_LENGTH_FROM_ONE_TO_FIVE
 
 class Car(val name: String) {
     var location = 0
         private set
 
+    init {
+        require(name.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH) { ERROR_NAME_LENGTH_FROM_ONE_TO_FIVE }
+    }
     fun plusLocation(num: Int) {
         require(num in MIN_NUMBER..MAX_NUMBER) { ERROR_INPUT_COUNT }
         if (num >= STANDARD_NUMBER) {
@@ -14,6 +18,8 @@ class Car(val name: String) {
     }
 
     companion object {
+        const val MIN_NAME_LENGTH = 1
+        const val MAX_NAME_LENGTH = 5
         const val MIN_NUMBER = 0
         const val MAX_NUMBER = 9
         const val STANDARD_NUMBER = 4
