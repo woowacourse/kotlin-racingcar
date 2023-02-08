2f4e6266ce15adef14a5d7bf681db67e65954dfa
EmilyCh0 <emily9803@ewhain.net>
2023-02-08 17:43:16 +0900

    refactor: 입력 반복 로직 분리

--- src/main/kotlin/utils/RepeatInputProcess.kt
+++ src/main/kotlin/utils/RepeatInputProcess.kt
@@ -3,11 +3,15 @@ package utils
 object RepeatInputProcess {
     fun repeat(inputProcess: () -> Any): Any {
         while (true) {
-            try {
-                return inputProcess()
-            } catch (e: IllegalArgumentException) {
-                println(e.message.toString())
-            }
+            return handleError(inputProcess)
+        }
+    }
+
+    private fun handleError(inputProcess: () -> Any): Any {
+        return try {
+            inputProcess()
+        } catch (e: IllegalArgumentException) {
+            println(e.message.toString())
         }
     }
 }
