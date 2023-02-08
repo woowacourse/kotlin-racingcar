e33942b146dd4e206fa75d16ff8810d4968813b3
changhwan <chang5hustle@gmail.com>
2023-02-08 14:34:21 +0900

    feat(racingcar): 자동차 객체별 전진정도 비교 기능 함수 작성

--- src/main/kotlin/racingcar/domain/RacingCar.kt
+++ src/main/kotlin/racingcar/domain/RacingCar.kt
@@ -3,8 +3,12 @@ package racingcar.domain
 class RacingCar(
     private val name: String,
     private var movingState: Int = 0,
-) {
+) : Comparable<RacingCar> {
     fun moveForward() {
         movingState += 1
+    }
+
+    override fun compareTo(other: RacingCar): Int {
+        return if (this.movingState >= other.movingState) 1 else -1
     }
 }
