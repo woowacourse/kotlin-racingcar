5c8a310c11271eb250413e7395db9d7fe2fa3c4f
inseonyun <yuninseon@naver.com>
2023-02-08 15:04:02 +0900

    feat: 자동차 이름 쉼표를 기준으로 스플릿하는 함수 구현

--- src/main/kotlin/service/RacingCarGameService.kt
+++ src/main/kotlin/service/RacingCarGameService.kt
@@ -2,4 +2,6 @@ package service
 
 class RacingCarGameService {
     fun isPossibleMove(): Boolean = RandomNumberGenerator().generate() >= 4
+
+    fun splitCarNames(carNames: String): List<String> = carNames.split(",")
 }
