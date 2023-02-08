f906b2c4d968fad913c4cf22b36d55e962718909
briandr97 <briandr@naver.com>
2023-02-08 13:07:29 +0900

    feat(OutputView): 함수, 변수명 변경

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,7 +1,7 @@
 package view
 
 private const val ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
-private const val ENTER_COUNT = "시도할 횟수는 몇 회인가요?"
+private const val ENTER_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
 private const val RESULT = "실행 결과"
 private const val COLON = " : "
 private const val WINNER = "최종 우승자: "
@@ -12,8 +12,8 @@ class OutputView {
         println(ENTER_CAR_NAME)
     }
 
-    fun printEnterCount() {
-        println(ENTER_COUNT)
+    fun printEnterTryCount() {
+        println(ENTER_TRY_COUNT)
     }
 
     fun printResult(result: Map<String, String>) {
