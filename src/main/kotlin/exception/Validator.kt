0818a3eee74d9cacdd4b669fb5d442e322937842
EmilyCh0 <emily9803@ewhain.net>
2023-02-08 14:26:38 +0900

    refactor: 이름 공백 예외 처리

--- src/main/kotlin/exception/Validator.kt
+++ src/main/kotlin/exception/Validator.kt
@@ -2,7 +2,7 @@ package exception
 
 class Validator {
     fun checkCarName(input: String): List<String> {
-        val names = input.split(",")
+        val names = input.split(",").map { it.trim() }
         for (name in names) {
             if (name.length > 5) {
                 throw IllegalArgumentException(NAME_LENGTH_ERROR)
