b5ee01ee85b206e575a755e1558a1ed143461cfc
changhwan <chang5hustle@gmail.com>
2023-02-08 13:11:38 +0900

    refactor(racingcar): 코드 간결화

--- src/main/kotlin/racingcar/domain/Validator.kt
+++ src/main/kotlin/racingcar/domain/Validator.kt
@@ -5,9 +5,6 @@ import racingcar.constant.MAX_NAME_LENGTH
 object Validator {
     fun isNameLengthInRange(value: String): Boolean {
         val length = value.length
-        if (length <= 0 || length > MAX_NAME_LENGTH) {
-            return false
-        }
-        return true
+        return !(length <= 0 || length > MAX_NAME_LENGTH)
     }
 }
