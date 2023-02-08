ec612ffc3471c26d296ebc2806d646c5d1759785
Doyeop Kim <dygames@naver.com>
2023-02-08 14:01:32 +0900

    feat(WorldExceptions): World Step에 대한 검증

--- src/main/kotlin/racingcar/exceptions/WorldExceptions.kt
+++ src/main/kotlin/racingcar/exceptions/WorldExceptions.kt
@@ -10,4 +10,9 @@ class WorldExceptions {
     fun validateAttemptCount(attemptCount: Int) {
         require(attemptCount > 0) { "1번 이상 시도해아 합니다." }
     }
+
+    fun validateProcessStep(car: Car, number: Int) {
+        checkNotNull(car) { "자동차 객체는 null이 아니어야 합니다." }
+        require(number in 0..9) { "생성된 임의 숫자는 0에서 9사이어야 합니다." }
+    }
 }
