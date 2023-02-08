91a38b01ebae4483a52b309c17d17252e79260c0
Hyemin Song <hyemdooly@gmail.com>
2023-02-08 16:19:14 +0900

    fix(ValidatorTest): Validator 함수 예외 테스트 추가 및 수정

--- src/test/kotlin/ValidatorTest.kt
+++ src/test/kotlin/ValidatorTest.kt
@@ -1,16 +1,39 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class ValidatorTest {
 
-    val validator = Validator()
+    private val validator = Validator()
+
+    @Test
+    fun checkNameTest() {
+        assertDoesNotThrow {
+            validator.checkName("dool")
+        }
+    }
 
     @ValueSource(strings = ["", "abcdef"])
     @ParameterizedTest
-    fun checkNameTest(input: String) {
+    fun checkNameExceptionTest(input: String) {
         assertThrows<IllegalArgumentException> {
             validator.checkName(input)
+        }
+    }
+
+    @Test
+    fun checkTryNumberTest() {
+        assertDoesNotThrow {
+            validator.checkTryNumber("123")
+        }
+    }
+    @ValueSource(strings = ["", "()*"])
+    @ParameterizedTest
+    fun checkTryNumberExceptionTest(input: String) {
+        assertThrows<IllegalArgumentException> {
+            validator.checkTryNumber(input)
         }
     }
 }
