a10a22bf33aa59b3767f7c0f305a44402d3d5b9e
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-08 13:45:35 +0900

    test (car) : 입력 받은 숫자에 따라 한칸 이동하도록 기능 구현 테스트

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -2,19 +2,41 @@ package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 internal class CarTest {
     @Test
     fun `Car 객체 생성이 잘 되는지 확인`() {
-        var car = Car("hihi")
+        val car = Car("hihi")
         assertThat(car.name).isEqualTo("hihi")
         assertThat(car.location).isEqualTo(0)
     }
 
-    @Test
-    fun `Car 위치가 잘 이동되는지 확인`() {
-        var car = Car("hihi")
-        car.plusLocation()
+    @ParameterizedTest(name = "숫자가 4 이하인 경우 이동하지 않는지 확인. {argumentsWithNames}")
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `숫자가 4 이하인 경우 이동하지 않는지 확인`(input: Int) {
+        val car = Car("hihi")
+        car.plusLocation(input)
+        assertThat(car.location).isEqualTo(0)
+    }
+
+    @ParameterizedTest(name = "숫자가 4 이하인 경우 이동하는지 확인. {argumentsWithNames}")
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `숫자가 4 이상인 경우 이동하는지 확인`(input: Int) {
+        val car = Car("hihi")
+        car.plusLocation(input)
         assertThat(car.location).isEqualTo(1)
+    }
+
+    @ParameterizedTest(name = "잘못된 입력일 경우 에러 메시지 발생 {argumentsWithNames}")
+    @ValueSource(ints = [-1, 10])
+    fun `잘못된 입력일 경우`(input: Int) {
+        val car = Car("hihi")
+        assertThrows<IllegalArgumentException> {
+            car.plusLocation(input)
+        }
     }
 }
