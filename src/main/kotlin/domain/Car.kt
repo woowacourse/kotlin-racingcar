919f2bd96fb4eadc1821cc1265f8c07005416ff9
briandr97 <briandr@naver.com>
2023-02-08 17:27:51 +0900

    feat(Car): Create some blocks
    1. Create Except method in init block
    2. Create companion object for hard coding

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,11 +1,23 @@
 package domain
 
 class Car(val carName: String, private val driver: Driver) {
+
     private var _distance = ""
     val distance get() = _distance
+
+    init {
+        require(carName.length in RANGE) { NAME_CONVENTION_ERROR_MESSAGE }
+    }
 
     fun decideCarMovement() {
         val decision = driver.decideMovement()
         _distance += decision.distance
+    }
+
+    companion object {
+        private const val NAME_CONVENTION_ERROR_MESSAGE = "[ERROR] 이름은 1~5글자여야합니다."
+        private const val NAME_LENGTH_LOWER_INCLUSIVE = 1
+        private const val NAME_LENGTH_UPPER_INCLUSIVE = 5
+        val RANGE = NAME_LENGTH_LOWER_INCLUSIVE..NAME_LENGTH_UPPER_INCLUSIVE
     }
 }
