495eed36f2a59872700e4484f9f0128a28d141f3
Gloria <ippnsj@gmail.com>
2023-02-08 17:09:57 +0900

    feat: 경주 시도 횟수를 입력받는 기능 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,6 +1,7 @@
 package view
 
 import dto.CarNamesDTO
+import dto.RaceTimeDTO
 
 class InputView {
     fun readCarNames(): CarNamesDTO {
@@ -9,11 +10,18 @@ class InputView {
         return CarNamesDTO(input)
     }
 
+    fun readRaceTime(): RaceTimeDTO {
+        println(GUIDE_RACE_TIME)
+        val input = readUser()
+        return RaceTimeDTO(input)
+    }
+
     private fun readUser(): String {
         return readln()
     }
 
     companion object {
         const val GUIDE_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+        const val GUIDE_RACE_TIME = "시도할 횟수는 몇 회인가요?"
     }
 }
