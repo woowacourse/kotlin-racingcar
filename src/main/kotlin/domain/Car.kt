b165c827557c11ebc7a4b9a9dca8cf2287104690
Gloria <ippnsj@gmail.com>
2023-02-08 17:46:43 +0900

    feat: 자동차 이동 기능 구현
    0 ~ 3이면 stop
    4 ~ 9면 go

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,3 +1,28 @@
 package domain
 
-class Car
+class Car(
+    private val generator: NumberGenerator,
+    private val name: String
+) {
+    private var distance = 0
+
+    fun race() {
+        val number = generator.generate()
+        if (checkGo(number)) go()
+    }
+
+    private fun checkGo(number: Int): Boolean {
+        if (number in RANGE_LOWER_INCLUSIVE..RANGE_UPPER_INCLUSIVE)
+            return true
+        return false
+    }
+
+    private fun go() {
+        distance++
+    }
+
+    companion object {
+        const val RANGE_LOWER_INCLUSIVE = 4
+        const val RANGE_UPPER_INCLUSIVE = 9
+    }
+}
