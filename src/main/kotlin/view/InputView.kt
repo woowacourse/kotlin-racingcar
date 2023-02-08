26f1e17a7d41ec2f6300d4c912dd93c8c39e7caf
Gloria <ippnsj@gmail.com>
2023-02-08 16:17:14 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,3 +1,19 @@
 package view
 
-class InputView
+import dto.CarNamesDTO
+
+class InputView {
+    fun readCarNames(): CarNamesDTO {
+        println(GUIDE_CAR_NAME)
+        val input = readUser()
+        return CarNamesDTO(input)
+    }
+
+    private fun readUser(): String {
+        return readln()
+    }
+
+    companion object {
+        const val GUIDE_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    }
+}
