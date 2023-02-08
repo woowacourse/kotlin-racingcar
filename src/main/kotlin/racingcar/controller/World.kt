f959f5433b878009d5cca403c17e76249d3b318c
Yerin <myheaven_b@naver.com>
2023-02-08 17:00:10 +0900

    refactor(World, RacingManager, RacingManagerException): 컴트롤러 내부의 로직을 racingManager로 옮김

--- src/main/kotlin/racingcar/controller/World.kt
+++ src/main/kotlin/racingcar/controller/World.kt
@@ -1,59 +1,44 @@
 package racingcar.controller
 
 import racingcar.Util
-import racingcar.exceptions.WorldExceptions
-import racingcar.model.Car
 import racingcar.model.RacingManager
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
 class World {
-    val cars = mutableListOf<Car>()
-    var attemptCount = 0
     private val outputView = OutputView()
     private val inputView = InputView()
-    private val exceptions = WorldExceptions()
     private val racingManager = RacingManager()
 
     fun init() {
-        initCar()
+        initCars()
         initAttemptCount()
     }
 
-    private fun initCar() {
+    private fun initCars() {
         Util.tryAndRerun {
             outputView.printMessage(OutputView.MSG_INPUT_CAR_NAME)
             val names = inputView.carNames()
-            for (name in names) {
-                cars.add(Car(name))
-            }
-            exceptions.validateCarCount(cars)
+            racingManager.initCars(names)
         }
     }
 
     private fun initAttemptCount() {
         Util.tryAndRerun {
             outputView.printMessage(OutputView.MSG_INPUT_ATTEMPT_COUNT)
-            attemptCount = inputView.attemptCount()
-            exceptions.validateAttemptCount(attemptCount)
+            val attemptCount = inputView.attemptCount()
+            racingManager.setAttemptCount(attemptCount)
         }
     }
 
     fun run() {
-        for (i in 0 until attemptCount) {
-            attempt()
-        }
-    }
-
-    private fun attempt() {
-        for (i in 0 until cars.size) {
-            racingManager.processStep(cars[i], Util.generateRandom())
-        }
         outputView.printMessage(OutputView.MSG_STEP_RESULT)
-        outputView.stepResult(cars)
+        racingManager.run {
+            outputView.stepResult(it)
+        }
     }
 
     fun quit() {
-        outputView.winner(racingManager.determineWinner(cars))
+        outputView.winner(racingManager.determineWinner())
     }
 }
--- src/main/kotlin/racingcar/model/RacingManager.kt
+++ src/main/kotlin/racingcar/model/RacingManager.kt
@@ -1,13 +1,41 @@
 package racingcar.model
 
+import racingcar.Util
 import racingcar.exceptions.RacingManagerExceptions
 
 class RacingManager {
     private val exceptions = RacingManagerExceptions()
+    private val cars = mutableListOf<Car>()
+    private var attemptCount = 0
 
-    fun determineWinner(cars: List<Car>): List<Car> {
+    fun initCars(names: List<String>) {
+        for (name in names) {
+            cars.add(Car(name))
+        }
+        exceptions.validateCarCount(cars)
+    }
+
+    fun setAttemptCount(attemptCount: Int) {
+        this.attemptCount = attemptCount
+        exceptions.validateAttemptCount(this.attemptCount)
+    }
+
+    fun determineWinner(): List<Car> {
         val sortedCars = cars.sortedWith { car, car2 -> if (car.compareTo(car2)) -1 else 1 }
         return sortedCars.filter { it.compareTo(sortedCars[0]) }
+    }
+
+    fun run(output: (car: List<Car>) -> Unit) {
+        for (i in 0 until attemptCount) {
+            attempt(output)
+        }
+    }
+
+    private fun attempt(output: (car: List<Car>) -> Unit) {
+        for (i in 0 until cars.size) {
+            processStep(cars[i], Util.generateRandom())
+        }
+        output(cars)
     }
 
     fun processStep(car: Car, number: Int) {
