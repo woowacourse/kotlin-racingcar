a03fce77e4a25f15cad20f528952963d29d3c72a
changhwan <chang5hustle@gmail.com>
2023-02-08 14:42:40 +0900

    feat(racingcar): 객체별 현재 전진 정도 toString 기능

--- src/main/kotlin/racingcar/domain/RacingCar.kt
+++ src/main/kotlin/racingcar/domain/RacingCar.kt
@@ -1,5 +1,7 @@
 package racingcar.domain
 
+import racingcar.constant.STATE_UNIT
+
 class RacingCar(
     private val name: String,
     private var movingState: Int = 0,
@@ -7,6 +9,16 @@ class RacingCar(
     fun moveForward() {
         movingState += 1
     }
+
+    fun getStateProgress(): String {
+        var value = ""
+        for (i in 0 until movingState) {
+            value += STATE_UNIT
+        }
+        return value
+    }
+
+    override fun toString(): String = "$name : ${getStateProgress()}"
 
     override fun compareTo(other: RacingCar): Int {
         return if (this.movingState >= other.movingState) 1 else -1
