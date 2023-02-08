5bfe8a772a1a0ddccc2e2c16ddff8e21e2c01e75
EmilyCh0 <emily9803@ewhain.net>
2023-02-08 17:27:32 +0900

    test: 시도 횟수 예외 처리 테스트

--- src/test/kotlin/exception/ValidatorTest.kt
+++ src/test/kotlin/exception/ValidatorTest.kt
@@ -24,6 +24,18 @@ class ValidatorTest {
     }
 
     @Test
-    fun checkRoundCount() {
+    fun `시도 횟수가 숫자가 아닌 경우 예외가 발생한다`() {
+        val input = "100a"
+        assertThrows<IllegalArgumentException> {
+            validator.checkRoundCount(input)
+        }
+    }
+
+    @Test
+    fun `시도 횟수가 음수인 경우 예외가 발생한다`() {
+        val input = "-5"
+        assertThrows<IllegalArgumentException> {
+            validator.checkRoundCount(input)
+        }
     }
 }
