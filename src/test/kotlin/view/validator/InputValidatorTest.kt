8855157c317e408850917f3bce643dd8231bcd4d
mendel <rhthrhrl0@naver.com>
2023-02-08 16:01:20 +0900

    test(view): 자동차 이름 입력에 대해 실패시 검증을 수행하는 테스트 코드 추가

--- src/test/kotlin/view/validator/InputValidatorTest.kt
+++ src/test/kotlin/view/validator/InputValidatorTest.kt
@@ -1,7 +1,10 @@
 package view.validator
 
 import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputValidatorTest {
 
@@ -10,5 +13,11 @@ class InputValidatorTest {
         assertThatCode {
             InputValidator.validateCarNames("wooah,토마스,멘델")
         }.doesNotThrowAnyException()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["men2e,토마스", "mendel,hi", "hi"])
+    fun `자동차 이름 입력 실패 테스트`(names: String) {
+        assertThatIllegalArgumentException().isThrownBy { InputValidator.validateCarNames(names) }
     }
 }
