247babf1b867a9eff25872525e73d12032cc8d93
Yerin <myheaven_b@naver.com>
2023-02-08 17:30:16 +0900

    refactor(InputViewExceptions): 예외 사항 추가

--- src/main/kotlin/racingcar/exceptions/InputViewExceptions.kt
+++ src/main/kotlin/racingcar/exceptions/InputViewExceptions.kt
@@ -3,6 +3,7 @@ package racingcar.exceptions
 class InputViewExceptions {
     fun validateNames(names: List<String>) {
         require(names.isNotEmpty()) { "경주를 시작하려면 차 이름을 입력해주세요" }
+        require(names.all { it.isNotEmpty() }) { "차 이름은 1글자 이상이어야 합니다." }
         require(names.all { it.length <= 5 }) { "차 이름은 5글자 이하여야 합니다." }
     }
 
