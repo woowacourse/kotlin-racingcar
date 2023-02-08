44cd1a59825083a8933509983158865b0f5cea7e
inseonyun <yuninseon@naver.com>
2023-02-08 14:33:25 +0900

    feat: 최종 우승자 출력 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -7,4 +7,5 @@ class OutputView {
     fun printGameStart() = println(PrintMessage.INPUT_CAR_NAMES_MESSAGE)
     fun printTryCount() = println(PrintMessage.INPUT_TRY_COUNT_MESSAGE)
     fun printRoundResult(roundResult: String) = println(roundResult)
+    fun printWinner(winner: String) = println(winner)
 }
