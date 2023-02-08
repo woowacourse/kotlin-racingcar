af69c419f43685c9fcaf4458a29bd7451f37178e
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-08 15:44:19 +0900

    refactor: numberGenerator를 필드에서 companion object로 이동

--- src/main/kotlin/racing/model/Car.kt
+++ src/main/kotlin/racing/model/Car.kt
@@ -4,16 +4,17 @@ import racing.util.NumberGenerator
 
 class Car(private val name: String) {
     private var position = 0
-    private val numberGenerator = NumberGenerator()
+
     fun getPosition() = position
 
     fun move() {
         position++
     }
 
-    fun checkMove(randomNumber: Int) = randomNumber >= CAN_MOVE_MINIMUM
+    private fun checkMove(randomNumber: Int) = randomNumber >= CAN_MOVE_MINIMUM
 
     companion object {
+        private val numberGenerator = NumberGenerator()
         private const val CAN_MOVE_MINIMUM = 4
     }
 }
