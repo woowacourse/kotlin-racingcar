b4482bfb472e75f0f6db46c265dc159d930f8ad4
mendel <rhthrhrl0@naver.com>
2023-02-09 13:38:05 +0900

    test(domain): 움직일 수 있을 때 차가 전진하는지 검증하는 테스트 코드 작성

--- src/test/kotlin/domain/CarRacingGameManagerTest.kt
+++ src/test/kotlin/domain/CarRacingGameManagerTest.kt
@@ -1,11 +1,33 @@
 package domain
 
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class CarRacingGameManagerTest {
+
+    @Nested
+    inner class `allCarsTryToMoveForward 메소드는` {
+        @Nested
+        inner class `첫 번째와 세 번째 차만 전진을 한다면` {
+            private val cars: List<Car> = listOf(Car("pobi"), Car("mendl"), Car("thom"))
+            private val gameManager = CarRacingGameManager(cars, FakeMovingStrategy())
+
+            @Test
+            fun `첫 번째 차와 세 번째 차의 전진 횟수만 1이 된다`() {
+                gameManager.allCarsTryToMoveForward()
+                val result = cars.map { it.advanceCount }
+                val expected = listOf(1, 0, 1)
+                assertThat(result).isEqualTo(expected)
+            }
+
+            inner class FakeMovingStrategy : MovingStrategy {
+                private val pattern = listOf(true, false, true)
+                private var i = 0
+                override fun isMovable(): Boolean = pattern[i++]
+            }
+        }
+    }
 
     @Nested
     inner class `getWinCars 메소드는` {
@@ -19,7 +41,7 @@ internal class CarRacingGameManagerTest {
             fun `전진 횟수가 가장 많은 자동차들을 반환한다`() {
                 val winCars = gameManager.getWinCars()
 
-                Assertions.assertThat(winCars).containsOnly(cars[2])
+                assertThat(winCars).containsOnly(cars[2])
             }
 
             private fun getCars(): List<Car> {
