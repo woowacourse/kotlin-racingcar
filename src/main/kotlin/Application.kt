d2a8f1dcf1a9877cc2809ff27be0895e380c8c2b
Gloria <ippnsj@gmail.com>
2023-02-09 13:51:03 +0900

    feat: 자동차 경주 구현

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,2 +1,10 @@
+import controller.Controller
+import view.InputView
+import view.OutputView
+
 fun main() {
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    Controller(inputView, outputView).run()
 }
