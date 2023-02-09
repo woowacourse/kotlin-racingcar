4400af439c537a9eda188877e1f0b05502435276
ki960213 <ki960213@github.com>
2023-02-09 11:23:29 +0900

    feat(view & domain): 최종 우승 자동차를 계산 후 정해진 형식에 따라 출력하고 최종 결과 출력하는 기능 구현

--- src/main/kotlin/domain/CarRacingGameManager.kt
+++ src/main/kotlin/domain/CarRacingGameManager.kt
@@ -6,4 +6,9 @@ class CarRacingGameManager(
 ) {
     fun allCarsTryToMoveForward() =
         cars.forEach { if (movingStrategy.isMovable()) it.move() }
+
+    fun getWinCars(): List<Car> {
+        val maxAdvancedCount: Int = cars.maxOf { it.advanceCount }
+        return cars.filter { it.advanceCount == maxAdvancedCount }
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,27 @@
+import common.ADVANCE_COUNT_REQUEST_MESSAGE
 import common.CAR_NAMES_REQUEST_MESSAGE
+import common.GAME_RESULT_HEADER
+import domain.Car
+import domain.CarRacingGameManager
+import domain.RandomMovingStrategy
+import view.InputView
 import view.OutputView
 
 fun main() {
     OutputView.printMessage(CAR_NAMES_REQUEST_MESSAGE)
+    val carNames: List<String> = InputView.readCarNames()
+    val cars = carNames.map { Car(it) }
+
+    OutputView.printMessage(ADVANCE_COUNT_REQUEST_MESSAGE)
+    val advanceCount: Int = InputView.readAdvanceCount()
+
+    OutputView.printMessage(GAME_RESULT_HEADER)
+    val racingGameManager = CarRacingGameManager(cars, RandomMovingStrategy())
+    repeat(advanceCount) {
+        racingGameManager.allCarsTryToMoveForward()
+        OutputView.printGameStatus(cars)
+    }
+
+    val winCars: List<Car> = racingGameManager.getWinCars()
+    OutputView.printGameResult(winCars)
 }
