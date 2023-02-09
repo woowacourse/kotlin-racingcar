3cebf766c5ed4a618d758f329f94b4be75e96f73
briandr97 <briandr@naver.com>
2023-02-09 14:55:40 +0900

    Refactor(Car): Release private of value NAME_CONVENTION_ERROR_MESSAGE

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -15,7 +15,7 @@ class Car(val carName: String, private val driver: Driver) {
     }
 
     companion object {
-        private const val NAME_CONVENTION_ERROR_MESSAGE = "[ERROR] 이름은 1~5글자여야합니다."
+        const val NAME_CONVENTION_ERROR_MESSAGE = "[ERROR] 이름은 1~5글자여야합니다."
         private const val NAME_LENGTH_LOWER_INCLUSIVE = 1
         private const val NAME_LENGTH_UPPER_INCLUSIVE = 5
         val RANGE = NAME_LENGTH_LOWER_INCLUSIVE..NAME_LENGTH_UPPER_INCLUSIVE
