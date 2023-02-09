dafae3adf4e7aeb4979d24bf80fed0714ba785b0
Hyemin Song <hyemdooly@gmail.com>
2023-02-09 15:38:58 +0900

    refactor(InputView): nullable 삭제 및 대처

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,11 +1,11 @@
 package view
 
 class InputView {
-    fun inputCarNames(): String? {
-        return readLine()
+    fun inputCarNames(): String {
+        return readLine() ?: ""
     }
 
-    fun inputTryNumber(): String? {
-        return readLine()
+    fun inputTryNumber(): String {
+        return readLine() ?: ""
     }
 }
