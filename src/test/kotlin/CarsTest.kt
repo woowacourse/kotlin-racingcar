bca6c3612db60210d0daa2761d863dfb031c8f09
Hyemin Song <hyemdooly@gmail.com>
2023-02-09 15:38:30 +0900

    refactor(CarsTest): Cars의 getCar 삭제로 인한 수정

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -4,24 +4,17 @@ import org.junit.jupiter.api.Test
 
 class CarsTest {
 
-    val cars = Cars("pobi,dool,woni")
+    private val cars = Cars("pobi,dool,woni")
     @Test
     fun mappingCarsTest() {
         val carNames = listOf("pobi", "dool", "woni")
         for (i in 0 until cars.getCarSize()) {
-            assertThat(cars.getCar(i).getInfo().first).isEqualTo(carNames[i])
+            assertThat(cars.getCarInfo(i).first).isEqualTo(carNames[i])
         }
     }
 
     @Test
     fun getCarSizeTest() {
         assertThat(cars.getCarSize()).isEqualTo(3)
-    }
-
-    @Test
-    fun findWinners() {
-        cars.getCar(0).move(4)
-        val winners = listOf("pobi")
-        assertThat(cars.findWinners()).isEqualTo(winners)
     }
 }
