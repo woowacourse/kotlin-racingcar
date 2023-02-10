e88bcc9aae7749a9afaaa773b369184387ae6eba
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-10 15:34:32 +0900

    refactor : 경로 출력중 실행결과라는 말을 한번만 출력하기 위해서 시도 횟수를 함수의 인자로 받기

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,25 +1,32 @@
 package view
 
-import data.CarPath
-import data.CarPath.Companion.subPath
-import data.PathState.Companion.getPathMarks
+import data.Car
 
 object OutputView {
 
     private const val RESULT = "실행 결과\n"
     private const val FINAL_WINNER = "최종 우승자: "
-    private const val TOKENIZER = ", "
     private const val CAR_PATH = "%s : %s"
+    private const val DASH = "-"
+    const val TOKENIZER = ","
+    const val EMPTY = ""
 
-    fun printResult(carsPath: List<CarPath>, numberOfTry: Int) {
-        carsPath.forEach { carPath ->
-            println(CAR_PATH.format(carPath.carName, carPath.subPath(numberOfTry).getPathMarks()))
-        }
-        println()
+    fun printErrorMsg(msg: String) {
+        println(msg)
     }
 
     fun printWinner(winners: List<String>) {
         println(FINAL_WINNER)
-        println(winners.joinToString(TOKENIZER))
+        println(winners.joinToString("$TOKENIZER "))
+    }
+
+    fun printPath(count: Int, cars: List<Car>) {
+        if (count == 0) {
+            println(RESULT)
+        }
+
+        cars.forEach { car ->
+            println(CAR_PATH.format(car.name, DASH.repeat(car.position)))
+        }
     }
 }
