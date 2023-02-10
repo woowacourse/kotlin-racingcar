3a81f78b6c1fc76c8f4d5ad015544da6741c5981
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-02-10 17:12:18 +0900

    <refactor> CarMovingTest 수정

--- src/test/kotlin/CarMovingTest.kt
+++ src/test/kotlin/CarMovingTest.kt
@@ -1,60 +1,30 @@
+import data.Car
+import data.PathState
 import data.generator.NumberGenerator
-import org.junit.jupiter.api.BeforeEach
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 class CarMovingTest {
 
-    class MovingNumberGenerator : NumberGenerator {
-
-        override fun generate(): Int {
-            return (4..9).random()
-        }
+    @Test
+    fun `정지 테스트 `() {
+        val car = Car("우기")
+        val condition = object : NumberGenerator {
+            override fun generate(): Int {
+                return (0..3).random()
+            }
+        }.generate()
+        assertThat(car.move(condition)).isEqualTo(PathState.STOP)
     }
 
-    class StopNumberGenerator : NumberGenerator {
-
-        override fun generate(): Int {
-            return (0..3).random()
-        }
+    @Test
+    fun `전진 테스트 `() {
+        val car = Car("핑구")
+        val condition = object : NumberGenerator {
+            override fun generate(): Int {
+                return (4..9).random()
+            }
+        }.generate()
+        assertThat(car.move(condition)).isEqualTo(PathState.MOVE)
     }
-
-    private lateinit var stopNumberGenerator: StopNumberGenerator
-    private lateinit var movingNumberGenerator: MovingNumberGenerator
-
-    @BeforeEach
-    fun setUp() {
-        stopNumberGenerator = StopNumberGenerator()
-        movingNumberGenerator = MovingNumberGenerator()
-    }
-
-    // @ParameterizedTest
-    // @ValueSource(ints = [2, 3, 4, 5, 6, 7])
-    // fun `정지 테스트 `(numberOfTry: Int) {
-    //     val player = CarRacingGamePlayer(
-    //         car = Car("우기"),
-    //         numberOfTry = numberOfTry,
-    //         numberGenerator = stopNumberGenerator
-    //     )
-    //
-    //     assertThat(
-    //         player.moveCar().path.count { pathState ->
-    //             pathState == PathState.MOVE
-    //         }
-    //     ).isEqualTo(0)
-    // }
-    //
-    // @ParameterizedTest
-    // @ValueSource(ints = [2, 3, 4, 5, 6, 7])
-    // fun `전진 테스트`(numberOfTry: Int) {
-    //     val player = CarRacingGamePlayer(
-    //         car = Car("우기"),
-    //         numberOfTry = numberOfTry,
-    //         numberGenerator = movingNumberGenerator
-    //     )
-    //
-    //     assertThat(
-    //         player.moveCar().path.count { pathState ->
-    //             pathState == PathState.MOVE
-    //         }
-    //     ).isEqualTo(numberOfTry)
-    // }
 }
