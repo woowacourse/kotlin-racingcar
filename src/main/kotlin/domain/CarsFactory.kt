c9db29abd94982b10b71fce4683fb7fa7a33cc90
vrexpert <vrexpert2302@gmail.com>
2023-02-10 23:00:17 +0900

    refactor: CarsDTO 삭제 및 자동차들을 생성하는 공장 추가
    CarsDTO 는 불필요하므로 삭제
    자동차들을 생성하는 공장 CarsFactory 정의
    자동차 공장에서 자동차의 개수 및 이름 중복 검증

---
+++ src/main/kotlin/domain/CarsFactory.kt
@@ -0,0 +1,26 @@
+package domain
+
+class CarsFactory(private val names: List<String>) {
+
+    init {
+        validateCarCount()
+        validateCarUniqueness()
+    }
+
+    fun makeCars(numberGenerator: RandomNumberGenerator): List<Car> {
+        return names.map { name -> Car(numberGenerator, name, 0) }
+    }
+
+    private fun validateCarCount() {
+        require(names.size in 2..20) { ERROR_CAR_COUNT }
+    }
+
+    private fun validateCarUniqueness() {
+        require(names.size == names.toSet().size) { ERROR_CAR_UNIQUENESS }
+    }
+
+    companion object {
+        const val ERROR_CAR_COUNT = "자동차 개수는 2대 이상 20대 이하여야 합니다."
+        const val ERROR_CAR_UNIQUENESS = "자동차 이름은 중복되면 안 됩니다."
+    }
+}
