cf2744de9ea7f18d0d09ca4a073cc1bd6df26ca1
Hyemin Song <hyemdooly@gmail.com>
2023-02-10 16:15:39 +0900

    refactor(Constants): companion object -> object로 변경

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -1,16 +1,13 @@
 package util
 
-class Constants {
-
-    companion object {
-        const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
-        const val INPUT_TRY_NUMBER = "시도할 횟수는 몇 회인가요?"
-        const val OUTPUT_RESULT = "실행 결과"
-        const val OUTPUT_WINNER = "최종 우승자: "
-        const val INPUT_NAME_SIZE_ERROR_MESSAGE = "자동차 이름의 길이가 5를 초괴합니다."
-        const val INPUT_NAME_NULL_ERROR_MESSAGE = "자동차 이름이 입력되지 않았습니다."
-        const val INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE = "시도할 횟수가 입력되지 않았습니다."
-        const val INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE = "시도할 횟수가 올바르게 입력되지 않았습니다."
-        const val INPUT_NAME_RIGHT_ERROR_MESSAGE = "자동차 이름은 한글과 영어만 가능합니다."
-    }
+object Constants {
+    const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    const val INPUT_TRY_NUMBER = "시도할 횟수는 몇 회인가요?"
+    const val OUTPUT_RESULT = "실행 결과"
+    const val OUTPUT_WINNER = "최종 우승자: "
+    const val INPUT_NAME_SIZE_ERROR_MESSAGE = "자동차 이름의 길이가 5를 초괴합니다."
+    const val INPUT_NAME_NULL_ERROR_MESSAGE = "자동차 이름이 입력되지 않았습니다."
+    const val INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE = "시도할 횟수가 입력되지 않았습니다."
+    const val INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE = "시도할 횟수가 올바르게 입력되지 않았습니다."
+    const val INPUT_NAME_RIGHT_ERROR_MESSAGE = "자동차 이름은 한글과 영어만 가능합니다."
 }
