4a42b3d486f8f12a58ca01fcae736b6a89d396fa
Hyemin Song <hyemdooly@gmail.com>
2023-02-10 16:59:34 +0900

    refactor(Cars): 부생성자 추가, getCarInfos 추가, findWinners 삭제

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -2,23 +2,20 @@ package model
 
 import generator.RandomGenerator
 
-class Cars(input: String) {
-    private val cars: List<Car>
+class Cars(private val cars: List<Car>) {
+    constructor(input: String) : this(input.split(",").mapIndexed { _, name -> Car(name.trim()) })
 
-    init {
-        this.cars = mappingCars(input)
+    fun getCarInfos(): List<CarInfo> {
+        val carInfos = mutableListOf<CarInfo>()
+        repeat(cars.size) {
+            carInfos.add(cars[it].getInfo())
+        }
+        return carInfos
     }
 
-    private fun mappingCars(input: String): List<Car> = input.split(",").mapIndexed { _, name -> Car(name.trim()) }
     fun getCarInfo(index: Int): CarInfo = cars[index].getInfo()
     fun getCarSize(): Int = cars.size
-
     fun move(index: Int) {
         cars[index].move(RandomGenerator().getRandomNumber())
-    }
-
-    fun findWinners(): List<String> {
-        val equalCars = cars.groupBy({ it.getInfo().position }, { it.getInfo().name })
-        return equalCars[equalCars.keys.max()]?.toList() ?: listOf()
     }
 }
