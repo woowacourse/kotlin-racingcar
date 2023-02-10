03a694b42b00495c360bba52729b03cbfbeab223
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-10 16:03:39 +0900

    feat(Constants): 상수 생성

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -2,6 +2,8 @@ package util
 
 object Constants {
 
+    const val RANDOM_LIMIT_MIM_NUM = 0
+    const val RANDOM_LIMIT_MAX_NUM = 9
     const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     const val INPUT_TRY_NUMBER = "시도할 횟수는 몇 회인가요?"
     const val OUTPUT_RESULT = "실행 결과"
