0a6aad039f6714f6385dccd2122d00272ae7dd42
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-10 14:14:48 +0900

    refactor (RandomNumberGenerator) : 랜덤 넘버 생성방법 변경

--- src/main/kotlin/racingcar/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/domain/RandomNumberGenerator.kt
@@ -1,9 +1,7 @@
 package racingcar.domain
 
-import kotlin.random.Random
-
 class RandomNumberGenerator : NumberGenerator {
     override fun generateNumber(minNumber: Int, maxNumber: Int): Int {
-        return Random.nextInt(minNumber, maxNumber)
+        return (minNumber..maxNumber).random()
     }
 }
