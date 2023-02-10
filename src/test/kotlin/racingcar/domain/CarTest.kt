3af4d4e232ff2dc8ebb4e75c535df5acedd1b678
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-10 09:35:41 +0900

    test (CarTest) : 테스트 명에 맞게 인자값 수정

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -2,16 +2,23 @@ package racingcar.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.lang.IllegalArgumentException
 
 internal class CarTest {
-    @Test
-    fun `Car 객체의 이름은 1자 이상 5자 이하만 가능`() {
+    @ParameterizedTest(name = "Car 객체 이름은 1자 이상 5자 이하는 정상 작동한다 {argumentsWithNames}")
+    @ValueSource(strings = ["1", "12345"])
+    fun `Car 객체 이름은 1자 이상 5자 이하는 정상 작동한다`(input: String) {
+        assertDoesNotThrow { Car(input) }
+    }
+
+    @ParameterizedTest(name = "Car 객체의 이름은 1자 이상 5자 이하 이외에는 에러가 난다 {argumentsWithNames}")
+    @ValueSource(strings = ["", "123456"])
+    fun `Car 객체의 이름은 1자 이상 5자 이하 이외에는 에러가 난다`(input: String) {
         assertThrows<IllegalArgumentException> { Car("") }
-        assertThrows<IllegalArgumentException> { Car("123456") }
     }
 
     @Test
