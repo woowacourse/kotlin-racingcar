3ab052f301e2f98c55568e45aee19167f5740c35
tmdgh1592 <tmdgh1592@naver.com>
2023-02-10 15:02:29 +0900

    refactor: Random 유틸 클래스를 제거하고 Service에서 로직 구현

--- src/main/kotlin/racingcar/service/RacingService.kt
+++ src/main/kotlin/racingcar/service/RacingService.kt
@@ -3,7 +3,6 @@ package racingcar.service
 import racingcar.model.Car
 import racingcar.utils.END_RANDOM_MOVEMENT_PROBABILITY
 import racingcar.utils.MOVEMENT_PROBABILITY
-import racingcar.utils.Random
 import racingcar.utils.START_RANDOM_MOVEMENT_PROBABILITY
 
 class RacingService {
@@ -16,13 +15,10 @@ class RacingService {
         }
     }
 
-    private fun isMove(): Boolean {
-        val random = Random.pickInRange(START_RANDOM_MOVEMENT_PROBABILITY, END_RANDOM_MOVEMENT_PROBABILITY)
-        if (random < MOVEMENT_PROBABILITY) {
-            return false
-        }
-        return true
-    }
+    private fun isMove(): Boolean = (getRandomProbabilityInRange() < MOVEMENT_PROBABILITY)
+
+    private fun getRandomProbabilityInRange(): Int =
+        (START_RANDOM_MOVEMENT_PROBABILITY..END_RANDOM_MOVEMENT_PROBABILITY + 1).random()
 
     fun getWinners(cars: List<Car>): List<Car> {
         val winnerStandard = cars.max()
