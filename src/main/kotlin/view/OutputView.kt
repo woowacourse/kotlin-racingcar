508319c218f9349c72f7665f93548d53392dbdb2
mendel <rhthrhrl0@naver.com>
2023-02-10 16:30:50 +0900

    refactor(view): OutputView에서 자동차의 전진 상태를 생성하는 로직을 메소드화시키는 리팩토링

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -8,11 +8,15 @@ object OutputView {
     fun printMessage(message: String) = println(message)
     fun printGameStatus(cars: List<Car>) {
         cars.forEach { car ->
-            println("${car.name} : ${"-".repeat(car.advanceCount)}")
+            println("${car.name} : ${carAdvanceState(car)}")
         }
         println()
     }
 
+    private fun carAdvanceState(car: Car) = ADVANCE_STEP.repeat(car.advanceCount)
+
     fun printGameResult(winCars: List<Car>) =
         println(GAME_RESULT_FORMAT.format(winCars.joinToString(", ") { it.name }))
+
+    private const val ADVANCE_STEP = "-"
 }
