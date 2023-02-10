8eb82d827a76341e085ddfe3a5fb32c52310d43c
EmilyCh0 <emily9803@ewhain.net>
2023-02-10 21:05:25 +0900

    test: 여러 값 테스트

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -1,34 +1,42 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
 
-    @Test
-    fun `값이 4인 경우 이동한다`() {
-        val car = Car("Tom", INITIAL_MOVE_COUNT)
-        car.tryMove(TestNumberGenerator(MIN_MOVE_NUMBER).generate())
+    private lateinit var car: Car
+
+    @BeforeEach
+    fun initCar() {
+        car = Car("Tom", INITIAL_MOVE_COUNT)
+    }
+
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun `값이 4 이상인 경우 이동한다`(number: Int) {
+        car.tryMove(TestNumberGenerator(number).generate())
         assertThat(car.moveCount).isEqualTo(EXPECTED_MOVE_COUNT)
+        println(car.moveCount)
     }
 
-    @Test
-    fun `값이 3인 경우 이동하지 않는다`() {
-        val car = Car("Tom", INITIAL_MOVE_COUNT)
-        car.tryMove(TestNumberGenerator(NOT_MOVE_NUMBER).generate())
+    @ValueSource(ints = [0, 1, 2, 3])
+    @ParameterizedTest
+    fun `값이 3 이하인 경우 이동하지 않는다`(number: Int) {
+        car.tryMove(TestNumberGenerator(number).generate())
         assertThat(car.moveCount).isEqualTo(INITIAL_MOVE_COUNT)
-    }
-
-    class TestNumberGenerator(private val number: Int) : CarNumberGenerator {
-        override fun generate(): Int {
-            return number
-        }
     }
 
     companion object {
         private const val INITIAL_MOVE_COUNT = 0
         private const val EXPECTED_MOVE_COUNT = 1
-        private const val MIN_MOVE_NUMBER = 4
-        private const val NOT_MOVE_NUMBER = 3
+    }
+}
+
+class TestNumberGenerator(private val number: Int) : CarNumberGenerator {
+    override fun generate(): Int {
+        return number
     }
 }
