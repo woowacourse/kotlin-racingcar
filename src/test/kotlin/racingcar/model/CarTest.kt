4f9ba23f4097ed03094c9f25a8bc9a3ec4f901e0
tmdgh1592 <tmdgh1592@naver.com>
2023-02-10 15:40:28 +0900

    test: 자동차 객체 생성 테스트 코드 구현

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -2,8 +2,28 @@ package racingcar.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["buna", "otter", "jack", "son"])
+    fun `자동차 생성 노말 테스트`(name: String) {
+        assertDoesNotThrow {
+            Car(name)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["refactoring", "reviewer", "", "jackson"])
+    fun `자동차 생성 이름 길이 예외 테스트`(name: String) {
+        assertThrows<IllegalArgumentException> {
+            Car(name)
+        }
+    }
 
     @Test
     fun `자동차 이동횟수 일치 테스트`() {
