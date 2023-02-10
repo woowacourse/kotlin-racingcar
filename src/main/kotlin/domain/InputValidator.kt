5b4b5a3205f9e8b7150985013dea56b42b7138c5
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-10 14:34:45 +0900

    feat : 자동차 이름을 입력하면서 ,이 연속적으로 있는 경우를 처리하기 위한 검증기능 추가

--- src/main/kotlin/domain/InputValidator.kt
+++ src/main/kotlin/domain/InputValidator.kt
@@ -1,11 +1,28 @@
 package domain
 
+import data.EMPTY
+import view.OutputView.TOKENIZER
+
 object InputValidator {
 
-    fun validateIsNumeric(numberOfTry: String) {
+    fun validateEmptyInput(nameInput: String): List<String> {
+        val names = nameInput.split(TOKENIZER)
+
+        names.forEach { name ->
+            require(name != EMPTY) {
+                InputError.EMPTY_INPUT_ERROR.message
+            }
+        }
+
+        return names
+    }
+
+    fun validateIsNumeric(numberOfTry: String): Int {
         require(numberOfTry.isNumeric()) {
             InputError.NUMERIC_ERROR.message
         }
+
+        return numberOfTry.toInt()
     }
 
     private fun String.isNumeric(): Boolean {
