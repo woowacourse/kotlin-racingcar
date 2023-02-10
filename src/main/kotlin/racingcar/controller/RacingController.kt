a2441a2f30ec26faa9970f3d8ffb59829547aab9
tmdgh1592 <tmdgh1592@naver.com>
2023-02-10 19:31:05 +0900

    refactor: 불필요한 상수 제거

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -63,6 +63,5 @@ class RacingController(
         private const val CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         private const val ROUND_COUNT_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?"
         private const val ROUNDS_RESULT_NOTIFICATION_MESSAGE = "\n실행 결과"
-        private const val WINNER_NOTIFICATION_MESSAGE = "최종 우승자"
     }
 }
