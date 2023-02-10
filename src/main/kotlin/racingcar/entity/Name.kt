b29c89a8a8317acdab215984fce707db52bec101
Doyeop Kim <dygames@naver.com>
2023-02-10 16:28:02 +0900

    refactor(Name): equals 삭제 후 data class화

--- src/main/kotlin/racingcar/entity/Name.kt
+++ src/main/kotlin/racingcar/entity/Name.kt
@@ -1,6 +1,6 @@
 package racingcar.entity
 
-class Name(private val name: String) {
+data class Name(private val name: String) {
     init {
         require(name.isNotEmpty()) { "차 이름은 1글자 이상이어야 합니다." }
         require(name.length <= MIN_CAR_NAME_LENGTH) { "차 이름은 5글자 이하여야 합니다." }
