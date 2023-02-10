0e835f568b04b5b5f13904c63c2769c5b52fce21
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-10 14:38:14 +0900

    refactor : null을 허용하지 않는 readln으로 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,15 +5,15 @@ object InputView {
     private const val CAR_NAMES_REQUEST_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
     private const val NUMBER_OF_TRY_REQUEST_MSG = "시도할 횟수는 몇 회인가요?\n"
 
-    fun inputCarNames(): String? {
+    fun inputCarNames(): String {
         println(CAR_NAMES_REQUEST_MSG)
 
-        return readLine()
+        return readln()
     }
 
-    fun inputNumberOfTry(): String? {
+    fun inputNumberOfTry(): String {
         println(NUMBER_OF_TRY_REQUEST_MSG)
 
-        return readLine()
+        return readln()
     }
 }
