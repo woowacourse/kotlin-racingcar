6c0d560145623a1a5e514aebf1990cd5e94c8758
Hyemin Song <hyemdooly@gmail.com>
2023-02-10 14:53:10 +0900

    refactor(Cars): position 의미 혼동 방지를 위한 변수 파라미터명 변경

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -10,15 +10,15 @@ class Cars(input: String) {
     }
 
     private fun mappingCars(input: String): List<Car> = input.split(",").mapIndexed { _, name -> Car(name.trim()) }
-    fun getCarInfo(position: Int): Pair<String, Int> = cars[position].getInfo()
+    fun getCarInfo(index: Int): CarInfo = cars[index].getInfo()
     fun getCarSize(): Int = cars.size
 
-    fun move(position: Int) {
-        cars[position].move(RandomGenerator().getRandomNumber())
+    fun move(index: Int) {
+        cars[index].move(RandomGenerator().getRandomNumber())
     }
 
     fun findWinners(): List<String> {
-        val equalCars = cars.groupBy({ it.getInfo().second }, { it.getInfo().first })
+        val equalCars = cars.groupBy({ it.getInfo().position }, { it.getInfo().name })
         return equalCars[equalCars.keys.max()]?.toList() ?: listOf()
     }
 }
