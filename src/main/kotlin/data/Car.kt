f9d61d039d8a77b7d7e97a8d2f68ccf15ac118f1
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-02-10 13:47:20 +0900

    <refactor> 매직넘버 상수화

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -1,14 +1,12 @@
 package data
 
-import util.CAR_NAME_ERROR_MSG
-
 class Car(
     private var position: Int,
     private val name: String
 ) {
 
     init {
-        require(name.length <= 5) {
+        require(name.length <= CAR_NAME_MAX) {
             CAR_NAME_ERROR_MSG
         }
     }
@@ -19,5 +17,10 @@ class Car(
 
     fun getCar(): Pair<String, Int> {
         return Pair(name, position)
+    }
+
+    companion object {
+        const val CAR_NAME_MAX = 5
+        const val CAR_NAME_ERROR_MSG = "[ERROR] 자동차 이름은 5자를 초과할 수 없다."
     }
 }
--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -7,7 +7,7 @@ const val RESULT = "실행 결과\n"
 const val FINAL_WINNER = "최종 우승자: "
 
 const val NUMERIC_ERROR_MSG = "[ERROR] 시도 횟수는 숫자 입력으로만 받습니다."
-const val CAR_NAME_ERROR_MSG = "[ERROR] 자동차 이름은 5자를 초과할 수 없다."
+
 
 const val MOVE_MARK = "-"
 const val STOP_MARK = ""
