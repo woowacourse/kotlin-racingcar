25a78e79c42a1378557dd4e1523a7ac31e809263
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-10 13:53:06 +0900

    refactor : bufferedReader와 bufferedWriter을 println과 readLine으로 대체

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,34 +1,25 @@
 package view
 
 import data.CarPath
-import util.getPathMarks
-import util.subPath
-import view.View.bw
+import data.CarPath.Companion.subPath
+import data.PathState.Companion.getPathMarks
 
 object OutputView {
 
     private const val RESULT = "실행 결과\n"
     private const val FINAL_WINNER = "최종 우승자: "
-    private const val MOVE_MARK = "-"
-    private const val STOP_MARK = ""
-
-    fun printMsg(msg: String) {
-        bw.write(msg)
-        bw.flush()
-    }
+    private const val TOKENIZER = ", "
+    private const val CAR_PATH = "%s : %s"
 
     fun printResult(carsPath: List<CarPath>, numberOfTry: Int) {
         carsPath.forEach { carPath ->
-            bw.write(carPath.carName + " : " + carPath.subPath(numberOfTry).getPathMarks() + "\n")
-            bw.flush()
+            println(CAR_PATH.format(carPath.carName, carPath.subPath(numberOfTry).getPathMarks()))
         }
-        bw.write("\n")
-        bw.flush()
+        println()
     }
 
     fun printWinner(winners: List<String>) {
-        bw.write(FINAL_WINNER)
-        bw.write(winners.joinToString(", ") + '\n')
-        bw.flush()
+        println(FINAL_WINNER)
+        println(winners.joinToString(TOKENIZER))
     }
 }
