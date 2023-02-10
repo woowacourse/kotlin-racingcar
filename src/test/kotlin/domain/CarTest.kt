6baa7e4f824f4b51b10170e9184f80ec5ad8f9cb
EmilyCh0 <emily9803@ewhain.net>
2023-02-10 19:37:54 +0900

    test: 기대값 로직 제거

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -9,14 +9,14 @@ class CarTest {
     fun `값이 4인 경우 이동한다`() {
         val car = Car("Tom", INITIAL_MOVE_COUNT)
         car.tryMove(TestNumberGenerator(MIN_MOVE_NUMBER))
-        assertThat(car.getMoveCount() == INITIAL_MOVE_COUNT + 1)
+        assertThat(car.getMoveCount()).isEqualTo(EXPECTED_MOVE_COUNT)
     }
 
     @Test
     fun `값이 3인 경우 이동하지 않는다`() {
         val car = Car("Tom", INITIAL_MOVE_COUNT)
         car.tryMove(TestNumberGenerator(NOT_MOVE_NUMBER))
-        assertThat(car.getMoveCount() == INITIAL_MOVE_COUNT)
+        assertThat(car.getMoveCount()).isEqualTo(INITIAL_MOVE_COUNT)
     }
 
     class TestNumberGenerator(private val number: Int) : CarNumberGenerator {
@@ -27,6 +27,7 @@ class CarTest {
 
     companion object {
         private const val INITIAL_MOVE_COUNT = 0
+        private const val EXPECTED_MOVE_COUNT = 1
         private const val MIN_MOVE_NUMBER = 4
         private const val NOT_MOVE_NUMBER = 3
     }
