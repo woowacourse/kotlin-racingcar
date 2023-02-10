dfc7b12762711a3489393037173d93c30b20c965
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-10 14:10:47 +0900

    refactor : extension에 존재했던 함수를 관련 클래스로 이동

--- src/main/kotlin/data/PathState.kt
+++ src/main/kotlin/data/PathState.kt
@@ -1,9 +1,21 @@
 package data
 
-import util.MOVE_MARK
-import util.STOP_MARK
+private const val DASH = "-"
+private const val EMPTY = ""
 
 enum class PathState(val state: String) {
-    MOVE(MOVE_MARK),
-    STOP(STOP_MARK)
+    MOVE(DASH),
+    STOP(EMPTY);
+
+    companion object {
+        fun List<PathState>.getPathMarks(): String {
+            var pathMarks = EMPTY
+
+            this.forEach { pathState ->
+                pathMarks += pathState.state
+            }
+
+            return pathMarks
+        }
+    }
 }
