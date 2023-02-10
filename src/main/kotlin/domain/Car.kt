9743bc55fd22d89ecb13107257476ecb14a11f95
ki960213 <ki960213@github.com>
2023-02-10 16:17:36 +0900

    feat(domain): Car 객체 생성 시 유효성 검사 코드 추가

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,8 +1,11 @@
 package domain
 
-class Car(
-    val name: String, private val movingStrategy: MovingStrategy
-) {
+class Car(val name: String, private val movingStrategy: MovingStrategy) {
+
+    init {
+        require(name.matches(Regex("[a-zA-Z가-힣]{1,5}"))) { ERROR_MESSAGE_FORMAT.format(CAR_NAME_REGEX_ERROR) }
+    }
+
     var advanceCount: Int = 0
         private set
 
@@ -10,4 +13,10 @@ class Car(
         if (movingStrategy.isMovable())
             advanceCount++
     }
+
+    companion object {
+        private const val ERROR_MESSAGE_FORMAT = "[ERROR] %s"
+    }
 }
+
+private const val CAR_NAME_REGEX_ERROR = "자동차의 이름은 한글 혹은 영어로 공백이 없어야 하며 최소 1글자에서 최대 5글자까지 가능합니다."
