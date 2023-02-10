6f4984b73c3646e042ae292bd7d9066b307756cc
Doyeop Kim <dygames@naver.com>
2023-02-10 19:49:48 +0900

    feat(SequentialNumberGenerator): 순차적 숫자 생성기

---
+++ src/main/kotlin/racingcar/model/SequentialNumberGenerator.kt
@@ -0,0 +1,6 @@
+package racingcar.model
+
+class SequentialNumberGenerator(private val numbers: List<Int>) : NumberGenerator {
+    private var index = 0
+    override fun generate(): Int = numbers[++index]
+}
