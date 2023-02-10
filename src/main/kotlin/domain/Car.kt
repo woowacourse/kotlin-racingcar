a69776c7b4db5cb1c4195e6b3c005f4e7cdc90e5
briandr97 <briandr@naver.com>
2023-02-10 21:37:39 +0900

    refactor(Car): 상수화한 에러메시지에 확장성 추가
    1, 5를 하드코딩하지 않고 다른 상수로 넣었다.

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -15,9 +15,10 @@ class Car(val carName: String, private val driver: Driver) {
     }
 
     companion object {
-        const val NAME_CONVENTION_ERROR_MESSAGE = "[ERROR] 이름은 1~5글자여야합니다."
         private const val NAME_LENGTH_LOWER_INCLUSIVE = 1
         private const val NAME_LENGTH_UPPER_INCLUSIVE = 5
+        const val NAME_CONVENTION_ERROR_MESSAGE =
+            "[ERROR] 이름은 $NAME_LENGTH_LOWER_INCLUSIVE~${NAME_LENGTH_UPPER_INCLUSIVE}글자여야합니다."
         val RANGE = NAME_LENGTH_LOWER_INCLUSIVE..NAME_LENGTH_UPPER_INCLUSIVE
     }
 }
