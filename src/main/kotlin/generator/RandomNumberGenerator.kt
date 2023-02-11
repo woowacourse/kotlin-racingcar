ba1563a7984f9914e8b75184b963310ba3ca6604
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 22:41:30 +0900

    refactor(RandomNumberGenerator): 상수 위치 변경

--- src/main/kotlin/generator/RandomNumberGenerator.kt
+++ src/main/kotlin/generator/RandomNumberGenerator.kt
@@ -1,9 +1,12 @@
 package generator
 
-import util.Constants
-
 class RandomNumberGenerator : NumberGenerator {
     override fun generate(): Int {
-        return (Constants.RANDOM_LIMIT_MIM_NUM..Constants.RANDOM_LIMIT_MAX_NUM).random()
+        return (RANDOM_LIMIT_MIM_NUM..RANDOM_LIMIT_MAX_NUM).random()
+    }
+
+    companion object {
+        const val RANDOM_LIMIT_MIM_NUM = 0
+        const val RANDOM_LIMIT_MAX_NUM = 9
     }
 }
