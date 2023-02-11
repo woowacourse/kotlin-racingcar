3c2939c8eedc68f1f2d8380fd2d34d72190c616d
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 14:57:45 +0900

    test(CarTest): 4이상이면 자동차가 전진, 3이하이면 자동차가 정지하는 테스트 추가

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,24 +1,22 @@
 import model.Car
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
-
-    @Test
-    fun getInfoTest() {
-        assertThat(Car("dool", 0).getInfo()).isEqualTo(Pair("dool", 0))
-        assertThrows<IllegalArgumentException> {
-            assertThat(Car("dooly", 0).getInfo()).isEqualTo(Pair("dooly", 0))
-        }
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun `4이상이면 자동차가 전진`(number: Int) {
+        val car = Car("pobi")
+        car.move(number)
+        assertThat(car.position).isEqualTo(1)
     }
 
-    @Test
-    fun moveTest() {
-        val car = Car("dool", 0)
-        car.move(3)
-        assertThat(car.getInfo().second).isEqualTo(0)
-        car.move(4)
-        assertThat(car.getInfo().second).isEqualTo(1)
+    @ValueSource(ints = [1, 2, 3])
+    @ParameterizedTest
+    fun `3이하이면 자동차가 정지`(number: Int) {
+        val car = Car("pobi")
+        car.move(number)
+        assertThat(car.position).isEqualTo(0)
     }
 }
