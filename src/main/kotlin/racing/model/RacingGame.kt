9c94948a4e5b72cf3bd2eed3b44286cbf4d8e3d4
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-11 15:43:15 +0900

    refactor: 호출 순서대로 함수 순서 변경

--- src/main/kotlin/racing/model/RacingGame.kt
+++ src/main/kotlin/racing/model/RacingGame.kt
@@ -15,20 +15,25 @@ class RacingGame(
         this.cars.addAll(cars)
     }
 
-    fun getWinners(): List<String> {
-        val result = mutableListOf<String>()
-        val winnerPosition = cars.maxOf { it.getPosition() }
-        cars.map {
-            if (it.getPosition() == winnerPosition) result.add(it.getName())
-        }
-        return result.toList()
-    }
-
     fun race(maxMoveCount: Int): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
         repeat(maxMoveCount) {
             moveCars(createRandomNumbers(cars.size))
             result.add(getGameResultOneTurn())
+        }
+        return result
+    }
+
+    private fun moveCars(randomNumbers: List<Int>) {
+        for ((index, car) in cars.withIndex()) {
+            car.move(randomNumbers[index])
+        }
+    }
+
+    private fun createRandomNumbers(count: Int): List<Int> {
+        val result = mutableListOf<Int>()
+        repeat(count) {
+            result.add(numberGenerator.generate())
         }
         return result
     }
@@ -41,17 +46,12 @@ class RacingGame(
         return result
     }
 
-    private fun moveCars(randomNumbers: List<Int>) { // 인자로 받을 필요없다
-        for ((index, car) in cars.withIndex()) {
-            car.move(randomNumbers[index])
+    fun getWinners(): List<String> {
+        val result = mutableListOf<String>()
+        val winnerPosition = cars.maxOf { it.getPosition() }
+        cars.map {
+            if (it.getPosition() == winnerPosition) result.add(it.getName())
         }
-    }
-
-    private fun createRandomNumbers(count: Int): List<Int> {
-        val result = mutableListOf<Int>()
-        repeat(count) {
-            result.add(numberGenerator.generate())
-        }
-        return result
+        return result.toList()
     }
 }
