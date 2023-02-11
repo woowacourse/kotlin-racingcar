ba4ce4472014c22d4f01e497f7dc0c60bce5d046
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 17:09:10 +0900

    test(CarsTest): 자동차 경주 우승자들 확인하는 테스트 추가

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -1,3 +1,4 @@
+import generator.NumberGenerator
 import generator.RandomGenerator
 import model.Car
 import model.Cars
@@ -6,7 +7,8 @@ import org.junit.jupiter.api.Test
 
 class CarsTest {
 
-    val inputCars = listOf(Car("pobi"), Car("woni"), Car("gugu"))
+    private val inputCars = listOf(Car("pobi"), Car("woni"), Car("gugu"))
+    private val tryNumber = 3
 
     @Test
     fun `자동차 이름들 ','로 구분되어 생성되었는지 확인`() {
@@ -14,5 +16,20 @@ class CarsTest {
         assertThat(mappingCars.cars[0].name).isEqualTo(inputCars[0].name)
         assertThat(mappingCars.cars[1].name).isEqualTo(inputCars[1].name)
         assertThat(mappingCars.cars[2].name).isEqualTo(inputCars[2].name)
+    }
+
+    @Test
+    fun `자동차 경주 우승자들 확인`() {
+        val cars = Cars(inputCars, TestNumberGenerator())
+        repeat(tryNumber) { cars.move() }
+        assertThat(cars.findWinners()).isEqualTo(listOf("pobi", "gugu"))
+    }
+
+    class TestNumberGenerator : NumberGenerator {
+        val testNumber = listOf(4, 1, 6)
+        var position = 0
+        override fun generateNumber(): Int {
+            return testNumber[position++ % 3]
+        }
     }
 }
