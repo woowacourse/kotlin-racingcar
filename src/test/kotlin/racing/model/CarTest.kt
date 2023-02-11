9575df6200b0f900efa1f06d19bab84c06744b44
no1msh <no1msh1217@gmail.com>
2023-02-11 19:31:41 +0900

    refactor: test의 의도에 맞게끔 메서드 이름 변경, 불필요한 구문 삭제, 차이름 관련 예외처리위치 변경

--- src/test/kotlin/racing/model/CarTest.kt
+++ src/test/kotlin/racing/model/CarTest.kt
@@ -1,42 +1,39 @@
 package racing.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class CarTest {
-    private var car = Car("woni")
+    private val car = Car("woni")
 
-    @BeforeEach
-    fun setUp() {
-        car = Car("woni")
+    @Test
+    fun `차 이름이 5글자가 넘으면 exception 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Car("aaaaaa")
+        }
     }
 
     @Test
-    fun `랜덤 숫자가 4,5가 나왔을 떄 테스트`() {
-        car.move(4)
-        car.move(5)
-        assertThat(car.position).isEqualTo(2)
+    fun `차 이름이 빈칸이면 exception 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Car("")
+        }
     }
 
     @ParameterizedTest
     @CsvSource("4", "5", "6", "7", "8", "9")
-    fun `랜덤 숫자가 4부터 9까지 사잇값이 나왔을 떄 이동하는지 테스트`(randomNumber: Int) {
+    fun `랜덤 숫자가 4부터 9까지 사잇값이 나왔을 떄 이동`(randomNumber: Int) {
         car.move(randomNumber)
         assertThat(car.position).isEqualTo(1)
     }
 
     @ParameterizedTest
     @CsvSource("1", "2", "3")
-    fun `랜덤 숫자가 1부터 3까지 사잇값이 나왔을 떄 이동을 안하는지 테스트`(randomNumber: Int) {
+    fun `랜덤 숫자가 1부터 3까지 사잇값이 나왔을 떄 이동하지않음`(randomNumber: Int) {
         car.move(randomNumber)
         assertThat(car.position).isEqualTo(0)
-    }
-
-    @Test
-    fun `getName() 정상적으로 작동하는지 테스트`() {
-        assertThat(car.name).isEqualTo("woni")
     }
 }
