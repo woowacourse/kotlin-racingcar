afaed61db456a737d2aeeaf344d9c22dfc93825d
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 17:26:39 +0900

    test(CarTest): 자동차 이름이 공백인 경우 예외 테스트 추가

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,9 +1,19 @@
 import model.Car
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
+
+    @Test
+    fun `자동차 이름이 공백인 경우 예외 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            Car("")
+        }
+    }
+
     @ValueSource(ints = [4, 5, 6, 7, 8, 9])
     @ParameterizedTest
     fun `4이상이면 자동차가 전진`(number: Int) {
