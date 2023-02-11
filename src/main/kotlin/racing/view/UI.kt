4fef8031ff9d6f326ec4ad81c5de0580defb46e6
no1msh <no1msh1217@gmail.com>
2023-02-11 17:17:52 +0900

    refactor: 기존 controller에 있던 출력 구문을 따로 관리

---
+++ src/main/kotlin/racing/view/UI.kt
@@ -0,0 +1,15 @@
+package racing.view
+
+object UI {
+    fun printRequestCarNames() {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    }
+
+    fun printRequestTimes() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+
+    fun printResult() {
+        println("실행 결과")
+    }
+}
