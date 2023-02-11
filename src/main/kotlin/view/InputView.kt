450b076c78f5f4b07fbf71509f2f30a3d332706d
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-11 19:47:08 +0900

    fix : 출력구문 이상한 부분 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,8 +2,8 @@ package view
 
 object InputView {
 
-    private const val CAR_NAMES_REQUEST_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
-    private const val NUMBER_OF_TRY_REQUEST_MSG = "시도할 횟수는 몇 회인가요?\n"
+    private const val CAR_NAMES_REQUEST_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    private const val NUMBER_OF_TRY_REQUEST_MSG = "시도할 횟수는 몇 회인가요?"
 
     fun inputCarNames(): String {
         println(CAR_NAMES_REQUEST_MSG)
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -4,7 +4,7 @@ import data.Car
 
 object OutputView {
 
-    private const val RESULT = "실행 결과\n"
+    private const val RESULT = "실행 결과"
     private const val FINAL_WINNER = "최종 우승자: "
     private const val CAR_PATH = "%s : %s"
     private const val DASH = "-"
@@ -16,8 +16,9 @@ object OutputView {
     }
 
     fun printWinner(winners: List<String>) {
-        println(FINAL_WINNER)
+        print(FINAL_WINNER)
         println(winners.joinToString("$TOKENIZER "))
+        println()
     }
 
     fun printPath(numberOfTry: Int, cars: List<Car>) {
@@ -28,5 +29,6 @@ object OutputView {
         cars.forEach { car ->
             println(CAR_PATH.format(car.name, DASH.repeat(car.position)))
         }
+        println()
     }
 }
