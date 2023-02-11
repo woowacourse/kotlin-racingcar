6397797dc73a3843ab41e833340161799f9ca9a9
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 22:44:50 +0900

    refactor(Validator): 상수 위치 변경

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -20,26 +20,34 @@ class Validator {
     }
 
     private fun checkNameNull(name: String?) {
-        require(name != null) { Constants.INPUT_NAME_NULL_ERROR_MESSAGE }
+        require(name != null) { INPUT_NAME_NULL_ERROR_MESSAGE }
     }
 
     private fun checkNameSize(name: String) {
-        require(name.length < 5) { Constants.INPUT_NAME_SIZE_ERROR_MESSAGE }
+        require(name.length < 5) { INPUT_NAME_SIZE_ERROR_MESSAGE }
     }
 
     private fun checkNameEmpty(name: String) {
-        require(name != "") { Constants.INPUT_NAME_NULL_ERROR_MESSAGE }
+        require(name != "") { INPUT_NAME_NULL_ERROR_MESSAGE }
     }
 
     private fun checkNameRight(name: String) {
-        require(name.contains("^[a-zA-Z가-힣]*$".toRegex())) { Constants.INPUT_NAME_RIGHT_ERROR_MESSAGE }
+        require(name.contains("^[a-zA-Z가-힣]*$".toRegex())) { INPUT_NAME_RIGHT_ERROR_MESSAGE }
     }
 
     private fun checkTryNumberNull(number: String?) {
-        require(number != null) { Constants.INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE }
+        require(number != null) { INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE }
     }
 
     private fun checkTryNumberIsRight(number: String?) {
-        require(number!!.toIntOrNull() != null) { Constants.INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE }
+        require(number!!.toIntOrNull() != null) { INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val INPUT_NAME_SIZE_ERROR_MESSAGE = "자동차 이름의 길이가 5를 초괴합니다."
+        const val INPUT_NAME_NULL_ERROR_MESSAGE = "자동차 이름이 입력되지 않았습니다."
+        const val INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE = "시도할 횟수가 입력되지 않았습니다."
+        const val INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE = "시도할 횟수가 올바르게 입력되지 않았습니다."
+        const val INPUT_NAME_RIGHT_ERROR_MESSAGE = "자동차 이름은 한글과 영어만 가능합니다."
     }
 }
