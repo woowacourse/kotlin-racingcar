56591ccb40e34f8a25b53047ed4defde19a140c9
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-11 16:25:00 +0900

    test(CarsTest): 자동차 이름들을 ','로 구분해서 생성했는지 확인하는 테스트 추가

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -1,3 +1,4 @@
+import generator.RandomGenerator
 import model.Car
 import model.Cars
 import org.assertj.core.api.Assertions.assertThat
@@ -5,13 +6,13 @@ import org.junit.jupiter.api.Test
 
 class CarsTest {
 
-    val cars = listOf(Car("pobi"), Car("woni"), Car("gugu"))
+    val inputCars = listOf(Car("pobi"), Car("woni"), Car("gugu"))
 
     @Test
-    fun `자동차 ','로 구분되었는지 확인`() {
-        val mappingCars = Cars.mappingCars("pobi,woni,gugu")
-        assertThat(mappingCars.cars[0].name).isEqualTo(cars[0].name)
-        assertThat(mappingCars.cars[1].name).isEqualTo(cars[1].name)
-        assertThat(mappingCars.cars[2].name).isEqualTo(cars[2].name)
+    fun `자동차 이름들 ','로 구분되어 생성되었는지 확인`() {
+        val mappingCars = Cars.mappingCars("pobi,woni,gugu", RandomGenerator())
+        assertThat(mappingCars.cars[0].name).isEqualTo(inputCars[0].name)
+        assertThat(mappingCars.cars[1].name).isEqualTo(inputCars[1].name)
+        assertThat(mappingCars.cars[2].name).isEqualTo(inputCars[2].name)
     }
 }
