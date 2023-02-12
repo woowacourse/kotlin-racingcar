2f67cd826c059c7624897525a37701aba1768b95
inseonyun <yuninseon@naver.com>
2023-02-12 23:42:12 +0900

    refactor: NameValidation Regex Pattern 상수화

--- src/main/kotlin/validation/NameValidation.kt
+++ src/main/kotlin/validation/NameValidation.kt
@@ -20,12 +20,13 @@ object NameValidation {
     }
 
     fun checkCarNameEnglishNumber(carName: String) {
-        require(Regex("^[a-zA-Z0-9]*\$").matches(carName)) {
+        require(Regex(REGEX_ONLY_ENGLISH_AND_NUMBER).matches(carName)) {
             NAME_IS_ONLY_ENGLISH_AND_NUMBER
         }
     }
 
     private const val MAXIMUM_CAR_NAME_LENGTH = 5
+    private const val REGEX_ONLY_ENGLISH_AND_NUMBER = "^[a-zA-Z0-9]*\$"
 
     private const val NAME_IS_ONLY_ENGLISH_AND_NUMBER = "[ERROR] 이름은 영어와 숫자만 가능합니다."
     private const val NAME_RANGE_ERROR_MASSAGE = "[ERROR] 자동차 이름은 1자 이상 5자 이하로 입력해주세요"
