1b29d5227d7319b5f7ba36d720418697aac99077
whk06061 <gju06061@naver.com>
2023-02-13 02:48:53 +0900

    refactor: moveCount 최댓값 찾는 로직과 우승자 찾는 로직을 함수로 분리

--- src/main/kotlin/domain/Judgment.kt
+++ src/main/kotlin/domain/Judgment.kt
@@ -2,9 +2,16 @@ package domain
 
 class Judgment {
     fun findWinners(cars: List<Car>): List<String> {
-        val maxCount = cars.maxOfOrNull { car ->
+        return findWinnersBy(cars, getMaxCount(cars))
+    }
+
+    private fun getMaxCount(cars: List<Car>): Int {
+        return cars.maxOf { car ->
             car.moveCount
         }
+    }
+
+    private fun findWinnersBy(cars: List<Car>, maxCount: Int): List<String> {
         return cars.filter { car ->
             car.moveCount == maxCount
         }.map { winner ->
