d59ac149b9865a32cd8193a2077e66442fa99072
ki960213 <ki960213@github.com>
2023-02-12 18:09:44 +0900

    refactor(domain): Car 클래스의 에러 메세지 포맷을 companion object에서 전역 변수로 변경

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -10,13 +10,9 @@ class Car(val name: String, private val movingStrategy: MovingStrategy) {
         private set
 
     fun moveAccordingToStrategy() {
-        if (movingStrategy.isMovable())
-            advanceCount++
-    }
-
-    companion object {
-        private const val ERROR_MESSAGE_FORMAT = "[ERROR] %s"
+        if (movingStrategy.isMovable()) advanceCount++
     }
 }
 
+private const val ERROR_MESSAGE_FORMAT = "[ERROR] %s"
 private const val CAR_NAME_REGEX_ERROR = "자동차의 이름은 한글 혹은 영어로 공백이 없어야 하며 최소 1글자에서 최대 5글자까지 가능합니다."
