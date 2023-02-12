af714c505a74b5a24da6175cd8dafb9090e923c6
ki960213 <ki960213@github.com>
2023-02-12 18:45:22 +0900

    test(domain): 게임 내 제약조건 검증 로직에 대한 테스트 코드 작성

--- src/test/kotlin/domain/CarRacingGameManagerTest.kt
+++ src/test/kotlin/domain/CarRacingGameManagerTest.kt
@@ -1,9 +1,62 @@
 package domain
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class CarRacingGameManagerTest {
+
+    @Nested
+    inner class `생성할 때` {
+
+        @Nested
+        inner class `자동차 중 중복된 것이 있으면` {
+            private val duplicatedCars = listOf(
+                Car("pobi", RandomMovingStrategy()),
+                Car("pobi", RandomMovingStrategy()),
+                Car("tony", RandomMovingStrategy())
+            )
+
+            @Test
+            fun `예외 메세지를 포함한 IllegalArgumentException을 던진다`() {
+                assertThatIllegalArgumentException().isThrownBy { CarRacingGameManager(duplicatedCars) }
+                    .withMessageContaining(ERROR_MESSAGE)
+            }
+        }
+
+        @Nested
+        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+        inner class `자동차의 개수가 2 이상 20 이하가 아니라면` {
+
+            @ParameterizedTest
+            @MethodSource("getIllegalSizeCars")
+            fun `예외 메세지를 포함한 IllegalArgumentException을 던진다`(cars: List<Car>) {
+                assertThatIllegalArgumentException().isThrownBy { CarRacingGameManager(cars) }
+                    .withMessageContaining(ERROR_MESSAGE)
+            }
+
+            private fun getIllegalSizeCars() = listOf(
+                Arguments.of(listOf(Car("pobi", RandomMovingStrategy()))),
+                Arguments.of((1..21).map { Car("pobi".plus('a' + it), RandomMovingStrategy()) })
+            )
+        }
+
+        @Nested
+        inner class `중복되지 않은 2대 이상 20대 이하의 자동차가 주어지면` {
+
+            @Test
+            fun `예외를 던지지 않는다`() {
+                val cars = getCars()
+
+                assertThatCode { CarRacingGameManager(cars) }.doesNotThrowAnyException()
+            }
+        }
+    }
 
     @Test
     fun `게임 내의 모든 자동차를 움직이려 하면 모든 자동차들이 전략대로 움직인다`() {
@@ -47,5 +100,9 @@ internal class CarRacingGameManagerTest {
 
     private class OnlyAdvanceMovingStrategy() : MovingStrategy {
         override fun isMovable(): Boolean = true
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
     }
 }
