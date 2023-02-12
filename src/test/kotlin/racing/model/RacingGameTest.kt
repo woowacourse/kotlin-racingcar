c79020f7b09971ea5614dae24afe3bfc8678c88f
no1msh <no1msh1217@gmail.com>
2023-02-13 01:51:33 +0900

    refactor: 테스트메서드에 들어가는 변수들 하드코딩으로 변경, RacingGame을 object로 변경함에 따라 수정

--- src/test/kotlin/racing/model/RacingGameTest.kt
+++ src/test/kotlin/racing/model/RacingGameTest.kt
@@ -1,30 +1,40 @@
 package racing.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class RacingGameTest {
 
-    private val racingGame = RacingGame(listOf(Car("woni"), Car("pobi"), Car("jason")))
+    @BeforeEach
+    fun setUp(){
+        RacingGame.removeAllCars()
+        RacingGame.registerCars(listOf("woni", "pobi", "jason"))
+    }
+
+    @Test
+    fun `잘못된 Car Name이 포함되어 있다면 예외처리`() {
+        assertThatIllegalArgumentException().isThrownBy { RacingGame.registerCars(listOf("sixchar", "woowa")) }
+    }
 
     @Test
     fun `getCars()로 반환이 되는지 확인`() {
-        val cars = racingGame.cars.map { car: Car -> car.name }
-        val expectation = listOf(Car("woni"), Car("pobi"), Car("jason")).map { car: Car -> car.name }
-        assertThat(cars).isEqualTo(expectation)
+        assertThat(RacingGame.cars.map { car: Car -> car.name })
+            .isEqualTo(listOf(Car("woni"), Car("pobi"), Car("jason")).map { car: Car -> car.name })
     }
 
     @Test
-    fun `우승자가 한명일 때`() {
-        racingGame.cars[WONI].move(PROGRESS)
-        assertThat(racingGame.getWinnerNames()).isEqualTo(listOf("woni"))
+    fun `우승자가 woni 한 명일 때 승자 이름에 woni를 담아 반환한다`() {
+        RacingGame.cars[WONI].move(PROGRESS)
+        assertThat(RacingGame.getWinnerNames()).isEqualTo(listOf("woni"))
     }
 
     @Test
-    fun `우승자가 두 명일 때`() {
-        racingGame.cars[WONI].move(PROGRESS)
-        racingGame.cars[POBI].move(PROGRESS)
-        assertThat(racingGame.getWinnerNames()).isEqualTo(listOf("woni", "pobi"))
+    fun `우승자가 woni와 pobi 두 명일 때 승자 이름에 담아 반환한다`() {
+        RacingGame.cars[WONI].move(PROGRESS)
+        RacingGame.cars[POBI].move(PROGRESS)
+        assertThat(RacingGame.getWinnerNames()).isEqualTo(listOf("woni", "pobi"))
     }
 
     companion object {
