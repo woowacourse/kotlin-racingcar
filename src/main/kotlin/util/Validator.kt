5c37c20e4d51a99050d718bb501afb617128b12d
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-12 23:44:49 +0900

    feat(Validator): 시도할 횟수가 1회 이상 100회 이하가 아닌 경우 예외 처리 기능 추가

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -17,6 +17,7 @@ class Validator {
     fun checkTryNumber(name: String?) {
         checkTryNumberNull(name)
         checkTryNumberIsRight(name)
+        checkTryNumberLimit(name)
     }
 
     private fun checkNameNull(name: String?) {
@@ -43,11 +44,16 @@ class Validator {
         require(number!!.toIntOrNull() != null) { INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE }
     }
 
+    private fun checkTryNumberLimit(number: String?) {
+        require(number!!.toInt() in 1..100) { INPUT_TRY_NUMBER_LIMIT_ERROR_MESSAGE }
+    }
+
     companion object {
         const val INPUT_NAME_SIZE_ERROR_MESSAGE = "자동차 이름의 길이가 5를 초괴합니다."
         const val INPUT_NAME_NULL_ERROR_MESSAGE = "자동차 이름이 입력되지 않았습니다."
         const val INPUT_TRY_NUMBER_NULL_ERROR_MESSAGE = "시도할 횟수가 입력되지 않았습니다."
         const val INPUT_TRY_NUMBER_RIGHT_ERROR_MESSAGE = "시도할 횟수가 올바르게 입력되지 않았습니다."
         const val INPUT_NAME_RIGHT_ERROR_MESSAGE = "자동차 이름은 한글과 영어만 가능합니다."
+        const val INPUT_TRY_NUMBER_LIMIT_ERROR_MESSAGE = "시도할 횟수가 1번 이상 100번 이하여야 합니다."
     }
 }
