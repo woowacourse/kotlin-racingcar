58bb6c9f446d854b027a064bd20c8565994d372d
ki960213 <ki960213@github.com>
2023-02-12 22:10:45 +0900

    refactor(view): 전진 상태 바 생성 로직을 함수로 변경

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -7,7 +7,7 @@ object OutputView {
     fun printMessage(message: String) = println(message)
     fun printGameStatus(cars: List<Car>) {
         cars.forEach { car ->
-            println("${car.name} : ${"-".repeat(car.advanceCount)}")
+            println("${car.name} : ${car.getAdvancedStatusBar()}")
         }
         println()
     }
@@ -15,5 +15,7 @@ object OutputView {
     fun printGameResult(winCars: List<Car>) =
         println(GAME_RESULT_FORMAT.format(winCars.joinToString(", ") { it.name }))
 }
+
+private fun Car.getAdvancedStatusBar() = "-".repeat(this.advanceCount)
 
 private const val GAME_RESULT_FORMAT = "최종 우승자: %s"
