43a64f9f910afe98b62f20d4ca182a8b7572f4f8
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-12 18:43:51 +0900

    refactor(RaceInfo) : data class 적용 해제

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -2,7 +2,6 @@ package racingcar.controller
 
 import racingcar.domain.RaceManager
 import racingcar.racingcar.domain.Cars
-import racingcar.racingcar.domain.RaceInfo
 import racingcar.racingcar.domain.raceresult.toDto
 import racingcar.view.InputView
 import racingcar.view.OutputView
@@ -14,21 +13,10 @@ class GameController(
     private val output by lazy { OutputView() }
 
     fun runGame() {
-        val raceInfo = initGame()
-        startGame(raceInfo)
-        endGame(raceInfo)
-    }
-
-    private fun initGame(): RaceInfo {
-        return RaceInfo(Cars(input.inputCarNames()), input.inputRacingCount())
-    }
-
-    private fun startGame(raceInfo: RaceInfo) {
-        val raceResult = raceManager.race(raceInfo.cars, raceInfo.racingCount)
+        val cars = Cars(input.inputCarNames())
+        val racingCount = input.inputRacingCount()
+        val raceResult = raceManager.race(cars, racingCount)
         output.printRaceResult(raceResult.toDto())
-    }
-
-    private fun endGame(raceInfo: RaceInfo) {
-        output.printWinner(raceManager.getWinners(raceInfo.cars))
+        output.printWinner(raceManager.getWinners(cars))
     }
 }
--- src/main/kotlin/racingcar/domain/RaceInfo.kt
+++
@@ -1,6 +0,0 @@
-package racingcar.racingcar.domain
-
-data class RaceInfo(
-    val cars: Cars,
-    val racingCount: Int,
-)
