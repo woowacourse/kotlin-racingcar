e1482f1d38ae5d60990c3196faaa160dc53bc4e8
Gloria <ippnsj@gmail.com>
2023-02-12 20:41:54 +0900

    refactor: SAM 익명 클래스로 전달

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -6,18 +6,16 @@ import org.junit.jupiter.api.Test
 class CarTest {
     @Test
     fun `자동차 이동 테스트`() {
-        val generator = TestNumberGenerator(listOf(2, 4, 3, 9, 5, 0))
+        val generator = object : NumberGenerator {
+            private val numbers = mutableListOf(2, 4, 3, 9, 5, 0)
+
+            override fun generate(): Int {
+                return numbers.removeAt(0)
+            }
+        }
         val name = "pobi"
         val car = Car(generator, name)
         val distances = listOf(0, 1, 1, 2, 3, 3)
         distances.map { distance -> assertThat(car.race().getDistance()).isEqualTo(distance) }
-    }
-
-    class TestNumberGenerator(numbers: List<Int>) : NumberGenerator {
-        private val numbers = numbers.toMutableList()
-
-        override fun generate(): Int {
-            return numbers.removeAt(0)
-        }
     }
 }
