d3c893216a1072ec026cc36000c439a76b68c9ca
Sujin Kim <sujinkim919@gmail.com>
2023-02-12 16:31:32 +0900

    refactor(racingcar): 상수 분리

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,9 +1,5 @@
 package racingcar.view
 
-import racingcar.constant.INPUT_CARS_NAME
-import racingcar.constant.INPUT_ROUND_COUNT
-import racingcar.constant.OUTPUT_RESULT
-import racingcar.constant.OUTPUT_WINNER
 import racingcar.constant.STATE_UNIT
 import racingcar.domain.RacingCar
 
@@ -29,5 +25,13 @@ class OutputView {
 
     fun printWinner(winner: List<String>) {
         println("$OUTPUT_WINNER${winner.joinToString(separator = ", ")}")
+    }
+
+    companion object {
+        const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+        const val INPUT_ROUND_COUNT = "시도할 횟수는 몇 회인가요?"
+
+        const val OUTPUT_RESULT = "\n실행 결과"
+        const val OUTPUT_WINNER = "최종 우승자: "
     }
 }
