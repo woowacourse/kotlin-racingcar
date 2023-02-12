51b97a9cd208903f90df6ec6964a9fb8e4bd83db
ki960213 <ki960213@github.com>
2023-02-12 21:44:55 +0900

    refactor(domain): GameCars 클래스의 필요없는 코드 제거

--- src/main/kotlin/domain/GameCars.kt
+++ src/main/kotlin/domain/GameCars.kt
@@ -21,4 +21,3 @@ private fun List<Car>.isNotDuplicated(): Boolean = this.size == this.distinct().
 
 private const val CAR_DISTINCT_ERROR = "자동차 이름 간에 중복이 존재합니다."
 private const val CARS_SIZE_ERROR = "자동차는 최소 2대에서 최대 20대 사이여야 합니다."
-private const val ADVANCE_COUNT_ERROR = "한 게임 동안 전진 횟수는 최소 1회에서 최대 100회 사이여야 합니다."
