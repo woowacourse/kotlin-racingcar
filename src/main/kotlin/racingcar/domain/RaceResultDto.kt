20399d0273f9c920fa08f0635bd9e42f52176023
hyunji1203 <hj27720@gmail.com>
2023-02-12 23:31:11 +0900

    refactor (RaceResultDto) : 중복된 불필요한 데이터 삭제

--- src/main/kotlin/racingcar/domain/RaceResultDto.kt
+++ src/main/kotlin/racingcar/domain/RaceResultDto.kt
@@ -1,9 +1,6 @@
 package racingcar.racingcar.domain
 
-import racingcar.domain.Car
-
 data class RaceResultDto(
     val names: List<String>,
-    val result: List<List<Int>>,
-    val cars: List<Car>
+    val result: List<List<Int>>
 )
