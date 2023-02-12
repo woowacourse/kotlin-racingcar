7c1f0738bb6a4be9bb449deb525d8637929f1926
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-12 23:41:50 +0900

    feat(Cars): 자동차의 수가 2대 이상 20대 이하가 아닌 경우 예외 처리 기능 추가

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -6,6 +6,7 @@ class Cars(cars: List<Car>, private val numberGenerator: NumberGenerator) {
 
     init {
         require(cars.toSet().size == cars.size) { CAR_NAME_OVERLAP_ERROR_MESSAGE }
+        require(cars.size in CARS_MIN_LIMIT_SIZE..CARS_MAX_LIMIT_SIZE) { CARS_SIZE_ERROR_MESSAGE }
     }
 
     var cars: List<Car> = cars.toList()
@@ -23,5 +24,8 @@ class Cars(cars: List<Car>, private val numberGenerator: NumberGenerator) {
     companion object {
         fun mappingCars(input: String, numberGenerator: NumberGenerator) = Cars(input.split(",").mapIndexed { _, name -> Car(name.trim()) }, numberGenerator)
         const val CAR_NAME_OVERLAP_ERROR_MESSAGE = "자동차 이름이 중복되었습니다."
+        const val CARS_MIN_LIMIT_SIZE = 2
+        const val CARS_MAX_LIMIT_SIZE = 20
+        const val CARS_SIZE_ERROR_MESSAGE = "자동차의 수는 2대 이상 20대 이하여야 합니다."
     }
 }
