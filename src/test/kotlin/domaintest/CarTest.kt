771af77ef6189815155865bd326425d63596c4c6
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-12 15:53:10 +0900

    refactor : 유효한 자동차의 이름 확인하기

--- src/test/kotlin/domaintest/CarTest.kt
+++ src/test/kotlin/domaintest/CarTest.kt
@@ -2,12 +2,15 @@ package domaintest
 
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CarTest : BaseTest() {
 
     @ParameterizedTest
@@ -18,8 +21,16 @@ class CarTest : BaseTest() {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("generateValidCarName")
+    fun `유효한 자동차 이름 확인하기`(names: List<String>) {
+        assertDoesNotThrow {
+            carGenerator.generateCars(names)
+        }
+    }
+
     @Test
-    private fun `정지 테스트 `() {
+    fun `정지 테스트 `() {
         stopCars.forEach { stopCar ->
             stopCar.move()
         }
@@ -38,8 +49,8 @@ class CarTest : BaseTest() {
         }
 
         assertThat(
-            stopCars.filter { stopCar ->
-                stopCar.position == 1
+            movingCars.filter { movingCar ->
+                movingCar.position == 1
             }
         ).isEqualTo(movingCars)
     }
@@ -51,6 +62,20 @@ class CarTest : BaseTest() {
             ),
             Arguments.of(
                 listOf("배달의 민족", "곽두팔")
+            ),
+            Arguments.of(
+                listOf("김진욱", "******", "      ")
+            ),
+        )
+    }
+
+    private fun generateValidCarName(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of(
+                listOf("제 임스", "우기", "스x캇")
+            ),
+            Arguments.of(
+                listOf("김*욱", "안  기효")
             ),
         )
     }
