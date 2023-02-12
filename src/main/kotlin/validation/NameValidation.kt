5a155b011307df166b1b9073e28aa770b8e4a343
chws0508 <chws0508@naver.com>
2023-02-12 16:18:39 +0900

    refactor : Validation 클래스를 ValidationResult를 반환하도록 수정

--- src/main/kotlin/validation/NameValidation.kt
+++ src/main/kotlin/validation/NameValidation.kt
@@ -1,29 +1,28 @@
 package validation
 
 class NameValidation {
-    private val nullException = NullValidation()
-    fun checkNames(carNames: String) {
+    fun checkNames(carNames: String?): ValidationResult {
+        if (carNames.isNullOrBlank()) return ValidationResult.Failure(INPUT_IS_EMPTY_ERROR_MESSAGE)
         splitCarNames(carNames).forEach { name ->
-            checkNameRange(nullException.checkNull(name))
-            checkCarNameEnglishNumber(name)
+            if (isNameLengthExceed(name)) return ValidationResult.Failure(NAME_RANGE_ERROR_MASSAGE)
+            if (!isCarNameEnglishNumber(name)) return ValidationResult.Failure(NAME_IS_ONLY_ENGLISH_AND_NUMBER)
         }
+        return ValidationResult.Success(carNames)
     }
 
-    fun checkNameRange(carName: String) = require(carName.length <= MAXIMUM_NAME_LENGTH) { NAME_RANGE_ERROR_MASSAGE }
+    fun isNameLengthExceed(carName: String) = carName.length > MAXIMUM_NAME_LENGTH
 
-    private fun splitCarNames(carNames: String): List<String> {
+    fun splitCarNames(carNames: String): List<String> {
         return carNames.split(",")
     }
 
-    fun checkCarNameEnglishNumber(carName: String) {
-        if (!Regex(ONLY_ENGLISH_NUMBER_REGEX).matches(carName))
-            throw IllegalArgumentException(NAME_IS_ONLY_ENGLISH_AND_NUMBER)
-    }
+    fun isCarNameEnglishNumber(carName: String) = Regex(ONLY_ENGLISH_NUMBER_REGEX).matches(carName)
 
     companion object {
         const val NAME_IS_ONLY_ENGLISH_AND_NUMBER = "[ERROR] 이름은 영어와 숫자만 가능합니다."
         const val NAME_RANGE_ERROR_MASSAGE = "[ERROR] 자동차 이름은 1자 이상 5자 이하로 입력해주세요"
         const val ONLY_ENGLISH_NUMBER_REGEX = "^[a-zA-Z0-9]*\$"
         const val MAXIMUM_NAME_LENGTH = 5
+        const val INPUT_IS_EMPTY_ERROR_MESSAGE = "[ERROR] 아무것도 입력하지 않았습니다."
     }
 }
--- src/main/kotlin/validation/TryCountValidation.kt
+++ src/main/kotlin/validation/TryCountValidation.kt
@@ -1,22 +1,23 @@
 package validation
 
 class TryCountValidation {
-    fun checkTryCount(tryCount: String) {
-        checkTryCountInteger(tryCount)
-        checkTryCountPositive(tryCount)
+    fun checkTryCount(tryCount: String?): ValidationResult {
+        if (tryCount.isNullOrBlank()) return ValidationResult.Failure(INPUT_IS_EMPTY_ERROR_MESSAGE)
+        if (!isTryCountInteger(tryCount)) return ValidationResult.Failure(TRY_COUNT_INTEGER_ERROR_MESSAGE)
+        if (!isTryCountPositive(tryCount)) return ValidationResult.Failure(TRY_COUNT_POSITIVE_ERROR_MESSAGE)
+        return ValidationResult.Success(tryCount)
     }
 
-    fun checkTryCountInteger(tryCount: String) {
-        tryCount.toIntOrNull() ?: throw IllegalArgumentException(TRY_COUNT_INTEGER_ERROR_MESSAGE)
+    fun isTryCountInteger(tryCount: String): Boolean {
+        tryCount.toIntOrNull() ?: return false
+        return true
     }
 
-    fun checkTryCountPositive(tryCount: String) {
-        if (tryCount.toInt() < MINIMUM_POSITIVE_NUMBER)
-            throw IllegalArgumentException(TRY_COUNT_POSITIVE_ERROR_MESSAGE)
-    }
+    fun isTryCountPositive(tryCount: String) = tryCount.toInt() < MINIMUM_POSITIVE_NUMBER
 
     companion object {
         const val MINIMUM_POSITIVE_NUMBER = 1
+        const val INPUT_IS_EMPTY_ERROR_MESSAGE = "[ERROR] 아무것도 입력하지 않았습니다."
         const val TRY_COUNT_INTEGER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자만 입력해 주세요"
         const val TRY_COUNT_POSITIVE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양의 정수여야 합니다"
     }
