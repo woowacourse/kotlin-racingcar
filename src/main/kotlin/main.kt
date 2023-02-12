336da52eb476882519b3ed6777593538e3ac71f9
ki960213 <ki960213@github.com>
2023-02-12 22:07:38 +0900

    fix(controller): 실행결과 출력 문자열 변경

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -58,4 +58,4 @@ private fun printGameResult() {
 
 private const val CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
 private const val ADVANCE_COUNT_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?"
-private const val GAME_RESULT_HEADER = "실행 결과"
+private const val GAME_RESULT_HEADER = "\n실행 결과"
