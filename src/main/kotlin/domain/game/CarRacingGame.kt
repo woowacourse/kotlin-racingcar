72395cd0e6cb84fb0798f90929369fd394dee6aa
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-13 20:13:01 +0900

    refactor : 이전 커밋의 클래스명을 가지고 있는 변수 수정

--- src/main/kotlin/domain/game/CarRacingGame.kt
+++ src/main/kotlin/domain/game/CarRacingGame.kt
@@ -3,12 +3,12 @@ package domain.game
 import model.Car
 
 class CarRacingGame(
-    private val referee: CarRacingGameWinner = CarRacingGameWinner()
+    private val carRacingGameWinner: CarRacingGameWinner = CarRacingGameWinner()
 ) {
 
     fun moveCarsOneCycle(cars: List<Car>) = cars.forEach { car ->
         car.move()
     }
 
-    fun getWinners(cars: List<Car>) = referee.judgeWinners(cars)
+    fun getWinners(cars: List<Car>) = carRacingGameWinner.judgeWinners(cars)
 }
