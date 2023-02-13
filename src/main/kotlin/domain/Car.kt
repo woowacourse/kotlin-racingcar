9e38bd9ed092a7f0b883da0909cc9f13c16e7fa7
ki960213 <ki960213@github.com>
2023-02-13 14:48:56 +0900

    refactor(domain): Car 객체를 생성할 때 잘못된 입력을 받으면 그 값을 출력하도록 변경

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -5,7 +5,9 @@ import common.ERROR_MESSAGE_FORMAT
 class Car(val name: String, private val movingStrategy: MovingStrategy) {
 
     init {
-        require(name.matches(carNameRegex)) { ERROR_MESSAGE_FORMAT.format(CAR_NAME_REGEX_ERROR) }
+        require(name.matches(carNameRegex)) {
+            ERROR_MESSAGE_FORMAT.format(CAR_NAME_REGEX_ERROR).plus("\n${ILLEGAL_INPUT_NAME_FORMAT.format(name)}")
+        }
     }
 
     var advancedCount: Int = 0
@@ -24,3 +26,4 @@ class Car(val name: String, private val movingStrategy: MovingStrategy) {
 }
 
 private const val CAR_NAME_REGEX_ERROR = "자동차의 이름은 한글 혹은 영어로 공백이 없어야 하며 최소 1글자에서 최대 5글자까지 가능합니다."
+private const val ILLEGAL_INPUT_NAME_FORMAT = "잘못된 사용자의 입력: %s"
