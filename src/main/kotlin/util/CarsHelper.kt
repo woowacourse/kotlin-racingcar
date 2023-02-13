1b39386fdd83229a1c75b92d88a8d58867f9c213
hyemdooly <hyemdooly@gmail.com>
2023-02-13 22:59:33 +0900

    refactor(CarsHelper): listOf() -> emptyList()로 변경

--- src/main/kotlin/util/CarsHelper.kt
+++ src/main/kotlin/util/CarsHelper.kt
@@ -5,6 +5,6 @@ import model.Cars
 object CarsHelper {
     fun findWinners(cars: Cars): List<String> {
         val equalCars = cars.cars.groupBy({ it.position }, { it.name })
-        return equalCars[equalCars.keys.max()]?.toList() ?: listOf()
+        return equalCars[equalCars.keys.max()]?.toList() ?: emptyList()
     }
 }
