27de67683434e77a3cac8860bd15c475e3a5ee91
vrexpert <vrexpert2302@gmail.com>
2023-02-13 22:42:46 +0900

    refactor: 테스트 명을 보다 구체적으로 변경

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -30,14 +30,14 @@ class CarTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["Pobi", " jun", "po bi", "   "])
-    fun `자동차 이름에 공백이 있거나 영문 소문자가 아니면 에러가 발생한다`(input: String) {
+    fun `자동차 이름은 영문 소문자를 제외한 공백이나 대문자로 지을 수 없다`(input: String) {
         val exception = assertThrows<IllegalArgumentException> { Car(numberGenerator, input, 0) }
         assertThat(exception.message).isEqualTo(Car.ERROR_NAME)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["woniii", ""])
-    fun `자동차 이름이 1글자 이상 5글자 이하가 아니면 에러가 발생한다`(input: String) {
+    fun `자동차 이름은 글자가 없거나 6글자 이상일 수 없다`(input: String) {
         val exception = assertThrows<IllegalArgumentException> { Car(numberGenerator, input, 0) }
         assertThat(exception.message).isEqualTo(Car.ERROR_NAME_LENGTH)
     }
--- src/test/kotlin/domain/RaceTimeTest.kt
+++ src/test/kotlin/domain/RaceTimeTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -8,14 +9,14 @@ import org.junit.jupiter.params.provider.ValueSource
 class RaceTimeTest {
     @ParameterizedTest
     @ValueSource(ints = [1, 5, 10])
-    fun `경주 시도 횟수가 10 이하의 자연수이면 에러가 발생하지 않는다`(input: Int) {
+    fun `경주는 10회 이하로만 반복할 수 있다`(input: Int) {
         Assertions.assertDoesNotThrow { RaceTime(input) }
     }
 
     @ParameterizedTest
     @ValueSource(ints = [0, 11])
-    fun `경주 시도 횟수가 1 미만 10 초과면 에러가 발생한다`(input: Int) {
+    fun `경주를 1회 미만 혹은 10회를 초과해서 반복할 수 없다`(input: Int) {
         val exception = assertThrows<IllegalArgumentException> { RaceTime(input) }
-        org.assertj.core.api.Assertions.assertThat(exception.message).isEqualTo(RaceTime.ERROR_NOT_IN_RANGE)
+        assertThat(exception.message).isEqualTo(RaceTime.ERROR_NOT_IN_RANGE)
     }
 }
