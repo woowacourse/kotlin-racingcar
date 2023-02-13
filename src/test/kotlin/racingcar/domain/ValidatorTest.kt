ef0920832bf9a61dc2c0e47cdfc557594b5774d0
changhwan <chang5hustle@gmail.com>
2023-02-13 10:43:54 +0900

    refactor(racingcar): ValidatorTest 함수명 변경

--- src/test/kotlin/racingcar/domain/ValidatorTest.kt
+++ src/test/kotlin/racingcar/domain/ValidatorTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class ValidatorTest {
     @ValueSource(strings = ["", "jinini"])
     @ParameterizedTest
-    fun `자동차 이름 길이 체크하는 함수 NameLengthInRange함수 테스트(자동차 이름 길이는 1~5사이) `(input: String) {
+    fun `자동차 이름 길이 체크하는 NameLengthInRange함수(자동차 이름 길이는 1~5사이) 정상범위의 숫자만 처리하는지 확인 `(input: String) {
         assertThat(
             Validator.isNameLengthInRange(input),
         ).isFalse
@@ -15,7 +15,7 @@ class ValidatorTest {
 
     @ValueSource(strings = ["", "a", "0d9", "-1"])
     @ParameterizedTest
-    fun `실행횟수 입력받기위한 숫자인지 검증함수 테스트`(input: String) {
+    fun `실행횟수 입력받기위한 숫자인지 검증함수 숫자만 처리하는지 확인`(input: String) {
         assertThat(
             Validator.isNumber(input),
         ).isFalse
