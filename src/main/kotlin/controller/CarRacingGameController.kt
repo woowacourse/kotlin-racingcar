b22e1d3d93dce6b248aa207d4087f1e5ff915441
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-13 16:43:24 +0900

    refactor : Validator를 삭제하고 해당 로직을 Controller로 이동

--- src/main/kotlin/controller/CarRacingGameController.kt
+++ src/main/kotlin/controller/CarRacingGameController.kt
@@ -2,7 +2,6 @@ package controller
 
 import domain.game.CarRacingGame
 import domain.generator.CarGenerator
-import domain.validator.ValidateUseCases
 import model.Car
 import model.CarRacingGameDataSource
 import view.InputView
@@ -11,7 +10,6 @@ import view.OutputView
 class CarRacingGameController(
     private val carRacingGame: CarRacingGame = CarRacingGame(),
     private val carGenerator: CarGenerator = CarGenerator(),
-    private val validateUseCases: ValidateUseCases = ValidateUseCases()
 ) {
 
     private val carRacingGameDataSource: CarRacingGameDataSource by lazy {
@@ -28,13 +26,12 @@ class CarRacingGameController(
     }
 
     private fun initCars(): List<Car> {
-        val names = validateUseCases.validateSuccessiveTokenizer(InputView.inputCarNames())
+        val names = InputView.inputCarNames().hasSuccessiveTokenizer()
 
         return carGenerator.generateCars(names)
     }
 
-    private fun initNumberOfTry(): Int =
-        validateUseCases.validateNumberOfTry(InputView.inputNumberOfTry())
+    private fun initNumberOfTry(): Int = InputView.inputNumberOfTry().isNumericNumberOfTry()
 
     private fun start(cars: List<Car>, numberOfTry: Int) {
         repeat(numberOfTry) { count ->
@@ -47,5 +44,34 @@ class CarRacingGameController(
         val winners = carRacingGame.getWinners(cars)
 
         OutputView.printWinner(winners)
+    }
+
+    private fun String.hasSuccessiveTokenizer(): List<String> {
+        val names = this.split(TOKENIZER)
+
+        require(names.hasEmptyString()) {
+            SUCCESSIVE_TOKENIZER_ERROR
+        }
+
+        return names
+    }
+
+    private fun String.isNumericNumberOfTry(): Int {
+        require(this.isNumeric()) {
+            NUMERIC_ERROR
+        }
+
+        return this.toInt()
+    }
+
+    private fun String.isNumeric(): Boolean = this.chars().allMatch { Character.isDigit(it) }
+
+    private fun List<String>.hasEmptyString(): Boolean = this.all { nameInput -> nameInput != EMPTY }
+
+    companion object {
+        private const val SUCCESSIVE_TOKENIZER_ERROR = "[ERROR] 이름 입력시 ,를 연속적으로 입력할 수 없습니다."
+        private const val NUMERIC_ERROR = "[ERROR] 시도 횟수는 숫자 입력으로만 받습니다."
+        private const val TOKENIZER = ","
+        private const val EMPTY = ""
     }
 }
--- src/main/kotlin/domain/validator/ValidateUseCases.kt
+++
@@ -1,6 +0,0 @@
-package domain.validator
-
-data class ValidateUseCases(
-    val validateNumberOfTry: ValidateNumberOfTry = ValidateNumberOfTry(),
-    val validateSuccessiveTokenizer: ValidateSuccessiveTokenizer = ValidateSuccessiveTokenizer()
-)
