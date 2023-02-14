d421306b2350861e86249682911653011142eae8
chws0508 <chws0508@naver.com>
2023-02-14 11:33:53 +0900

    refactor : 고차 함수를 넘겨서 테스트를 진행하도록 수정

--- src/test/kotlin/controller/RacingGameControllerTest.kt
+++ src/test/kotlin/controller/RacingGameControllerTest.kt
@@ -14,8 +14,13 @@ class RacingGameControllerTest {
     @Test
     fun `시도횟수가 3번이고, 차량이 3대이고, 시도 할때마다 랜덤 숫자가 5,6,3 이 반복될 때, 앞의 두 차량만 3번 움직인 상태를 출력한다 `() {
         val numberGenerator = TestNumberGenerator(listOf(5, 6, 3, 5, 6, 3, 5, 6, 3))
+        val expect = listOf("aa", "bb")
         val racingGameController =
-            RacingGameController(TestInputView(), TestOutputView(), RacingCarGameService(numberGenerator))
+            RacingGameController(
+                TestInputView(),
+                TestOutputView { assertThat(it).isEqualTo(expect) },
+                RacingCarGameService(numberGenerator)
+            )
         racingGameController.run()
     }
 
@@ -37,7 +42,7 @@ class RacingGameControllerTest {
         }
     }
 
-    class TestOutputView() : OutputViewInterface {
+    class TestOutputView(val assertWinner: (List<String>) -> Unit) : OutputViewInterface {
         override fun printRoundResult(roundResult: String) {
             val acutal = roundResult
             val expect = "aa : -\n" +
@@ -53,7 +58,7 @@ class RacingGameControllerTest {
         }
 
         override fun printWinner(winners: List<String>) {
-            assertThat(winners).isEqualTo(listOf("aa", "bb"))
+            assertWinner(winners)
         }
 
         override fun printRunResultMessage() {
