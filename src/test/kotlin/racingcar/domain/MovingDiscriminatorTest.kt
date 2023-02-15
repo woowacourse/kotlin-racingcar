2888778c679df9a2052a014d811cccff089b3c19
changhwan <chang5hustle@gmail.com>
2023-02-15 13:44:19 +0900

    refactor(racingcar): 테스트 함수명 변경

--- src/test/kotlin/racingcar/domain/MovingDiscriminatorTest.kt
+++ src/test/kotlin/racingcar/domain/MovingDiscriminatorTest.kt
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test
 class MovingDiscriminatorTest {
 
     @Test
-    fun `4이상이면 전진한다`() {
+    fun `4이상이면 전진한다 범위(4~9)`() {
         Assertions.assertThat(
             MovingDiscriminator.checkGoingForward(4),
         ).isTrue
     }
 
     @Test
-    fun `3이하이면 전진하지 않는다`() {
+    fun `3이하이면 전진하지 않는다(0~3)`() {
         Assertions.assertThat(
             MovingDiscriminator.checkGoingForward(3),
         ).isFalse
