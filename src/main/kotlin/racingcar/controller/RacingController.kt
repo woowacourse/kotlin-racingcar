2ce563b246cf45e123a0bf3cfc6d16c28fb96e00
otter <a33397397@gmail.com>
2023-02-19 17:34:31 +0900

    refactor: OutputController class가 controller의 성격에 맞지 않는 문제 해결
    - RacingController class가 OutputController class를 제어하지 않도록 OutputController의 역할을 OutputView가 할 수 있도록 수정

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -2,10 +2,11 @@ package racingcar.controller
 
 import racingcar.domain.Cars
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class RacingController(
     private val inputView: InputView = InputView(),
-    private val outputController: OutputController = OutputController(),
+    private val outView: OutputView = OutputView(),
 ) {
 
     fun runRacing() {
@@ -15,11 +16,11 @@ class RacingController(
         runRounds(roundCount, cars)
 
         val winners = cars.getWinners()
-        outputController.printWinners(winners.cars)
+        outView.printWinners(winners.cars)
     }
 
     private fun runRounds(roundCount: Int, cars: Cars) {
-        outputController.printRunResult()
+        outView.printRunResult()
         repeat(roundCount) {
             runRound(cars)
         }
@@ -27,6 +28,6 @@ class RacingController(
 
     private fun runRound(cars: Cars) {
         cars.moveCarsRandomly()
-        outputController.printRoundResult(cars.cars)
+        outView.printRoundResult(cars)
     }
 }
--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,15 +1,28 @@
 package racingcar.view
 
 import racingcar.domain.Car
+import racingcar.domain.Cars
 
 class OutputView {
-    fun printMessage(message: String) = println(message)
 
-    fun printRoundResult(cars: List<Car>) {
-        cars.forEach { car ->
-            println("${car.name} : ${car.getPositionAsDash()}")
+    fun printRoundResult(cars: Cars) {
+        cars.cars.forEach { car ->
             println("$car : ${car.getPositionAsDash()}")
         }
         println()
+    }
+
+    fun printRunResult() {
+        println()
+        println(ROUNDS_RESULT_NOTIFICATION_MESSAGE)
+    }
+
+    fun printWinners(winners: List<Car>) {
+        println("$WINNER_NOTIFICATION_MESSAGE: ${winners.joinToString(", ")}")
+    }
+
+    companion object {
+        private const val ROUNDS_RESULT_NOTIFICATION_MESSAGE = "실행 결과"
+        private const val WINNER_NOTIFICATION_MESSAGE = "최종 우승자"
     }
 }
