c769800c3c58e5a926dbcb122e3207e1e219815f
songusika <sws981202@gmail.com>
2023-04-18 22:08:23 +0900

    refactor: private 키워드 삭제

--- src/main/kotlin/racingCar/domain/Name.kt
+++ src/main/kotlin/racingCar/domain/Name.kt
@@ -1,6 +1,6 @@
 package racingCar.domain
 
-data class Name(private val name: String) {
+data class Name(val name: String) {
     init {
         if (name.isEmpty() || name.length > 5) {
             throw IllegalArgumentException()
