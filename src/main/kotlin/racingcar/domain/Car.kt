17abec7de916be1646aacdfb1601bbd0450651c4
drunkenhw <drunkenhw@gmail.com>
2023-04-18 14:28:24 +0900

    feat: 이름의 길이를 검증하는 기능 추가

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,6 +1,15 @@
 package racingcar.domain
 
 class Car(private val name: String, private val position: Int) {
+    init {
+        validate(name)
+    }
+
+    private fun validate(name: String) {
+        if (name.length >= 5) {
+            throw IllegalArgumentException("이름은 5자 이하만 가능합니다")
+        }
+    }
 
     fun move(number: Int): Car {
         if (number > 4) {
