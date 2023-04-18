ea713e691557d0b39152d731fedf6265a54f70f3
songusika <sws981202@gmail.com>
2023-04-18 22:07:38 +0900

    feat: 게임 시도 회수가 남았는지 검사하는 기능 구현

--- src/main/kotlin/racingCar/domain/Count.kt
+++ src/main/kotlin/racingCar/domain/Count.kt
@@ -1,9 +1,17 @@
 package racingCar.domain
 
-data class Count(private val count: Int) {
+data class Count(private var count: Int) {
     init {
-        if (count <= 0) {
-            throw IllegalArgumentException()
+        if (this.count <= 0) {
+            throw IllegalArgumentException("숫자를 입력하세요")
         }
+    }
+
+    fun deduct() {
+        this.count = this.count.dec()
+    }
+
+    fun isOpportunity(): Boolean {
+        return this.count > 0
     }
 }
