5672bf06ea75dddec11bc67246d20162e7ac6139
songusika <sws981202@gmail.com>
2023-04-18 22:08:51 +0900

    feat: 잘못된 입력에 대한 예외 처리

--- src/main/kotlin/racingCar/view/InputView.kt
+++ src/main/kotlin/racingCar/view/InputView.kt
@@ -2,17 +2,17 @@ package racingCar.view
 
 class InputView {
 
-    fun readCarNames(): List<String>? {
+    fun readCarNames(): List<String> {
         println("자동차 이름을 입력해주세요")
-        return splitCarNames(readLine())
+        return splitCarNames(readLine()) ?: throw NullPointerException("이름을 입력하세요")
     }
 
-    private fun splitCarNames(input : String?): List<String>? {
+    private fun splitCarNames(input: String?): List<String>? {
         return input?.split(",")
     }
 
-    fun readTryCount(): Int? {
+    fun readTryCount(): Int {
         println("시도 횟수를 입력해주세요")
-        return readLine()?.toInt()
+        return readLine()?.toInt() ?: throw NullPointerException("숫자를 입력하세요")
     }
 }
