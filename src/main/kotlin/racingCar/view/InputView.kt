3c32ae04af134302e034df150fea3d7c3daa2c33
songusika <sws981202@gmail.com>
2023-04-19 16:45:32 +0900

    refactor: 사용자 입력 예외 변경

--- src/main/kotlin/racingCar/view/InputView.kt
+++ src/main/kotlin/racingCar/view/InputView.kt
@@ -4,7 +4,7 @@ class InputView {
 
     fun readCarNames(): List<String> {
         println("자동차 이름을 입력해주세요")
-        return splitCarNames(readLine()) ?: throw NullPointerException("이름을 입력하세요")
+        return splitCarNames(readLine()) ?: throw IllegalArgumentException("이름을 입력하세요")
     }
 
     private fun splitCarNames(input: String?): List<String>? {
@@ -13,6 +13,6 @@ class InputView {
 
     fun readTryCount(): Int {
         println("시도 횟수를 입력해주세요")
-        return readLine()?.toInt() ?: throw NullPointerException("숫자를 입력하세요")
+        return readLine()?.toInt() ?: throw IllegalArgumentException("숫자를 입력하세요")
     }
 }
