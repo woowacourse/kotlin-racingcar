45e09af679fc0c88273e60313bf1641fdca99c17
songusika <sws981202@gmail.com>
2023-04-19 16:46:05 +0900

    refactor: 에러 메시지 추가

--- src/main/kotlin/racingCar/domain/Name.kt
+++ src/main/kotlin/racingCar/domain/Name.kt
@@ -3,7 +3,7 @@ package racingCar.domain
 data class Name(val name: String) {
     init {
         if (name.isEmpty() || name.length > 5) {
-            throw IllegalArgumentException()
+            throw IllegalArgumentException("이름을 다시 입력해주세요")
         }
     }
 }
