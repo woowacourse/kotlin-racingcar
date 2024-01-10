e0c3274c9a57b23bd48f8f2b9837c4c115a9e20a
이승민 <sml7179@naver.com>
2024-01-10 17:30:18 +0900

    test: var val 차이 스터디 추가

---
+++ src/test/kotlin/study/ValVar차이.kt
@@ -0,0 +1,20 @@
+package study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ValVar차이 {
+
+    @Test
+    fun val_var_차이() {
+        val popcorn = 5;
+
+        // popcorn = 6; // compile error
+
+        var sandwhich = 6;
+
+        sandwhich = 5;
+
+        assertThat(sandwhich).isEqualTo(popcorn);
+    }
+}
