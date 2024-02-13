b99883679dc1865225d4177a2ddb4ab0e565a56e
woowahan-pjs <jason@woowahan.com>
2024-02-13 13:55:03 +0900

    build: upgrade to kotlin 1.9.0 and gradle 8.2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 자동차 경주 게임
+# kotlin-racingcar
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm") version "1.9.0"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 group = "camp.nextstep.edu"
@@ -11,17 +13,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj", "assertj-core", "3.25.3")
+    testImplementation("io.kotest", "kotest-runner-junit5", "5.8.0")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
     }
     test {
         useJUnitPlatform()
