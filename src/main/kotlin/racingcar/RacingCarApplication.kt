233b0e681800bbae69fe5065ff7d393ba7284391
somin <salth0601@gmail.com>
2024-02-14 19:44:11 +0900

    feat: 게임에 필요한 메시지 출력 메소드 추가
    Co-authored-by: kmkim2689 <101035437+kmkim2689@users.noreply.github.com>

--- src/main/kotlin/racingcar/RacingCarApplication.kt
+++ src/main/kotlin/racingcar/RacingCarApplication.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import racingcar.controller.RacingController
+import racingcar.view.OutputView
 
 fun main() {
     RacingController.start()
---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,23 @@
+package racingcar.view
+
+import racingcar.constants.Constants.CURRENT_POSITION_TEMPLATE
+import racingcar.constants.Constants.FINAL_WINNERS
+import racingcar.constants.Constants.INPUT_CAR_NAMES_MESSAGE
+import racingcar.constants.Constants.INPUT_TRIAL_NUM_MESSAGE
+import racingcar.constants.Constants.PROGRESS_BAR
+import racingcar.constants.Constants.TRIAL_RESULT_MESSAGE
+
+object OutputView {
+    fun printInputCarNamesMessage() = println(INPUT_CAR_NAMES_MESSAGE)
+
+    fun printInputTrialNumMessage() = println(INPUT_TRIAL_NUM_MESSAGE)
+
+    fun printTrialResultMessage() = println(TRIAL_RESULT_MESSAGE)
+
+    fun printFinalWinnersMessage() = println(FINAL_WINNERS)
+
+    fun printCurrentPosition(
+        name: String,
+        position: Int
+    ) = println(CURRENT_POSITION_TEMPLATE.format(name, PROGRESS_BAR.repeat(position)))
+}
