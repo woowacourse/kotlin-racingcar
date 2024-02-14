0a562c71e6bef27ba817200ada276216f95998e4
lee989898 <wnsrud1352@gmail.com>
2024-02-14 15:11:00 +0900

    feat: 랜덤 전진조건을 담은 리스트를 반환 기능 구현

---
+++ src/main/kotlin/racingcar/controller/RandomNumberGenerator.kt
@@ -0,0 +1,21 @@
+package racingcar.controller
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+
+class RandomNumberGenerator(
+    private val numberOfCars: Int,
+) {
+
+    fun putRandomNumbers(): List<Int> {
+        val randomNumbers: MutableList<Int> = mutableListOf()
+
+        for (index in 0 until numberOfCars) {
+            val randomNumber: Int = Random.nextInt(0..9)
+            randomNumbers.add(randomNumber)
+        }
+
+        return randomNumbers
+    }
+}
