ebbc8e03ed62f18e8b1926995f069c1327c49857
songpink <jejeje04@naver.com>
2024-02-14 18:29:36 +0900

    fix: 출력 템플릿에 맞도록 수정

--- src/main/kotlin/racingcar/model/Messages.kt
+++ src/main/kotlin/racingcar/model/Messages.kt
@@ -5,6 +5,6 @@ object Messages {
     const val ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     const val ENTER_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
     const val EXECUTION_RESULTS = "실행 결과"
-    const val LAST_WINNER = "최종 우승자:"
+    const val LAST_WINNER = "최종 우승자: "
     const val DASH = "-"
 }
