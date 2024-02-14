737a5acd660e5ba3c9b8cc916ad2e6bb35ddd243
lee989898 <wnsrud1352@gmail.com>
2024-02-14 15:21:38 +0900

    feat: 메시지 출력기능 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,5 +3,8 @@ package racingcar.view
 import racingcar.model.Messages
 
 class OutputView {
-    fun printChart() = println(Messages.EXECUTION_RESULTS)
+    fun printEnterCarNames() = println(Messages.ENTER_CAR_NAMES)
+    fun enterNumberOfAttempts() = println(Messages.ENTER_NUMBER_OF_ATTEMPTS)
+    fun printExecutionResults() = println(Messages.EXECUTION_RESULTS)
+    fun printLastWinner() = println(Messages.LAST_WINNER)
 }
