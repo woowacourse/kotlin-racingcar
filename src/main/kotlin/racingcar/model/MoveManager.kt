95c980d501c14c93aae7221793b5e688bc901229
somin <salth0601@gmail.com>
2024-02-14 19:22:04 +0900

    chore: 이동 결정 관련 메소드의 상수 추가
    Co-authored-by: kmkim2689 <101035437+kmkim2689@users.noreply.github.com>

--- src/main/kotlin/racingcar/model/MoveManager.kt
+++ src/main/kotlin/racingcar/model/MoveManager.kt
@@ -3,5 +3,12 @@ package racingcar.model
 import kotlin.random.Random
 
 class MoveManager {
-    fun isMoveAble(): Boolean = Random.nextInt(0, 10) > 4
+    fun isMoveAble(): Boolean =
+        Random.nextInt(MINIMUM_RANDOM_SELECTION_NUM, MAXIMUM_RANDOM_SELECTION_NUM) > STOP_THRESHOLD
+
+    companion object {
+        private const val MINIMUM_RANDOM_SELECTION_NUM = 0
+        private const val MAXIMUM_RANDOM_SELECTION_NUM = 10
+        private const val STOP_THRESHOLD = 4
+    }
 }
