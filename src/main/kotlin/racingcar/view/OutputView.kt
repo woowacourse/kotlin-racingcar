fe4de4499271222ec0d2adb109bcb35fe9146a4a
pipeline <sorkwhwo@naver.com>
2024-02-14 17:22:26 +0900

    refactor: 라운드별 결과 출력 기능 상세 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,9 +3,15 @@ package racingcar.view
 import racingcar.domain.Car
 
 class OutputView {
+    fun printResultMessage() {
+        println("실행 결과")
+    }
+
     fun printRoundResult(cars : List<Car>){
         cars.forEach{
             println("${it.name} : ${"-".repeat(it.position)}")
         }
+        println()
     }
+
 }
