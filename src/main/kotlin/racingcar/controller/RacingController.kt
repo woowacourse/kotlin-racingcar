f21d9101cfe546c2c62105e2ddf5c0ae07fbef59
somin <salth0601@gmail.com>
2024-02-14 19:17:46 +0900

    feat: Car 객체 생성 후 이동 로직 구현
    Co-authored-by: kmkim2689 <101035437+kmkim2689@users.noreply.github.com>

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -1,11 +1,22 @@
 package racingcar.controller
 
+import racingcar.model.Car
+import racingcar.model.MoveManager
 import racingcar.view.InputView
 
 object RacingController {
+    private val manager = MoveManager()
     fun start() {
         val names = InputView.readNames()
         val trialNum = InputView.readTrialNum()
+        val cars = names.map { Car(it) }
+        repeat(trialNum) {
+            play(cars)
+            println(cars.toString())
+        }
+    }
 
+    private fun play(cars: List<Car>) {
+        cars.map { if (manager.isMoveAble()) it.position++ }
     }
 }
---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,6 @@
+package racingcar.model
+
+data class Car(
+    val name: String,
+    var position: Int = 0
+)
