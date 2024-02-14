1e585b8b8daeb9b78bf6e0e4af3f384c587747ff
lee989898 <wnsrud1352@gmail.com>
2024-02-14 16:22:19 +0900

    feat: 레이싱 게임 전체를 실행하는 클래스 생성

---
+++ src/main/kotlin/racingcar/controller/Run.kt
@@ -0,0 +1,49 @@
+package racingcar.controller
+
+import racingcar.model.Car
+import racingcar.model.Messages
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class Run {
+
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val moveOrStay = MoveOrStay()
+
+    fun run() {
+        outputView.printEnterCarNames()
+        val carNames = inputView.askCarNames()
+        val commaSeparatedListBuilder = CommaSeparatedListBuilder(carNames)
+        val nameOfCars = commaSeparatedListBuilder.commaSeparatedListBuild()
+        val numberOfCars = nameOfCars.size
+        val cars: MutableList<Car> = mutableListOf()
+
+        repeat(numberOfCars) {
+            cars.add(Car(nameOfCars[it]))
+        }
+
+        outputView.enterNumberOfAttempts()
+        val numberOfAttempts = inputView.askNumberOfAttempts()
+
+        outputView.printExecutionResults()
+
+        val randomNumberGenerator = RandomNumberGenerator(numberOfCars)
+
+        repeat(numberOfAttempts) {
+            val randomNumbers = randomNumberGenerator.putRandomNumbers()
+            cars.forEachIndexed { index, car ->
+                if(moveOrStay.decideMovement(randomNumbers[index])){
+                    car.position += Messages.DASH
+                }
+
+            }
+            cars.forEach {
+                println("${it.name}: ${it.position}")
+            }
+            println()
+        }
+
+        outputView.printLastWinner()
+    }
+}
