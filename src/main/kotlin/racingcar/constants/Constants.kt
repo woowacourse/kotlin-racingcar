17eb5da27569857a81df9af42ee4e321c85d6fcf
somin <salth0601@gmail.com>
2024-02-14 17:07:55 +0900

    chore: 자동차 이름 입력 예외 메시지 상수 내용 추가
    Co-authored-by: kmkim2689 <101035437+kmkim2689@users.noreply.github.com>

--- src/main/kotlin/racingcar/constants/Constants.kt
+++ src/main/kotlin/racingcar/constants/Constants.kt
@@ -1,4 +1,7 @@
 package racingcar.constants
 
 object Constants {
+    const val INVALID_NAME_SIZE_EXCEPTION = "이름은 1~5글자 사이로 입력해주세요."
+    const val DUPLICATED_NAME_EXCEPTION = "서로 중복되지 않는 이름을 입력해주세요."
+    const val INVALID_NUM_OF_CARS_EXCEPTION = "2대 이상의 자동차가 필요합니다."
 }
