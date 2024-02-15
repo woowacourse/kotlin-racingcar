5204c945714bbfbc7b9ed077f878f901cd171ee9
JunJangE <wnswkd486@gmail.com>
2024-02-15 16:13:06 +0900

    feat: 에러 발생 명칭 기입

--- src/main/kotlin/racingcar/utils/Exceptions.kt
+++ src/main/kotlin/racingcar/utils/Exceptions.kt
@@ -4,8 +4,9 @@ object Exceptions {
 
     const val MESSAGE_ERROR = "잘못입력하셨습니다"
 
-    fun checkCarNames(carNames: String) = require(carNames.split(", ").all { it.length in 1..5 }) { MESSAGE_ERROR }
+    fun checkCarNames(carNames: String) =
+        require(carNames.trim().split(",").all { it.length in 1..5 }) { IllegalArgumentException(MESSAGE_ERROR) }
 
-    fun checkRoundCnt(round: String) = requireNotNull(round.toIntOrNull()) { MESSAGE_ERROR }
+    fun checkRoundCnt(round: String) = requireNotNull(round.toIntOrNull()) { NumberFormatException(MESSAGE_ERROR) }
 
 }
