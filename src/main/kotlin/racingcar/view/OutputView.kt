4685ce7d097f46a0b537724a898ea1e3c46e7a06
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:45:36 +0900

    feat(view): print winners
    Function for printing winner list added.

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -14,11 +14,15 @@ class OutputView {
 
     fun printNewLine() = println()
 
+    fun printWinner(winners: List<String>) = println("$FINAL_WINNER${winners.joinToString(COMMA)}")
+
     companion object {
         const val REQUEST_CAR_NAMES_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         const val REQUEST_ROUND_NUMBER_GUIDE = "시도할 횟수는 몇 회인가요?"
         const val EXECUTION_RESULT = "실행 결과"
+        const val FINAL_WINNER = "최종 우승자: "
 
         const val DASH = "-"
+        const val COMMA = ", "
     }
 }
