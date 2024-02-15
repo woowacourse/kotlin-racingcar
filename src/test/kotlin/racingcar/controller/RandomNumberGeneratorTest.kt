280fe2589d8e0b1aaac75163610c82be4ce21c04
songpink <jejeje04@naver.com>
2024-02-15 13:10:19 +0900

    test: 랜덤넘버 리스트의 요소가 전부 0 ~ 9 인지 테스트 구현

--- src/test/kotlin/racingcar/controller/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/controller/RandomNumberGeneratorTest.kt
@@ -1,6 +1,8 @@
 package racingcar.controller
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
 
 class RandomNumberGeneratorTest {
     private lateinit var randomNumberGenerator: RandomNumberGenerator
@@ -8,5 +10,10 @@ class RandomNumberGeneratorTest {
     @BeforeEach
     fun setup() {
         randomNumberGenerator = RandomNumberGenerator(3)
+    }
+    @RepeatedTest(100)
+    fun `랜덤넘버 리스트의 요소가 전부 0 ~ 9 인지`(){
+        val result = randomNumberGenerator.putRandomNumbers()
+        assertThat(result).allMatch { it in 0..9 }
     }
 }
