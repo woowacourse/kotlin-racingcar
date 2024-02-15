055501ee1c0b4b41147da5f1f8747b1ee2ba709f
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:10:29 +0900

    feat(model): generate random number
    Function for generating random number added.

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,11 +1,15 @@
 package racingcar.model
 
+import kotlin.random.Random
+
 data class Car(val name: String, var position: Int = 0) {
     init {
         require(name.isNotEmpty()) { ERROR_MESSAGE }
         require(name.length in 1..5) { ERROR_MESSAGE }
         require(!name.contains(" ")) { ERROR_MESSAGE }
     }
+
+    fun getRandomNumber(): Int = Random.nextInt(0, 9)
 
     companion object {
         const val ERROR_MESSAGE = "잘못된 입력입니다."
