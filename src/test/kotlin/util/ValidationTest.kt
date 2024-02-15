ac0be660d611867efac3d00413bb98719b3f965e
gaeun <geun5744@gmail.com>
2024-02-15 14:37:28 +0900

    style(ValidationTest): ktlint 포맷팅

--- src/test/kotlin/util/ValidationTest.kt
+++ src/test/kotlin/util/ValidationTest.kt
@@ -15,7 +15,7 @@ class ValidationTest {
     }
 
     @ParameterizedTest(name = "이름이 {0}이면 생성 불가능")
-    @ValueSource(strings = ["가은", "가나다라마바,가은","가은,가은,나은"])
+    @ValueSource(strings = ["가은", "가나다라마바,가은", "가은,가은,나은"])
     fun `유효하지 않은 자동차 이름 입력 테스트`(carsName: String) {
         assertThrows<IllegalArgumentException> {
             Validation.carsName(carsName)
@@ -23,13 +23,13 @@ class ValidationTest {
     }
 
     @ParameterizedTest(name = "시도횟수가 {0}면 생성 가능")
-    @ValueSource(strings = ["1","2","3","100"])
+    @ValueSource(strings = ["1", "2", "3", "100"])
     fun `유효한 시도횟수 입력 테스트`(attemptCount: String) {
         assertDoesNotThrow { Validation.attemptCount(attemptCount) }
     }
 
     @ParameterizedTest(name = "시도횟수가 {0}이면 생성 불가능")
-    @ValueSource(strings = ["-1","0","횟수"])
+    @ValueSource(strings = ["-1", "0", "횟수"])
     fun `유효하지 않은 시도횟수 입력 테스트`(attemptCount: String) {
         assertThrows<IllegalArgumentException> {
             Validation.attemptCount(attemptCount)
