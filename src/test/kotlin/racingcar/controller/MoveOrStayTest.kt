2daa482a0f36396665ac569dcf40fb4f074603e5
songpink <jejeje04@naver.com>
2024-02-15 13:22:55 +0900

    test: MoveOrStay 인스턴스 변수 초기화 로직 구현

---
+++ src/test/kotlin/racingcar/controller/MoveOrStayTest.kt
@@ -0,0 +1,12 @@
+package racingcar.controller
+
+import org.junit.jupiter.api.BeforeEach
+
+class MoveOrStayTest {
+    private lateinit var moveOrStay: MoveOrStay
+
+    @BeforeEach
+    fun setUp() {
+        moveOrStay = MoveOrStay()
+    }
+}
