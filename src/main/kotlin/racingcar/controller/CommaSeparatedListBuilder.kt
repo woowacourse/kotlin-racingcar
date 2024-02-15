699edc4a6772cfbd94da19283a665ad33db9fc7d
songpink <jejeje04@naver.com>
2024-02-15 13:58:40 +0900

    refactor: 라인 길이가 너무 길어서 줄바꿈

--- src/main/kotlin/racingcar/controller/CommaSeparatedListBuilder.kt
+++ src/main/kotlin/racingcar/controller/CommaSeparatedListBuilder.kt
@@ -1,5 +1,8 @@
 package racingcar.controller
 
+import racingcar.model.Settings
+
 class CommaSeparatedListBuilder() {
-    fun commaSeparatedListBuild(carNamesText: String,) = carNamesText.split(",").map { it.trim() }
+    fun commaSeparatedListBuild(carNamesText: String) =
+        carNamesText.split(Settings.CAR_NAME_DELIMITERS).map { it.trim() }
 }
