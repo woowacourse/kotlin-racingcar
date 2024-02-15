81766101892c29d77c224aa1a684bf52d1b604b8
songpink <jejeje04@naver.com>
2024-02-15 14:14:58 +0900

    refactor: initCars, progressCarPosition 함수 분리

--- src/main/kotlin/racingcar/controller/Run.kt
+++ src/main/kotlin/racingcar/controller/Run.kt
@@ -20,9 +20,7 @@ class Run {
         var nameOfCars = commaSeparatedListBuilder.commaSeparatedListBuild(carNames)
         val numberOfCars = nameOfCars.size
 
-        repeat(numberOfCars) {
-            cars.add(Car(nameOfCars[it]))
-        }
+        initCars(numberOfCars, cars, nameOfCars)
 
         while (!(exceptionHandling.nameFormat(carNames) && exceptionHandling.limitNumberOfCars(
                 numberOfCars
@@ -44,12 +42,7 @@ class Run {
         val randomNumberGenerator = RandomNumberGenerator(numberOfCars)
 
         repeat(numberOfAttempts) {
-            val randomNumbers = randomNumberGenerator.putRandomNumbers()
-            cars.forEachIndexed { index, car ->
-                if (moveOrStay.decideMovement(randomNumbers[index])) {
-                    car.position += Settings.PROGRESS
-                }
-            }
+            progressCarPosition(randomNumberGenerator, cars)
             cars.forEach {
                 println("${it.name}: ${it.position}")
             }
@@ -58,5 +51,27 @@ class Run {
         outputView.printLastWinner()
         val finalWinners = finalWinner.decideWinner(cars)
         print(finalWinners.joinToString(Settings.FINAL_WINNER_PRINT_SEPARATOR))
+    }
+
+    private fun initCars(
+        numberOfCars: Int,
+        cars: MutableList<Car>,
+        nameOfCars: List<String>
+    ) {
+        repeat(numberOfCars) {
+            cars.add(Car(nameOfCars[it]))
+        }
+    }
+
+    private fun progressCarPosition(
+        randomNumberGenerator: RandomNumberGenerator,
+        cars: MutableList<Car>
+    ) {
+        val randomNumbers = randomNumberGenerator.putRandomNumbers()
+        cars.forEachIndexed { index, car ->
+            if (moveOrStay.decideMovement(randomNumbers[index])) {
+                car.position += Settings.PROGRESS
+            }
+        }
     }
 }
