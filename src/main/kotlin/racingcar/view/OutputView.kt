79e4e7cfecc8e979360b99164a81d3047c27f48e
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 14:30:19 +0900

    feat(view): implement print round number guide
    Printing round number input guide announcement function added.

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,6 +3,8 @@ package racingcar.view
 class OutputView {
     fun printCarNamesGuide() = println(REQUEST_CAR_NAMES_GUIDE)
 
+    fun printRoundNumberGuide() = println(REQUEST_ROUND_NUMBER_GUIDE)
+
     companion object {
         const val REQUEST_CAR_NAMES_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     }
