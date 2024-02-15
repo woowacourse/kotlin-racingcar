e0bffacd9521483745d923ce06ab2063c7c175e0
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:06:58 +0900

    feat(model): validate car name in model
    Function for car name validation added.
    Exceptions,
    1) Empty input
    2) name with space

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,4 +1,13 @@
 package racingcar.model
 
 data class Car(val name: String, var position: Int = 0) {
+    init {
+        require(name.isNotEmpty()) { ERROR_MESSAGE }
+        require(name.length in 1..5) { ERROR_MESSAGE }
+        require(!name.contains(" ")) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "잘못된 입력입니다."
+    }
 }
