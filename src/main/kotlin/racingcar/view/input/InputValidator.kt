fe65db0c3ae3c3f9b851daaab6179d0ab9cbfd07
kimhm0728 <kimhm0728@naver.com>
2024-02-15 13:34:16 +0900

    fix: 예외 처리 로직 수정

--- src/main/kotlin/racingcar/view/input/InputValidator.kt
+++ src/main/kotlin/racingcar/view/input/InputValidator.kt
@@ -7,7 +7,7 @@ class InputValidator {
     fun validateCarNames(inputCarNames: String?) {
         val carNames = splitCarNames(inputCarNames)
         carNames?.forEach {
-            require(validateCarNameLength(it) || validateCarNameRegex(it)) {
+            require(validateEmpty(it) && validateCarNameLength(it) && validateCarNameRegex(it)) {
                 ErrorConstants.INVALID_INPUT_ERROR_MESSAGE
             }
         }
@@ -17,24 +17,24 @@ class InputValidator {
     private fun splitCarNames(inputCarNames: String?) =
         inputCarNames?.split(GameConstants.SPLIT_DELIMITER)
 
+    private fun validateEmpty(input: String?) = !input.isNullOrEmpty()
+
     private fun validateCarNameLength(carName: String) =
-        carName.length in GameConstants.CAR_NAME_RANGE
+        carName.length !in GameConstants.CAR_NAME_RANGE
 
     private fun validateCarNameRegex(carName: String) =
-        Regex(GameConstants.CAR_NAME_REGEX).matches(carName)
+        !Regex(GameConstants.CAR_NAME_REGEX).matches(carName)
 
     private fun validateCarNamesDuplication(carNames: List<String>?) =
-        carNames?.size == carNames?.distinct()?.size
+        carNames?.size != carNames?.distinct()?.size
 
     fun validateTryCount(inputTryCount: String?) {
-        require(validateEmpty(inputTryCount) || validateCountRange(inputTryCount)) {
+        require(validateEmpty(inputTryCount) && validateCountRange(inputTryCount)) {
             ErrorConstants.INVALID_INPUT_ERROR_MESSAGE
         }
     }
 
-    private fun validateEmpty(inputTryCount: String?) = inputTryCount.isNullOrEmpty()
-
     private fun validateCountRange(inputTryCount: String?) =
-        inputTryCount?.toInt() in GameConstants.COUNT_RANGE
+        inputTryCount?.toInt() !in GameConstants.COUNT_RANGE
 
 }
