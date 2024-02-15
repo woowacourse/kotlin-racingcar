7f53951c0fe853dd9187c719880038492b25d9a1
songpink <jejeje04@naver.com>
2024-02-15 14:01:07 +0900

    feat: 초기 세팅 상수화

---
+++ src/main/kotlin/racingcar/model/Settings.kt
@@ -0,0 +1,8 @@
+package racingcar.model
+
+object Settings {
+    const val PROGRESS = "-"
+    const val CAR_NAME_DELIMITERS = ","
+    const val DEFAULT_POSITION_OF_CAR = ""
+    const val FINAL_WINNER_PRINT_SEPARATOR = ", "
+}
