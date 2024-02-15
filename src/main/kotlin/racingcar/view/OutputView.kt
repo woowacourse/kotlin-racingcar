93481671b971e5229ad3c12a5d9f9c5f7b428548
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:34:14 +0900

    feat(view): print results for each round
    Function to print current status(name, position) was added.

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -7,9 +7,16 @@ class OutputView {
 
     fun printResultHeader() = println(EXECUTION_RESULT)
 
+    fun printRaceResult(
+        name: String,
+        position: Int,
+    ) = println("$name : ${DASH.repeat(position)}")
+
     companion object {
         const val REQUEST_CAR_NAMES_GUIDE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         const val REQUEST_ROUND_NUMBER_GUIDE = "시도할 횟수는 몇 회인가요?"
         const val EXECUTION_RESULT = "실행 결과"
+
+        const val DASH = "-"
     }
 }
