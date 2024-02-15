e24cbb1ee13f347b836111e2ec39082a1cd2b709
sh1mj1 <7wlgns@gmail.com>
2024-02-15 14:50:32 +0900

    feat(OutputView): 경주 결과를 출력한다
    - 각 실행(phase)마다 자동차 위치를 출력한다
    - 최종 우승자를 출력한다

---
+++ src/main/kotlin/view/OutputView.kt
@@ -0,0 +1,32 @@
+package view
+
+import domain.Cars
+
+class OutputView {
+
+    fun printResultHeader() = println(HEADER_RESULT)
+
+    fun printPhase(cars: Cars) {
+        cars.cars.forEach {
+            println("${it.name} :" + "-".repeat(it.position))
+        }
+        println()
+    }
+
+    fun printWinner(cars: Cars) {
+        val maxPosition = cars.cars.maxOf {
+            it.position
+        }
+
+        val winners = cars.cars.filter {
+            it.position == maxPosition
+        }
+
+        println("$HEADER_WINNERS ${winners.joinToString(",")} ")
+    }
+
+    companion object {
+        const val HEADER_RESULT = "실행 결과"
+        const val HEADER_WINNERS = "최종 우승자 :"
+    }
+}
