28e1ca692b4c4570c5a872e6434b439451766cbb
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:20:20 +0900

    feat(controller): implement getting round number
    Function getting round number added.
    Get string input from InputView, and return int.

--- src/main/kotlin/racingcar/controller/Race.kt
+++ src/main/kotlin/racingcar/controller/Race.kt
@@ -1,5 +1,8 @@
 package racingcar.controller
 
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
 class Race {
     private val outputView = OutputView()
     private val inputView = InputView()
@@ -29,6 +32,23 @@ class Race {
         require(names.size > 1) { ERROR_MESSAGE }
         require(names.size < 21) { ERROR_MESSAGE }
         require(names.distinct().size == names.size) { ERROR_MESSAGE }
+    }
+
+    fun getRoundNumber(): Int {
+        outputView.printRoundNumberGuide()
+        var roundNumber: Int
+
+        while (true) {
+            try {
+                val roundNumberInput = inputView.readRoundNumber()
+
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+
+        return roundNumber
     }
 
     companion object {
