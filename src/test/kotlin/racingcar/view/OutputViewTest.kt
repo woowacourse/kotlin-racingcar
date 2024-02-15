9fe35a0afe31a0b1e7f227b57257403c72831606
somin <salth0601@gmail.com>
2024-02-15 11:34:00 +0900

    test: OutputViewTest에 ParameterizedTest 활용
    Co-authored-by: kmkim2689 <101035437+kmkim2689@users.noreply.github.com>

--- src/test/kotlin/racingcar/view/OutputViewTest.kt
+++ src/test/kotlin/racingcar/view/OutputViewTest.kt
@@ -2,7 +2,8 @@ package racingcar.view
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.model.Car
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
@@ -16,29 +17,16 @@ class OutputViewTest {
         outContent = ByteArrayOutputStream()
     }
 
-    @Test
-    fun `현재 위치가 0일 때, 현재 위치 출력 테스트`() {
-        val car = Car("km")
+    @ParameterizedTest
+    @ValueSource(ints = [0, 3, 10])
+    fun `현재 위치가 0일 때, 현재 위치 출력 테스트`(nums: Int) {
+        val car = Car("carName", nums)
         System.setOut(PrintStream(outContent))
         OutputView.printCurrentPosition(car.name, car.position)
 
         val output = outContent.toString()
         assertThat(output).contains(
-            "km : "
-        )
-    }
-
-
-    @Test
-    fun `현재 위치가 3일 때, 현재 위치 출력 테스트`() {
-        val car = Car("km", 3)
-        System.setOut(PrintStream(outContent))
-        OutputView.printCurrentPosition(car.name, car.position)
-
-        val output = outContent.toString()
-        assertThat(output).contains(
-            "km : ",
-            "---"
+            "carName : ", "-".repeat(nums)
         )
     }
 }
