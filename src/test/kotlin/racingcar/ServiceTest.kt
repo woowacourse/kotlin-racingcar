2f5bc27139038f65e371bde84d622a83dcc6716f
kimhm0728 <kimhm0728@naver.com>
2024-02-15 15:05:04 +0900

    test: WinnerService 서비스 테스트 구현

--- src/test/kotlin/racingcar/ServiceTest.kt
+++ src/test/kotlin/racingcar/ServiceTest.kt
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test
 import racingcar.model.Car
 import racingcar.service.ForwardService
 import racingcar.service.RandomGenerator
+import racingcar.service.WinnerService
 
 class ServiceTest {
     private val forwardService = ForwardService()
+    private val winnerService = WinnerService()
 
     /** RandomGenerator Test */
     @Test
@@ -43,5 +45,37 @@ class ServiceTest {
 
         // then
         assertThat(car1.compareTo(car2)).isEqualTo(0)
+    }
+
+    /** WinnerService Test */
+    @Test
+    fun `단독 우승자를 확인한다`() {
+        // given
+        val cars = listOf(
+            Car("olive"),
+            Car("chae").apply { forward() }
+        )
+
+        // when
+        val winners = winnerService.getWinners(cars)
+
+        // then
+        assertThat(winners[0].toString()).isEqualTo("chae")
+    }
+
+    @Test
+    fun `공동 우승자를 확인한다`() {
+        // given
+        val cars = listOf(
+            Car("olive").apply { forward() },
+            Car("chae").apply { forward() }
+        )
+
+        // when
+        val winners = winnerService.getWinners(cars)
+
+        // then
+        assertThat(winners.map { it.toString() })
+            .contains("olive", "chae")
     }
 }
