f2f57cd59697bd9896f6cd8d683a96593e96ff04
Hogu59 <dbstjdguswkd@naver.com>
2024-02-15 15:13:33 +0900

    feat(model): determine when to move
    Function for determining when to move added.
    If random number is same or greater than 4, the position would be changed)
    (as, move was conducted)

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -11,6 +11,10 @@ data class Car(val name: String, var position: Int = 0) {
 
     fun getRandomNumber(): Int = Random.nextInt(0, 9)
 
+    fun move() {
+        if (getRandomNumber() >= 4) position++
+    }
+
     companion object {
         const val ERROR_MESSAGE = "잘못된 입력입니다."
     }
