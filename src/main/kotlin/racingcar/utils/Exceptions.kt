2ab33faec7c9fe5b78dbcf873f671d42532861df
JunJangE <wnswkd486@gmail.com>
2024-02-16 00:41:28 +0900

    fix: MESSAGE_ERROR private 처리

--- src/main/kotlin/racingcar/utils/Exceptions.kt
+++ src/main/kotlin/racingcar/utils/Exceptions.kt
@@ -2,7 +2,7 @@ package racingcar.utils
 
 object Exceptions {
 
-    const val MESSAGE_ERROR = "잘못입력하셨습니다"
+private const val MESSAGE_ERROR = "잘못입력하셨습니다"
 
     fun checkCarNames(carNames: String) =
         require(carNames.trim().split(",").all { it.length in 1..5 }) { IllegalArgumentException(MESSAGE_ERROR) }
