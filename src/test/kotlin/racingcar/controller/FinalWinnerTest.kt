24813ddbb605fc90c4999700ae033a6c51ceff89
songpink <jejeje04@naver.com>
2024-02-15 13:20:23 +0900

    test: 최종 우승자 판정이 제대로 이루어지는지, 최종 우승자가 두명일 경우 우승자 판정이 제대로 이루어지는지 테스트 구현

--- src/test/kotlin/racingcar/controller/FinalWinnerTest.kt
+++ src/test/kotlin/racingcar/controller/FinalWinnerTest.kt
@@ -1,6 +1,9 @@
 package racingcar.controller
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import racingcar.model.Car
 
 class FinalWinnerTest {
     private lateinit var finalWinner: FinalWinner
@@ -8,5 +11,21 @@ class FinalWinnerTest {
     @BeforeEach
     fun setUp() {
         finalWinner = FinalWinner()
+    }
+
+    @Test
+    fun `최종 우승자 판정이 제대로 이루어지는지`() {
+        val result = finalWinner.decideWinner(
+            listOf(Car("a", "---"), Car("b", "--"))
+        )
+        assertThat(result).isEqualTo(listOf("a"))
+    }
+
+    @Test
+    fun `최종 우승자가 두명일 경우 우승자 판정이 제대로 이루어지는지`() {
+        val result = finalWinner.decideWinner(
+            listOf(Car("a", "---"), Car("b", "---"))
+        )
+        assertThat(result).isEqualTo(listOf("a", "b"))
     }
 }
