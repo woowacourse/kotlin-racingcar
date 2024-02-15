f11e3942def3c9a8c1aa95054ed5c4f8b1cd5564
murjune <sis9221302@gmail.com>
2024-02-15 11:37:09 +0900

    Chore(RaceCars) : reformat code

--- src/main/kotlin/RaceCars.kt
+++ src/main/kotlin/RaceCars.kt
@@ -1,17 +1,11 @@
 data class RaceCars(private val cars: List<RaceCar>) {
-    fun findWinners(): List<RaceCar> {
-        with(cars) {
-            val winnerCar = maxOf { it }
-            val winnerCars = filter { it.compareTo(winnerCar) == 0 }
-            return winnerCars
-        }
+    fun findWinners(): List<RaceCar> = with(cars) {
+        val winnerCar = maxOf { it }
+        val winnerCars = filter { it.compareTo(winnerCar) == 0 }
+        return winnerCars
     }
 
-    override fun toString(): String {
-        val sb = StringBuilder()
-        cars.forEach {
-            sb.append("$it\n")
-        }
-        return sb.toString()
-    }
+    override fun toString() = StringBuilder()
+        .apply { cars.forEach { append("$it\n") } }
+        .toString()
 }
