3dc5e6aefa5d7a814679efd7890f841779243248
songpink <jejeje04@naver.com>
2024-02-15 11:32:42 +0900

    test: 자동차 이름이 영어와 한글문자로 이루어져 있을 때 true를 반환하는지, 자동차 이름의 영어와 한글문자로 이루어져 있지 않을 때 false를 반환하는지 테스트 구현

--- src/test/kotlin/racingcar/controller/ExceptionHandlingTest.kt
+++ src/test/kotlin/racingcar/controller/ExceptionHandlingTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class ExceptionHandlingTest {
@@ -28,6 +27,7 @@ class ExceptionHandlingTest {
         val result = exceptionHandling.limitNumberOfCars(input)
         assertThat(result).isFalse()
     }
+
     @Test
     fun `자동차 이름의 중복이 없을 때 true를 반환하는지`() {
         val result = exceptionHandling.duplicatedCarName(listOf("alsong", "haeum"))
@@ -39,6 +39,7 @@ class ExceptionHandlingTest {
         val result = exceptionHandling.duplicatedCarName(listOf("alsong", "haeum", "alsong"))
         assertThat(result).isFalse()
     }
+
     @ParameterizedTest
     @ValueSource(ints = [1, 5, 10, 20, 100, 1000, 10000])
     fun `시도 횟수가 1 ~ 10000일 때 true를 반환하는지`(input: Int) {
@@ -50,6 +51,20 @@ class ExceptionHandlingTest {
     @ValueSource(ints = [-1, 0, 10001, 20000, 100_000])
     fun `시도 횟수가 1 ~ 10000이 아닐 때 false를 반환하는지`(input: Int) {
         val result = exceptionHandling.limitNumberOfAttempts(input)
+        assertThat(result).isFalse()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["알송,해음,alsong,haeum", "a,b,c,d"])
+    fun `자동차 이름이 영어와 한글문자로 이루어져 있을 때 true를 반환하는지`(input: String) {
+        val result = exceptionHandling.nameFormat(input)
+        assertThat(result).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["!,alsong123", "haeum?"])
+    fun `자동차 이름의 영어와 한글문자로 이루어져 있지 않을 때 false를 반환하는지`(input: String) {
+        val result = exceptionHandling.nameFormat(input)
         assertThat(result).isFalse()
     }
 }
