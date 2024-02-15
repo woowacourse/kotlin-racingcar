4060579fcedbe4df47486bef92304fa3d9195f84
songpink <jejeje04@naver.com>
2024-02-15 13:11:58 +0900

    test: FinalWinner 인스턴스 변수 초기화 로직 구현

---
+++ src/test/kotlin/racingcar/controller/FinalWinnerTest.kt
@@ -0,0 +1,12 @@
+package racingcar.controller
+
+import org.junit.jupiter.api.BeforeEach
+
+class FinalWinnerTest {
+    private lateinit var finalWinner: FinalWinner
+
+    @BeforeEach
+    fun setUp() {
+        finalWinner = FinalWinner()
+    }
+}
