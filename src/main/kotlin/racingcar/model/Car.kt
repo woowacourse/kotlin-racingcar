cdb784bdf418a79001314025938afd9111d03697
haeum808 <haeumueah@gmail.com>
2024-02-16 15:34:27 +0900

    refactor: Car의 postion을 밖에서 수정하지 못하게 변경하고 유효성 검증을 이안에서 하는 것으로 변경 및 Car객체가 직접 position을 변경하는 행동을 한다. Settings에 있던 상수들을 companion object 위치로 변경

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,3 +1,25 @@
 package racingcar.model
 
-data class Car(val name: String, var position: String = Settings.DEFAULT_POSITION_OF_CAR)
+class Car(val name: String, position: String = START_POSITION_OF_CAR) {
+    var position: String = position
+        private set
+
+    init {
+        require(name.isNotBlank()) { NAME_LENGTH_ERROR_MESSAGE }
+        require(name.length in MINIMUM_OF_NAME_LENGTH..MAXIMUM_OF_NAME_LENGTH) { NAME_FORMAT_ERROR_MESSAGE }
+    }
+
+    fun move(number: Int) {
+        if (number >= MOVE_CONDITION) position += PROGRESS_ELEMENT
+    }
+
+    companion object {
+        private const val START_POSITION_OF_CAR = ""
+        private const val PROGRESS_ELEMENT = "-"
+        private const val MINIMUM_OF_NAME_LENGTH = 1
+        private const val MAXIMUM_OF_NAME_LENGTH = 5
+        private const val MOVE_CONDITION = 4
+        private const val NAME_LENGTH_ERROR_MESSAGE = "자동차 이름 길이는 1 ~ 5여야 합니다."
+        private const val NAME_FORMAT_ERROR_MESSAGE = "자동차 이름에 공백이 있으면 안됩니다."
+    }
+}
