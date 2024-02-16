1cfc500f8838abee33f1a72b944c53665a0a9749
Gimun Kim <kmkim7575@gmail.com>
2024-02-16 17:33:09 +0900

    test: MoveManager의 메소드명을 조건 변경에 유연하게 대처할 수 있도록 변경

--- src/test/kotlin/racingcar/model/MoveManagerTest.kt
+++ src/test/kotlin/racingcar/model/MoveManagerTest.kt
@@ -3,6 +3,8 @@ package racingcar.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class MoveManagerTest {
     private lateinit var moveManager: MoveManager
@@ -12,15 +14,16 @@ class MoveManagerTest {
         moveManager = MoveManager()
     }
 
-    @Test
-    fun `4 미만의 정수가 뽑혔을 경우 false를 반환`() {
-        val num = 3
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `정지 최댓값보다 작거나 같은 뽑힌 경우 false 반환`(num: Int) {
         assertThat(moveManager.isMoveAble(num)).isFalse()
     }
 
 
-    @Test
-    fun `4 이상의 정수가 뽑혔을 경우 true를 반환`() {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `전진을 위한 최솟값보다 크거나 같은 값이 뽑힌 경우 true 반환`() {
         val num = 4
         assertThat(moveManager.isMoveAble(num)).isTrue()
     }
