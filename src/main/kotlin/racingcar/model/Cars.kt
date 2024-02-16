cd4e2cbace98bedcf3cceeb3adbf6698922adb62
haeum808 <haeumueah@gmail.com>
2024-02-16 16:17:11 +0900

    refactor: Cars에서 자동차 경기 진행 및 우승자 반환하는 책임 구현

--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -1,9 +1,25 @@
 package racingcar.model
 
-class Cars(private val cars: List<Car>) {
+class Cars(
+    private val cars: List<Car>,
+    private val randomNumberGenerator: RandomNumberGenerator = RandomNumberGenerator(),
+) {
     init {
         require(cars.distinct().size == cars.size) { NUMBER_OF_CAR_ERROR_MESSAGE }
         require(cars.size in MINIMUM_NUMBER_OF_CAR..MAXIMUM_NUMBER_OF_CAR) { DUPLICATE_CAR_NAME_ERROR_MESSAGE }
+    }
+
+    fun proceed(attempts: Int) {
+        repeat(attempts) {
+            val number = randomNumberGenerator.make()
+            cars.forEach { car -> car.move(number) }
+        }
+    }
+
+    fun winner(): List<String> {
+        val winnerPosition = cars.maxOf { it.position.length }
+
+        return cars.filter { it.position.length == winnerPosition }.map { it.name }
     }
 
     companion object {
