d345421c0607ece07ec84ed63f4efe10e5ecaafb
haeum808 <haeumueah@gmail.com>
2024-02-16 15:19:17 +0900

    refactor: OutputView companion object에 있는 상수들 가시성 변경자를 private로 변경

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -20,12 +20,12 @@ class OutputView {
     fun printLastWinner(winners: List<String>) = print("$LINE_BREAK$LAST_WINNER ${winners.joinToString(LAST_WINNERS_SEPARATOR)}")
 
     companion object {
-        const val ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
-        const val ENTER_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
-        const val EXECUTION_RESULTS = "실행 결과"
-        const val LAST_WINNER = "최종 우승자:"
-        const val INTERMEDIATE_RESULT_DELIMITERS = "\n"
-        const val LAST_WINNERS_SEPARATOR = ", "
-        const val LINE_BREAK = "\n"
+        private const val ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+        private const val ENTER_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
+        private const val EXECUTION_RESULTS = "실행 결과"
+        private const val LAST_WINNER = "최종 우승자:"
+        private const val INTERMEDIATE_RESULT_DELIMITERS = "\n"
+        private const val LAST_WINNERS_SEPARATOR = ", "
+        private const val LINE_BREAK = "\n"
     }
 }
