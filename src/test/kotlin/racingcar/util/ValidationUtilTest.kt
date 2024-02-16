db4a886e1d984a6a6a61025bfad20c2e3fd048f7
jaeyeongjo <nos05075@naver.com>
2024-02-16 16:03:44 +0900

    test: ValidationUtil 단위 테스트 작성

---
+++ src/test/kotlin/racingcar/util/ValidationUtilTest.kt
@@ -0,0 +1,40 @@
+package racingcar.util
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+class ValidationUtilTest {
+
+    private val validationUtil = ValidationUtil()
+
+    @Test
+    fun `validateCarNames with valid names`() {
+        val validNames = "car1,car2,car3"
+        assertDoesNotThrow { validationUtil.validateCarNames(validNames) }
+    }
+
+    @Test
+    fun `validateCarNames with invalid names due to wrong character length`() {
+        val invalidNames = "car123456,car2,car3"
+        assertThrows<IllegalArgumentException> { validationUtil.validateCarNames(invalidNames) }
+    }
+
+    @Test
+    fun `validateCarNames with invalid names due to duplication`() {
+        val invalidNames = "car1,car1,car3"
+        assertThrows<IllegalArgumentException> { validationUtil.validateCarNames(invalidNames) }
+    }
+
+    @Test
+    fun `validateRoundCounts with valid count`() {
+        val validCount = "5"
+        assertDoesNotThrow { validationUtil.validateRoundCounts(validCount) }
+    }
+
+    @Test
+    fun `validateRoundCounts with invalid count`() {
+        val invalidCount = "0"
+        assertThrows<IllegalArgumentException> { validationUtil.validateRoundCounts(invalidCount) }
+    }
+}
