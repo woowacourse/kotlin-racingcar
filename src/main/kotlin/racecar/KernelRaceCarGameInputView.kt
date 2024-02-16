8984d9c4915d08af1c08068ebc35739e4ba85ae1
murjune <sis9221302@gmail.com>
2024-02-16 20:20:41 +0900

    refactor : Kernel - RaceGameVIew

---
+++ src/main/kotlin/racecar/KernelRaceCarGameInputView.kt
@@ -0,0 +1,21 @@
+package racecar
+
+import RaceCarGameInputView
+
+class KernelRaceCarGameInputView : RaceCarGameInputView {
+    override fun inputCarNames(): List<String> {
+        return readln().split(DELIMITER).map { it.trim() }
+    }
+
+    override fun inputTrialCount(): Int {
+        return readln().also(::validateTrialCount).toInt()
+    }
+
+    private fun validateTrialCount(input: String) {
+        check(input.all { it.isDigit() }) { "입력값은 모두 숫자여야한다" }
+    }
+
+    companion object {
+        private const val DELIMITER = ","
+    }
+}
---
+++ src/main/kotlin/racecar/KernelRaceCarGameOutputView.kt
@@ -0,0 +1,30 @@
+package racecar
+
+import RaceCar
+
+class KernelRaceCarGameOutputView : RaceCarGameOutputView {
+    override fun outputCarNamesRequest() = println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+
+    override fun outputTrialCountRequest() = println("시도할 횟수는 몇 회인가요?")
+
+    override fun outputRaceResultTitle() = println("실행 결과")
+
+    override fun outputRacingStatus(cars: List<RaceCar>) {
+        println(cars.formatToRaceStatus())
+    }
+
+    override fun outputWinners(winner: List<RaceCar>) = print(winner.formatToWinners())
+
+    private fun List<RaceCar>.formatToRaceStatus() =
+        StringBuilder()
+            .also { sb -> forEach { car -> sb.append("${car.formatToStatus()}\n") } }
+            .toString()
+
+    private fun List<RaceCar>.formatToWinners() = "최종 우승자: ${joinToString { it.name }}"
+
+    private fun RaceCar.formatToStatus(): String = "$name : ${DISTANCE_UNIT.repeat(position)}"
+
+    companion object {
+        private const val DISTANCE_UNIT = "-"
+    }
+}
