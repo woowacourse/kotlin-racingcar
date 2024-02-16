a1f2c930b0e986a3a35b402bd869d6825093103b
hxeyexn <kck85075@kumoh.ac.kr>
2024-02-16 19:53:23 +0900

    fix(racetest): 테스트가 통과되지 않는 오류 수정
    리팩토링 과정에서 유효성 검사 에러 메세지 세분화 후 테스트 코드를 수정하지 않아 발생하는 오류 해결

--- src/test/kotlin/racingcar/RaceTest.kt
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -18,7 +18,7 @@ class RaceTest {
             }
 
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_REQUEST_INPUT,
             exception.message,
         )
     }
@@ -32,7 +32,7 @@ class RaceTest {
             }
 
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_CAR_LESS_THAN_TWO,
             exception.message,
         )
     }
@@ -50,7 +50,7 @@ class RaceTest {
             }
 
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_CAR_GREATER_THAN_TWENTY,
             exception.message,
         )
     }
@@ -64,7 +64,7 @@ class RaceTest {
             }
 
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_NAME_DUPLICATION,
             exception.message,
         )
     }
@@ -76,7 +76,7 @@ class RaceTest {
                 race.checkRoundNumber("35035810684041351035")
             }
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_INPUT_NUMBER_ONLY,
             exception.message,
         )
     }
@@ -89,7 +89,7 @@ class RaceTest {
                 race.checkRoundNumber(roundNumber)
             }
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_INPUT_NUMBER_ONLY,
             exception.message,
         )
     }
@@ -101,25 +101,30 @@ class RaceTest {
                 race.checkRoundNumber("")
             }
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_INPUT_NUMBER_ONLY,
             exception.message,
         )
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["0", "-1", "-9182758172598719587"])
+    @ValueSource(strings = ["0", "-1"])
     fun inputLessThanOneRoundNumber(roundNumber: String) {
         val exception =
             assertThrows<IllegalArgumentException> {
                 race.getValidRoundNumber(roundNumber)
             }
         assertEquals(
-            ERROR_MESSAGE,
+            ERROR_ROUND_LESS_THAN_ONE,
             exception.message,
         )
     }
 
     companion object {
-        const val ERROR_MESSAGE = "잘못된 입력입니다."
+        const val ERROR_REQUEST_INPUT = "[Error] 값을 입력해 주세요"
+        const val ERROR_CAR_LESS_THAN_TWO = "[Error] 자동차 이름은 2대 이상 입력해 주세요."
+        const val ERROR_CAR_GREATER_THAN_TWENTY = "[Error] 자동차 이름은 20대 이하로 입력해 주세요."
+        const val ERROR_NAME_DUPLICATION = "[Error] 중복된 자동차 이름은 작성할 수 없습니다."
+        const val ERROR_ROUND_LESS_THAN_ONE = "[Error] 1회 이상 입력해 주세요."
+        const val ERROR_INPUT_NUMBER_ONLY = "[Error] 숫자만 입력해 주세요."
     }
 }
