d810835263151404d313eb9c43782feff2a1310e
songpink <jejeje04@naver.com>
2024-02-16 23:31:34 +0900

    refactor: 랜덤넘버를 미리 여러개 만들어 리스트에 넣는 것이 아닌 하나를 만드는 함수를 여러번 호출하는 로직으로 변경

--- src/main/kotlin/racingcar/controller/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/controller/RandomNumberGenerator.kt
@@ -1,19 +1,7 @@
 package racingcar.controller
 
-import kotlin.random.Random
-import kotlin.random.nextInt
-
-class RandomNumberGenerator(private val numberOfCars: Int) {
-    fun putRandomNumbers(): List<Int> {
-        val randomNumbers: MutableList<Int> = mutableListOf()
-
-        for (index in 0 until numberOfCars) {
-            val randomNumber: Int = Random.nextInt(randomNumber)
-            randomNumbers.add(randomNumber)
-        }
-        return randomNumbers
-    }
-
+class RandomNumberGenerator {
+    fun generateRandomNumber(): Int = randomNumber.random()
     companion object{
         val randomNumber = 0..9
     }
