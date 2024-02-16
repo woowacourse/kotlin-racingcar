26e1eac06d1c559c578181b793c0011f2f793068
kimhm0728 <kimhm0728@naver.com>
2024-02-16 18:24:31 +0900

    refactor: 시도 횟수만큼 반복하는 기능 수정

--- src/main/kotlin/racingcar/model/TryCount.kt
+++ src/main/kotlin/racingcar/model/TryCount.kt
@@ -1,9 +1,5 @@
 package racingcar.model
 
 class TryCount(private val count: Int) {
-    fun forEach(action: () -> Unit) {
-        for (idx in 1..count) {
-            action()
-        }
-    }
+    fun forEach(action: () -> Unit) = repeat(count) { action() }
 }
