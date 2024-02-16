31a4b479bac5ddcf9229528f571983cb5386d17c
haeum808 <haeumueah@gmail.com>
2024-02-16 16:52:16 +0900

    refactor: CarRacing 비대해진 것을 함수화 하여 해소

--- src/main/kotlin/racingcar/controller/CarRacing.kt
+++ src/main/kotlin/racingcar/controller/CarRacing.kt
@@ -1,5 +1,7 @@
 package racingcar.controller
 
+import racingcar.model.Car
+import racingcar.model.CarHandler
 import racingcar.view.input.InputView
 import racingcar.view.output.OutputView
 
@@ -7,5 +9,34 @@ class CarRacing(
     private val inputView: InputView,
     private val outputView: OutputView,
 ) {
-    fun play() {}
+    private lateinit var carHandler: CarHandler
+
+    fun play() {
+        val cars = getCars()
+        carHandler = CarHandler(cars)
+        race(cars)
+        printRacerResult()
+    }
+
+    private fun getCars(): List<Car> {
+        outputView.enterCarNames()
+        val carNames = inputView.askCarNames()
+
+        return carNames.map { carName -> Car(carName) }
+    }
+
+    private fun race(cars: List<Car>) {
+        outputView.enterNumberOfAttempts()
+        val attempts = inputView.askNumberOfAttempts()
+        outputView.printExecutionResult()
+        repeat(attempts) {
+            carHandler.proceed()
+            outputView.printProgress(cars.map { car -> "${car.name}: ${car.position}" })
+        }
+    }
+
+    private fun printRacerResult() {
+        val winners = carHandler.winner()
+        outputView.printLastWinner(winners)
+    }
 }
