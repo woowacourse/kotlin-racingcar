96cb5e3f6bce646fd1199961ddcf095f25108540
haeum808 <haeumueah@gmail.com>
2024-02-16 16:52:43 +0900

    refactor: CarRacing에 필요한 객체들 생성하여 CarRacing에 전달

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,13 @@
 package racingcar
 
-import racingcar.controller.Run
+import racingcar.controller.CarRacing
+import racingcar.view.input.ConsoleInputView
+import racingcar.view.output.ConsoleOutputView
 
 fun main() {
-    val run = Run()
-    run.run()
+    val inputView = ConsoleInputView()
+    val outputView = ConsoleOutputView()
+    val carRacing = CarRacing(inputView, outputView)
+
+    carRacing.play()
 }
