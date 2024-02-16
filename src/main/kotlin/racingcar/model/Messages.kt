3e6fb6d794e574ca62792e40d555f5295d722512
haeum808 <haeumueah@gmail.com>
2024-02-16 15:48:44 +0900

    refactor: 불필요한 상수 파일들 삭제

--- src/main/kotlin/racingcar/model/Messages.kt
+++
@@ -1,8 +0,0 @@
-package racingcar.model
-
-object Messages {
-    const val NUMBER_OF_ATTEMPTS_ERROR = "시도 횟수는 1 ~ 10000여야 합니다."
-    const val NAME_FORMAT_ERROR = "이름은 알파벳과 한글로만 이루어져 있어야 합니다."
-    const val NUMBER_OF_CAR_ERROR = "차의 갯수는 1 ~ 100여야 합니다."
-    const val DUPLICATED_CAR_NAME_ERROR = "차 이름은 중복이 있으면 안 됩니다."
-}
--- src/main/kotlin/racingcar/model/Ranges.kt
+++
@@ -1,9 +0,0 @@
-package racingcar.model
-
-object Ranges {
-    val move = 4..9
-    val numberOfAttempts = 1..10000
-    val numberOfCars = 1..100
-    val carNamingFormat = "^[a-zA-Z가-힣,]+\$"
-    val randomNumber = 0..9
-}
