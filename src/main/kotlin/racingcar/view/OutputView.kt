b89e86599c8e54c752217b65e83d587aac78d62c
songpink <jejeje04@naver.com>
2024-02-16 23:29:06 +0900

    refactor: 입력 안내메시지를 InputView에서 출력하는 것으로 변경

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,13 +1,7 @@
 package racingcar.view
 
-import racingcar.model.Messages
-
 class OutputView {
-    fun printEnterCarNames() = println(Messages.ENTER_CAR_NAMES)
-
-    fun enterNumberOfAttempts() = println(Messages.ENTER_NUMBER_OF_ATTEMPTS)
-
-    fun printExecutionResults() = println(Messages.EXECUTION_RESULTS)
-
-    fun printLastWinner() = print(Messages.LAST_WINNER)
+    fun printFinalWinners(finalWinners: List<String>){
+        print(finalWinners.joinToString(","))
+    }
 }
