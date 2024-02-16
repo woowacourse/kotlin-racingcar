14d0e7cf4be11992a41084afefdadc45674ffa5c
kimhm0728 <kimhm0728@naver.com>
2024-02-16 19:03:27 +0900

    refactor: 우승자 구하는 기능 수정

--- src/main/kotlin/racingcar/service/WinnerService.kt
+++ src/main/kotlin/racingcar/service/WinnerService.kt
@@ -4,7 +4,9 @@ import racingcar.model.Car
 
 class WinnerService {
     fun getWinners(cars: List<Car>): List<Car> {
-        val maxForwardCountCar = cars.sorted()[0]
-        return cars.filter { it.compareTo(maxForwardCountCar) == 0 }
+        val maxForwardCountCar = cars.maxOf { it.forwardCount }
+        return cars.filter { car ->
+            car.forwardCount == maxForwardCountCar
+        }
     }
 }
