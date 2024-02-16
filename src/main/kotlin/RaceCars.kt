5880771b15911e922d43703d6fc2bd9c68c3b415
murjune <sis9221302@gmail.com>
2024-02-16 13:52:18 +0900

    fix : ktFormat

--- src/main/kotlin/RaceCars.kt
+++ src/main/kotlin/RaceCars.kt
@@ -1,17 +1,18 @@
 data class RaceCars(
     private val cars: List<RaceCar>,
-    private val numberGenerator: NumberGenerator
+    private val numberGenerator: NumberGenerator,
 ) {
     init {
         require(cars.distinct().size == cars.size) { "차는 중복될 수 없습니다." }
         require(cars.isNotEmpty()) { "차 리스트는 비어 있으면 안된다." }
     }
 
-    fun findHeadGroup(): List<RaceCar> = with(cars) {
-        val winnerCar = maxOf { it }
-        val winnerCars = filter { it.compareTo(winnerCar) == 0 }
-        return winnerCars
-    }
+    fun findHeadGroup(): List<RaceCar> =
+        with(cars) {
+            val winnerCar = maxOf { it }
+            val winnerCars = filter { it.compareTo(winnerCar) == 0 }
+            return winnerCars
+        }
 
     fun moveOrStop() {
         cars.forEach {
@@ -19,9 +20,10 @@ data class RaceCars(
         }
     }
 
-    override fun toString() = StringBuilder()
-        .apply { cars.forEach { append("$it\n") } }
-        .toString()
+    override fun toString() =
+        StringBuilder()
+            .apply { cars.forEach { append("$it\n") } }
+            .toString()
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -42,7 +44,10 @@ data class RaceCars(
     }
 
     companion object {
-        fun from(carNames: List<String>, numberGenerator: NumberGenerator): RaceCars {
+        fun from(
+            carNames: List<String>,
+            numberGenerator: NumberGenerator,
+        ): RaceCars {
             return RaceCars(carNames.map { RaceCar(it) }, numberGenerator)
         }
     }
