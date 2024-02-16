093e8e2ca296d4273f1cbbe20195d41e47d80e0e
haeum808 <haeumueah@gmail.com>
2024-02-16 16:08:39 +0900

    refactor: Controller 역할인 Run 클래스명을 CarRacing으로 변경 및 InputView, OutputView 위치를 생성자 파라미터로 변경

---
+++ src/main/kotlin/racingcar/controller/CarRacing.kt
@@ -0,0 +1,11 @@
+package racingcar.controller
+
+import racingcar.view.input.InputView
+import racingcar.view.output.OutputView
+
+class CarRacing(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun play() {}
+}
