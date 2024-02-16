710b78dfda0e8f860110f8a1c7aaf8bebe298d0f
Hogu59 <dbstjdguswkd@naver.com>
2024-02-16 15:20:28 +0900

    fix(controller): fix problem(scope of cars)
    Error was occured because of the scope issue.
    1) getWinners function get cars as an argument.
    2) judgeWinners function uses the lateinit member variable.
    
    To fix the problem, cars(List<Car>) parameter was added.
    But, it should be considered again (using three parameter is not recommended.

--- src/main/kotlin/racingcar/controller/Race.kt
+++ src/main/kotlin/racingcar/controller/Race.kt
@@ -111,23 +111,25 @@ class Race {
 
     fun getWinners(cars: List<Car>): List<String> {
         val winners = mutableListOf<String>()
+        val maxPosition = getMaxPosition(cars)
 
         cars.forEach { car ->
-            judgeWinners(car, winners)
+            judgeWinners(car, maxPosition, winners)
         }
         return winners
     }
 
     private fun judgeWinners(
         car: Car,
+        maxPosition: Int,
         winners: MutableList<String>,
     ) {
-        val maxPosition = cars.maxOfOrNull { it.position }
-
         if (car.position == maxPosition) {
             winners.add(car.name)
         }
     }
+
+    private fun getMaxPosition(cars: List<Car>): Int = cars.maxOfOrNull { it.position }!!
 
     private fun showWinners() {
         val winners = getWinners(cars)
