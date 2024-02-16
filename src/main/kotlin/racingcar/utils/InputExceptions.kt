5664c367957c84a9a6ca138e08af71867fb19ead
JoYehyun <jo990714@naver.com>
2024-02-16 18:25:32 +0900

    feat: 시도 횟수 예외 상황 추가

--- src/main/kotlin/racingcar/utils/InputExceptions.kt
+++ src/main/kotlin/racingcar/utils/InputExceptions.kt
@@ -2,7 +2,7 @@ package racingcar.utils
 
 object InputExceptions {
     private const val MESSAGE_ERROR_CAR_NAME_INPUT = "자동차 이름은 5자를 초과할 수 없으며, 쉼표로 구분합니다."
-    private const val MESSAGE_ERROR_ROUND_INPUT = "시도 횟수는 자연수로 입력해주세요."
+    private const val MESSAGE_ERROR_ROUND_INPUT = "시도 횟수는 1 이상의 숫자로 입력해주세요."
 
     fun checkCarNames(carNames: String) =
         require(
@@ -11,5 +11,8 @@ object InputExceptions {
             }.all { it.length in 1..5 },
         ) { MESSAGE_ERROR_CAR_NAME_INPUT }
 
-    fun checkRoundCnt(round: String) = requireNotNull(round.toIntOrNull()) { MESSAGE_ERROR_ROUND_INPUT }
+    fun checkRoundCnt(round: String) {
+        requireNotNull(round.toIntOrNull()) { MESSAGE_ERROR_ROUND_INPUT }
+        require(round.toInt() > 0) { MESSAGE_ERROR_ROUND_INPUT }
+    }
 }
