5aa03e9484d4f9cbae85bfea61a635e4b77c9677
Sanghyun Ko <kkosang@Sanghyunui-noteubug.local>
2024-02-17 19:49:09 +0900

    refactor: 진행상황 출력형태 수정

--- src/main/kotlin/racingcar/view/output.kt
+++ src/main/kotlin/racingcar/view/output.kt
@@ -2,17 +2,18 @@ package racingcar.view
 
 import racingcar.model.Car
 
-fun printRunMenu(){
+fun printRunMenu() {
     println("실행 결과")
 }
 
-fun printProgress(cars : List<Car>) {
-    for(car in cars){
-        println("${car.name} : ${car.position}")
+fun printProgress(cars: List<Car>) {
+    for (car in cars) {
+        val progressBar = "-".repeat(car.position)
+        println("${car.name} : $progressBar")
     }
     println()
 }
 
-fun printWinners(winners:List<String>){
+fun printWinners(winners: List<String>) {
     print("최종 우승자 : ${winners.joinToString(", ")}")
 }
