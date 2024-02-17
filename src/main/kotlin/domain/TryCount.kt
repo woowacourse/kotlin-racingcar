1ce671f3c5439647dc459a3cba6ff7f05cbc7be8
hevton <hevton22@gmail.com>
2024-02-17 12:08:26 +0900

    refactor(TryCount): 유효성 검사 폼 변경
    - 이전: 클래스 외부에 확장함수로 정의
    - 이후: 클래스 내에서만 쓰이는 유효성 검증인만큼, 클래스 내부 함수로 정의

--- src/main/kotlin/domain/TryCount.kt
+++ src/main/kotlin/domain/TryCount.kt
@@ -4,16 +4,16 @@ class TryCount private constructor(val count: Int) {
 
     companion object {
         fun from(input: String) =
-            input.isValidCount()
+            validateCount(input)
                 .run { TryCount(this) }
-    }
-}
 
-private fun String.isValidCount(): Int {
-    val count = this.toIntOrNull()
-    requireNotNull(count) { EXCEPTION_TRY_COUNT_IS_NOT_INT }
-    require(count > 0) { EXCEPTION_INVALID_TRY_COUNT_RANGE }
-    return count
+        private fun validateCount(input: String): Int {
+            val count = input.toIntOrNull()
+            requireNotNull(count) { EXCEPTION_TRY_COUNT_IS_NOT_INT }
+            require(count > 0) { EXCEPTION_INVALID_TRY_COUNT_RANGE }
+            return count
+        }
+    }
 }
 
 private const val EXCEPTION_TRY_COUNT_IS_NOT_INT = "시도 횟수는 정수여야 합니다"
