e2f8412dd50ebb1ffecaf137af7b6867cf42f02d
gaeun <geun5744@gmail.com>
2024-02-17 18:50:30 +0900

    refactor(CarsManager): CsvSource로 리팩토링

--- src/test/kotlin/model/CarsManagerTest.kt
+++ src/test/kotlin/model/CarsManagerTest.kt
@@ -4,15 +4,16 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
 
 class CarsManagerTest {
 
 
     @ParameterizedTest
-    @MethodSource("provideRandomNumberExample")
+    @CsvSource("2,0","3,0","4,1","5,1")
     fun `한 라운드에서, Car 객체의 moveForward 메서드가 모두 동작했는지 검증 테스트`(randomNumber: Int, forwardCount: Int) {
-        val carsManager = CarsManager(listOf("가나,다라,마바,사")) { randomNumber }
+        val carsManager = CarsManager(listOf("가나","다라","마바")) { randomNumber }
         carsManager.move()
         assertThat(carsManager.cars.all { it.forwardCount == forwardCount }).isTrue
 
@@ -29,10 +30,6 @@ class CarsManagerTest {
     }
 
     companion object {
-        @JvmStatic
-        fun provideRandomNumberExample() = listOf(
-            Arguments.of(2, 0), Arguments.of(3, 0), Arguments.of(4, 1), Arguments.of(5, 1)
-        )
 
         val isMoveExample = listOf(true, false)
     }
