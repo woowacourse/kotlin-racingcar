89cd5a8c385292ff165f1ceed08dbb1005c0dae7
hevton <hevton22@gmail.com>
2024-02-18 02:59:37 +0900

    refactor(InputView): 상수 접근 제어자 변경
    InputView 에서만 쓰이는 상수이기에, private 접근 제어자 명시

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -12,8 +12,8 @@ class InputView {
     }
 
     companion object {
-        const val HEADER_READ_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-        const val INPUT_IS_NULL = "입력이 있어야 합니다"
-        const val HEADER_READ_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
+        private const val HEADER_READ_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val INPUT_IS_NULL = "입력이 있어야 합니다"
+        private const val HEADER_READ_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
     }
 }
