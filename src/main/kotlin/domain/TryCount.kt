ce8b3b465855eb957db5798d59f96403c3e49702
sh1mj1 <7wlgns@gmail.com>
2024-02-17 16:19:30 +0900

    refactor(TryCount): 로그와 비교를 위한 메서드를 오버라이드
    - toString, equals, hashCode 메서드 오버라이드

--- src/main/kotlin/domain/TryCount.kt
+++ src/main/kotlin/domain/TryCount.kt
@@ -1,6 +1,25 @@
 package domain
 
 class TryCount private constructor(val count: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TryCount
+
+        if (count != other.count) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return count
+    }
+
+    override fun toString(): String {
+        return "TryCount(count=$count)"
+    }
+
     companion object {
         fun from(input: String) =
             input.isValidCount()
