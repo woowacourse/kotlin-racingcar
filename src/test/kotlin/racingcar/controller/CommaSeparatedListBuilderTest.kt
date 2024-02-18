253c04045bedc79812e6c1d0f4293283d3691b10
songpink <jejeje04@naver.com>
2024-02-18 14:35:06 +0900

    refactor: 클래스에서 오브젝트로 수정

--- src/test/kotlin/racingcar/controller/CommaSeparatedListBuilderTest.kt
+++ src/test/kotlin/racingcar/controller/CommaSeparatedListBuilderTest.kt
@@ -7,17 +7,11 @@ import org.junit.jupiter.params.provider.ValueSource
 import racingcar.model.CommaSeparatedListBuilder
 
 class CommaSeparatedListBuilderTest {
-    private lateinit var commaSeparatedListBuilder: CommaSeparatedListBuilder
-
-    @BeforeEach
-    fun setup() {
-        commaSeparatedListBuilder = CommaSeparatedListBuilder()
-    }
 
     @ParameterizedTest
     @ValueSource(strings = ["alsong,haeum,pobi", "alsong, haeum, pobi"])
     fun `문자열을 쉼표로 기준으로 나눠서 리스트로 반환하는지`(input: String) {
-        val result = commaSeparatedListBuilder.commaSeparatedListBuild(input)
+        val result = CommaSeparatedListBuilder.commaSeparatedListBuild(input)
         assertThat(result).isEqualTo(listOf("alsong", "haeum", "pobi"))
     }
 }
