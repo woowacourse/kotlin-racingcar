18551f23eab334e4407acda2990b418e15ed0ca9
Gimun Kim <kmkim7575@gmail.com>
2024-02-18 17:23:00 +0900

    refactor: 테스트 코드 가독성 향상을 위해 코드 스타일 변경

--- src/test/kotlin/racingcar/model/RacingStatusManagerTest.kt
+++ src/test/kotlin/racingcar/model/RacingStatusManagerTest.kt
@@ -1,5 +1,34 @@
 package racingcar.model
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-class RacingStatusManagerTest
+class RacingStatusManagerTest {
+
+    private lateinit var racingStatusManager: RacingStatusManager
+    private lateinit var numberGenerator: NumberGenerator
+    private lateinit var movementDecisionMaker: MovementDecisionMaker
+
+    @BeforeEach
+    fun setUp() {
+        val initialStatus = listOf(Car("kmkim"), Car("kkm"), Car("kmk"))
+        racingStatusManager = RacingStatusManager(initialStatus)
+        numberGenerator = TestNumberGenerator()
+        movementDecisionMaker = ThresholdMovementDecisionMaker()
+    }
+
+    @Test
+    fun `숫자를 산출한 횟수가 짝수라면 움직임이 가능한 숫자를 반환하는 메소드를 활용해, 경주 1회 시행 시 결과가 올바르게 설정되는지 테스트`() {
+        racingStatusManager.setRacingResult(
+            getNumber = numberGenerator::getNumber,
+            isMovable = movementDecisionMaker::isMovable
+        )
+
+        assertThat(racingStatusManager.currentRacingStatus).contains(
+            Car(name = "kmkim", position = 0),
+            Car(name = "kkm", position = 1),
+            Car(name = "kmk", position = 0)
+        )
+    }
+}
--- src/test/kotlin/racingcar/model/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/model/RandomNumberGeneratorTest.kt
@@ -7,16 +7,16 @@ import racingcar.constants.Constants.MAXIMUM_RANDOM_SELECTION_NUM
 import racingcar.constants.Constants.MINIMUM_RANDOM_SELECTION_NUM
 
 class RandomNumberGeneratorTest {
-    private lateinit var randomNumberGenerator: RandomNumberGenerator
+    private lateinit var numberGenerator: NumberGenerator
 
     @BeforeEach
     fun setup() {
-        randomNumberGenerator = RandomNumberGenerator()
+        numberGenerator = RandomNumberGenerator()
     }
 
     @RepeatedTest(value = 100)
     fun `선택된 랜덤 숫자가 허용 범위 내에 있는지의 여부 테스트`() {
-        val randomNumber = randomNumberGenerator.getRandomNumber()
-        assertThat(randomNumber).isBetween(MINIMUM_RANDOM_SELECTION_NUM, MAXIMUM_RANDOM_SELECTION_NUM - 1)
+        val randomNumber = numberGenerator.getNumber()
+        assertThat(randomNumber).isBetween(MINIMUM_RANDOM_SELECTION_NUM, MAXIMUM_RANDOM_SELECTION_NUM)
     }
 }
