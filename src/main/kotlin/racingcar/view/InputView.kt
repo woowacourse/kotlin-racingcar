1a41b50473129f74eb2a90c6626aac0976694109
jinuemong <jinwoo3661@naver.com>
2024-02-18 13:51:56 +0900

    refactor: Input, Output View의 역할 분리하도록 수정

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -7,16 +7,12 @@ object InputView {
     private val br = BufferedReader(InputStreamReader(System.`in`))
 
     fun inputCarNames(): List<String> {
-        println(REGISTER_CAR_NAMES)
         return br.readLine().split(SEPARATOR)
     }
 
     fun inputNumberOfRound(): String {
-        println(NUMBER_OF_ROUND)
         return br.readLine()
     }
 
     private const val SEPARATOR = ","
-    private const val REGISTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    private const val NUMBER_OF_ROUND = "시도할 횟수는 몇 회인가요?"
 }
--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,10 +3,17 @@ package racingcar.view
 import racingcar.model.Car
 
 object OutputView {
-
     fun outputStartGame() {
         println()
         println(RESULT_START)
+    }
+
+    fun outputRegisterCar() {
+        println(REGISTER_CAR_NAMES)
+    }
+
+    fun outputRegisterNumberOfRoundRound() {
+        println(NUMBER_OF_ROUND)
     }
 
     fun outputRoundResults(cars: List<Car>) {
@@ -25,4 +32,6 @@ object OutputView {
     private const val RESULT_START = "실행 결과"
     private const val ROUND_RESULT = "%s : %s"
     private const val FINAL_WINNERS = "최종 우승자: %s"
+    private const val REGISTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private const val NUMBER_OF_ROUND = "시도할 횟수는 몇 회인가요?"
 }
