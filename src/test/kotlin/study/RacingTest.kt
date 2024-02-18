698d98db6222c62602b168e2e9fe7d3140bdfe23
Sanghyun Ko <kkosang@Sanghyunui-noteubug.local>
2024-02-19 03:17:15 +0900

    refator: 차가 주어진 조건에 제대로 움직이는지 테스트

--- src/test/kotlin/study/RacingTest.kt
+++ src/test/kotlin/study/RacingTest.kt
@@ -4,14 +4,19 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import racingcar.model.Car
-import racingcar.model.findWinnerNames
+import racingcar.model.Racing
 
 class RacingTest {
     @Test
     fun `우승자 한명인 경우 테스트`() {
-        val cars = listOf(Car("a", 1), Car("b", 5), Car("c", 3))
+        val racing = Racing()
+        val cars = listOf(Car("a"), Car("b"), Car("c"))
 
-        val winner = findWinnerNames(cars)
+        cars[0].move(1)
+        cars[1].move(5)
+        cars[2].move(3)
+
+        val winner = racing.findWinnerNames(cars)
         val expected = listOf("b")
 
         assertEquals(expected, winner)
@@ -19,33 +24,42 @@ class RacingTest {
 
     @Test
     fun `우승자가 여러명인 경우 테스트`() {
-        val cars = listOf(Car("a", 1), Car("b", 5), Car("c", 5))
+        val racing = Racing()
+        val cars = listOf(Car("a"), Car("b"), Car("c"))
 
-        val winner = findWinnerNames(cars)
+        cars[0].move(1)
+        cars[1].move(5)
+        cars[2].move(5)
+
+        val winner = racing.findWinnerNames(cars)
         val expected = listOf("b", "c")
 
         assertEquals(expected, winner)
     }
 
     @Test
-    fun `차가 올바르게 전진한 경우 테스트`() {
+    fun `차가 올바르게 이동한 경우 테스트`() {
         val car = Car(name = "abc")
+        val racing = Racing()
 
         car.move(4)
-        val expected = 4
+        car.move(racing.goOrStop(6))
+        val expected = 5
         val actual = car.position
 
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
-    fun `차가 올바르지 않게 전진한 경우 테스트`() {
+    fun `차가 올바르게 멈춘 경우 테스트`() {
         val car = Car(name = "abc")
+        val racing = Racing()
 
-        car.move(4)
-        val expected = 3
+        car.move(2)
+        car.move(racing.goOrStop(3))
+        val expected = 2
         val actual = car.position
 
-        assertThat(actual).isNotEqualTo(expected)
+        assertThat(actual).isEqualTo(expected)
     }
 }
