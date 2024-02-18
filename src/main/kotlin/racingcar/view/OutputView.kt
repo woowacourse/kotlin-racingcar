0436173a4ab9d929a08a3b4359097c3248c61088
songpink <jejeje04@naver.com>
2024-02-18 17:32:50 +0900

    refactor: 상수화

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,6 +4,10 @@ class OutputView {
     fun printExecutionResults() = println(InputView.EXECUTION_RESULTS)
     fun printLastWinner() = print(InputView.LAST_WINNER)
     fun printFinalWinners(finalWinners: List<String>){
-        print(finalWinners.joinToString(","))
+        print(finalWinners.joinToString(FINAL_WINNER_PRINT_SEPARATOR))
+    }
+
+    companion object{
+        const val FINAL_WINNER_PRINT_SEPARATOR = ","
     }
 }
