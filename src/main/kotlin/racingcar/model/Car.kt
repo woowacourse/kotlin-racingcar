5075b8ee3611cc2fc517386a71945432f01e1fb4
songpink <jejeje04@naver.com>
2024-02-18 17:09:08 +0900

    refactor: 상수화

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,16 +1,18 @@
 package racingcar.model
 
 class Car(val name: String){
-    var position: String = ""
+    var position: String = DEFAULT_POSITION
         private set
     private fun decideMovement(randomNumber: Int): Boolean = randomNumber in moveIfInThisRange
 
     fun moveCar(randomNumber: Int){
         if (decideMovement(randomNumber)){
-            position += "-"
+            position += POSITION_INDICATOR
         }
     }
     companion object{
         val moveIfInThisRange = 4..9
+        const val DEFAULT_POSITION = ""
+        const val POSITION_INDICATOR = "-"
     }
 }
