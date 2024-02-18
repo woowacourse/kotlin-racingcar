0e7a4d23a1f3ffef2281315cca101cf650968d52
kimhm0728 <kimhm0728@naver.com>
2024-02-18 22:49:15 +0900

    style: 코드 스타일 변경

--- src/main/kotlin/racingcar/service/RacingGame.kt
+++ src/main/kotlin/racingcar/service/RacingGame.kt
@@ -3,9 +3,8 @@ package racingcar.service
 import racingcar.model.Car
 
 class RacingGame(private val cars: List<Car>) {
-    fun updateCarsPosition() {
+    fun updateCarsPosition() =
         cars.forEach { car ->
             ForwardService.tryForwardCar(car, RandomForwardNumberGenerator)
         }
-    }
 }
