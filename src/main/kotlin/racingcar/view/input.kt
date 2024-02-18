caedefc2f7e4eafb6687dd69bf7246c489be2937
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-18 23:43:21 +0900

    refect: 문자열 상수 변환

--- src/main/kotlin/racingcar/view/input.kt
+++ src/main/kotlin/racingcar/view/input.kt
@@ -1,9 +1,13 @@
 package racingcar.view
 
+// 최상위 수준의 상수 정의
+const val CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+const val TRY_NUMBER_PROMPT = "시도할 횟수는 몇 회인가요?"
+
 fun inputCar() {
-    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으루 구분).")
+    println(CAR_NAME_PROMPT)
 }
 
 fun inputTry() {
-    println("시도할 횟수는 몇 회인가요?")
+    println(TRY_NUMBER_PROMPT)
 }
