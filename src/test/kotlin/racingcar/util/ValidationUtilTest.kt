5a65a1ded8a9a268ae426531f63c46e5109d6309
jaeyeongjo <nos05075@naver.com>
2024-02-18 20:36:07 +0900

    test: ValidationUtilTest 함수 이름 개선

--- src/test/kotlin/racingcar/util/ValidationUtilTest.kt
+++ src/test/kotlin/racingcar/util/ValidationUtilTest.kt
@@ -8,31 +8,31 @@ class ValidationUtilTest {
     private val validationUtil = ValidationUtil()
 
     @Test
-    fun `validateCarNames with valid names`() {
+    fun `유효한 자동차 이름이 입력될 시 예외가 발생하지 않는다`() {
         val validNames = "car1,car2,car3"
         assertDoesNotThrow { validationUtil.validateCarNames(validNames) }
     }
 
     @Test
-    fun `validateCarNames with invalid names due to wrong character length`() {
+    fun `자동차 이름이 5글자가 넘을 경우 에외가 발생한다`() {
         val invalidNames = "car123456,car2,car3"
         assertThrows<IllegalArgumentException> { validationUtil.validateCarNames(invalidNames) }
     }
 
     @Test
-    fun `validateCarNames with invalid names due to duplication`() {
+    fun `자동차 이름이 중복될 경우, 에외가 발생한다`() {
         val invalidNames = "car1,car1,car3"
         assertThrows<IllegalArgumentException> { validationUtil.validateCarNames(invalidNames) }
     }
 
     @Test
-    fun `validateRoundCounts with valid count`() {
+    fun `자연수가 라운드의 수로 입력될 시 예외가 발생하지 않는다`() {
         val validCount = "5"
         assertDoesNotThrow { validationUtil.validateRoundCounts(validCount) }
     }
 
     @Test
-    fun `validateRoundCounts with invalid count`() {
+    fun `자연수가 아닌 수가 라운드의 수로 입력될 시 예외가 발생한다`() {
         val invalidCount = "0"
         assertThrows<IllegalArgumentException> { validationUtil.validateRoundCounts(invalidCount) }
     }
