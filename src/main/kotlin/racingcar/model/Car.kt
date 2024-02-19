4fd67242ecf1e91febdf8d4c5629815e966c7653
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-19 15:12:23 +0900

    refect: Car 문자열 상수로 변환

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,9 +1,10 @@
 package racingcar.model
 
+const private val needCarLen="자동차 이름은 1자 이상 5자 이하이어야 합니다. 입력된 이름:"
 class Car(val name: String, var position: Int = 0) {
     init {
         require(name.length in 1..5) {
-            "자동차 이름은 1자 이상 5자 이하이어야 합니다. 입력된 이름: $name"
+            "$needCarLen $name"
         }
     }
 }
