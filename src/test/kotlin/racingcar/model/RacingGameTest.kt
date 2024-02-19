9c386561c23fe3131f497ef7db060f9e64b09bc5
songpink <jejeje04@naver.com>
2024-02-19 17:39:51 +0900

    test: 자동차가 실제로 이동하는지 테스트
    이부분 테스트는 랜덤변수를 제가 테스트코드에서 제어할 수가 없어서 제대로 작성하지 못했습니다..

--- src/test/kotlin/racingcar/model/RacingGameTest.kt
+++ src/test/kotlin/racingcar/model/RacingGameTest.kt
@@ -3,16 +3,27 @@ package racingcar.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
 
 class RacingGameTest {
     private lateinit var racingGame: RacingGame
+    private lateinit var carA: Car
+    private lateinit var carB: Car
+
     @BeforeEach
     fun setup(){
-        racingGame = RacingGame(mutableListOf(Car("A"), Car("B")))
+        carA = Car("A")
+        carB = Car("B")
+        racingGame = RacingGame(mutableListOf(carA, carB))
     }
     @RepeatedTest(100)
     fun `랜덤넘버 리스트의 요소가 전부 제한 범위 내인지`() {
         val result = racingGame.generateRandomNumber()
         assertThat(result).isIn(0..9)
+    }
+    @Test
+    fun `자동차가 실제로 이동하는지`(){
+        racingGame.carMovingControl()
+        assert(carA.position != "" || carB.position != "")
     }
 }
