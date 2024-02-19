f2aea321de504cd940dbce7877b38cbd1ad1f86e
songpink <jejeje04@naver.com>
2024-02-19 16:59:10 +0900

    test: 랜덤변수가 이동조건에 해당하면 이동하는지 테스트

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -1,20 +1,28 @@
 package racingcar.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeEach import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
-    lateinit var car: Car
+    private lateinit var car: Car
+
     @BeforeEach
-    fun setup(){
+    fun setup() {
         car = Car("A")
     }
+
     @ParameterizedTest
     @ValueSource(ints = [8, 5, 6, 9])
-    fun `랜덤변수가 이동조건에 해당하면 이동하는지`(input: Int) {
+    fun `랜덤변수가 이동조건에 해당하면 true를 반환하는지`(input: Int) {
         val result = car.decideMovement(input)
         assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `랜덤변수가 이동조건에 해당하면 이동하는지`() {
+        car.moveCar(7)
+        assertThat(car.position == "-------")
     }
 }
