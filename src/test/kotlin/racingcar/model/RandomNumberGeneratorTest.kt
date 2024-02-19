19a5386fa1e9bcb8845023d14deb2e6d18a971ed
songpink <jejeje04@naver.com>
2024-02-19 16:44:17 +0900

    refactor: 테스트코드 수정
    generateRandomNumber 메서드의 위치를 옮겼기 때문에

--- src/test/kotlin/racingcar/model/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/model/RandomNumberGeneratorTest.kt
@@ -1,12 +1,18 @@
 package racingcar.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
 
 class RandomNumberGeneratorTest {
+    lateinit var racingGame: RacingGame
+    @BeforeEach
+    fun setup(){
+        racingGame = RacingGame(mutableListOf(Car("A"), Car("B")))
+    }
     @RepeatedTest(100)
     fun `랜덤넘버 리스트의 요소가 전부 제한 범위 내인지`() {
-        val result = RandomNumberGenerator.generateRandomNumber()
+        val result = racingGame.generateRandomNumber()
         assertThat(result).isIn(0..9)
     }
 }
