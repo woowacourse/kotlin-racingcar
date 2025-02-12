affcec591d60b5506414aa0e76e5bb936b3553c4
지원 <cheerfulboys@naver.com>
2025-02-12 19:30:01 +0900

    chore: `자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다` 테스트를 위한 코드 수정

--- src/main/kotlin/Racecourse.kt
+++ src/main/kotlin/Racecourse.kt
@@ -7,16 +7,17 @@ class Racecourse(
             startRound()
             println()
         }
-        showWinners()
+        println("최종 우승자: ${winners.joinToString { car: Car -> car.name }}")
     }
 
     private fun startRound() {
         cars.forEach { car: Car -> car.move() }
     }
 
-    private fun showWinners() {
-        val maxDistance = cars.maxOf { car: Car -> car.distance }
-        val winners: List<Car> = cars.filter { car: Car -> car.distance == maxDistance }
-        println("최종 우승자: ${winners.joinToString { car: Car -> car.name }}")
-    }
+    val winners: List<Car>
+        get() {
+            val maxDistance = cars.maxOf { car: Car -> car.distance }
+            val winners: List<Car> = cars.filter { car: Car -> car.distance == maxDistance }
+            return winners
+        }
 }
