9311d14566113e5c6c347edb45205ba55276b2d3
HamBeomJoon <hbj0802@naver.com>
2025-02-12 15:32:20 +0900

    feat: 자동차 이름 입력 예외 처리

--- src/main/kotlin/Racingcar.kt
+++ src/main/kotlin/Racingcar.kt
@@ -2,11 +2,20 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import kotlin.random.Random
 
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
+
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     val carsInput = br.readLine().split(",").map { it.trim() }
+    carsInput.forEach {
+        if (it.isEmpty() || it.length > 5) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    if (carsInput.size > carsInput.toSet().size) {
+        throw IllegalArgumentException()
+    }
 
     val cars = mutableSetOf<Car>()
     carsInput.map { cars.add(Car(it, 0)) }
