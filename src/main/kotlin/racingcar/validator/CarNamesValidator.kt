478a8fab1353b16e32180c79bc31ee205b1e31ad
이지윤 <02leejiyun@naver.com>
2025-02-12 16:17:16 +0900

    feat: 자동차 이름이 5자를 초과한 경우 예외 처리

--- src/main/kotlin/racingcar/validator/CarNamesValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNamesValidator.kt
@@ -1,16 +1,27 @@
 package racingcar.validator
 
 class CarNamesValidator {
-    fun validate(carNames: String) {
-        isNotEmpty(carNames)
+    fun validate(input: String) {
+        isNotEmpty(input)
+        val carNames = input.split(COMMA)
+        isValidLength(carNames)
     }
 
     private fun isNotEmpty(carName: String) {
         require(carName.isNotBlank()) { EMPTY_INPUT_ERROR }
     }
 
+    private fun isValidLength(carNames: List<String>) {
+        require(carNames.all { it.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH }) { INVALID_NAME_LENGTH_ERROR }
+    }
+
     companion object {
         private const val ERROR = "[ERROR]"
         const val EMPTY_INPUT_ERROR = "$ERROR 빈 값을 입력하셨습니다."
+        const val INVALID_NAME_LENGTH_ERROR = "$ERROR 이름은 1~5자이어야 합니다."
+
+        const val COMMA = ','
+        const val MIN_NAME_LENGTH = 1
+        const val MAX_NAME_LENGTH = 5
     }
 }
