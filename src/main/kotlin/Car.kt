db1f11b74020398223adcaa733b68583ca76ccd0
지원 <cheerfulboys@naver.com>
2025-02-12 16:41:52 +0900

    feat: 이동 거리 Car가 출력하는 기능 구현

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -3,6 +3,23 @@ class Car(val name: String) {
         private set
 
     fun move() {
-        distance += 1
+        if (isMovable()) {
+            distance += 1
+        }
+        println("$name : ${displayDistance()}")
+    }
+
+    private fun displayDistance(): String {
+        var output = ""
+        repeat(distance) {
+            output += "-"
+        }
+        return output
+    }
+
+    private fun isMovable(): Boolean {
+        // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+        val random: Int = (0..9).random()
+        return random >= 4
     }
 }
