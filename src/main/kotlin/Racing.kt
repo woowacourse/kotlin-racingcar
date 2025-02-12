072aeaf403eb72f1fabd61e541266c1c25345601
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-12 15:49:33 +0900

    feat(Racing): 시도 횟수에 따른 자동차 이동 기능 추가

--- src/main/kotlin/Racing.kt
+++ src/main/kotlin/Racing.kt
@@ -3,19 +3,24 @@ class Racing {
     fun play() {
         val cars = getCars(inputView.readCars())
         val attempts = getAttempts(inputView.readAttempt())
+        race(cars,attempts)
     }
 
     fun getCars(carsInput: String): List<Car> {
         val carsName = carsInput.split(",")
         carsName.forEach {
-            require(it.length < 5) { "[ERROR]: 자동차 이름은 5자를 내외입니다."  }
+            require(it.length < 5) { "[ERROR]: 자동차 이름은 5자를 내외입니다." }
         }
         return carsName.map { carName -> Car(carName.trim()) }
     }
 
-    fun getAttempts(attempts: String) = requireNotNull(attempts.toIntOrNull()) { "[ERROR]: 시도 횟수를 입력해주세요." }
+    fun getAttempts(attempts: String):Int = requireNotNull(attempts.toIntOrNull()) { "[ERROR]: 시도 횟수를 입력해주세요." }
 
-    fun race(cars: List<Car>) {
-        
+    fun race(cars: List<Car>, attempts: Int) {
+        repeat(attempts){
+            cars.forEach { car ->
+                car.move()
+            }
+        }
     }
 }
