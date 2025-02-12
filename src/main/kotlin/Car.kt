eec58f7f3b032fb31a7eef079412350546f760d0
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-12 21:08:54 +0900

    refactor: 컨벤션 수정 및 하드코드 상수화

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,10 +1,10 @@
 import kotlin.random.Random
 
 class Car(val name: String) {
-    private var _position = 0
+    private var _position = INIT_POSITION
     val position get() = _position
 
     fun move() {
-        if (Random.nextInt(10) >= 4) _position++
+        if (Random.nextInt(TRY_NUMBER_UNTIL) >= MOVE_MIN_NUMBER) _position++
     }
 }
--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,15 +1,15 @@
-import java.util.*
+import java.util.Scanner
 
 class InputView {
     private val scanner = Scanner(System.`in`)
 
     fun readCars(): String {
-        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        println(INPUT_CAR_NAME)
         return scanner.nextLine()
     }
 
     fun readAttempt(): String {
-        println("시도할 횟수는 몇 회인가요?")
+        println(INPUT_ATTEMPTS)
         return scanner.nextLine()
     }
 }
