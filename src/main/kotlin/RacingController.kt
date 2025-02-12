7adfdfbc14e95356b2dccd9409ec1b7a2b2977fe
DongjooSEO <readyjoo24@gmail.com>
2025-02-12 15:05:19 +0900

    feat: 자동차 이름 유효성 검사 메소드 구현

--- src/main/kotlin/RacingController.kt
+++ src/main/kotlin/RacingController.kt
@@ -2,13 +2,20 @@ import view.InputView
 
 class RacingController(private val inputView: InputView) {
     private lateinit var carNames: List<String>
+    private var tryNumber: Int = 0
 
-    fun run(){
+    fun run() {
         carNames = splitCarNames(inputView.inputCarNames())
 
     }
 
     private fun splitCarNames(inputCarNames: String): List<String> {
         return inputCarNames.split(',')
+    }
+
+    private fun validateCarNames(splitCarNames: List<String>) {
+        require(splitCarNames.all { it.length <= 5 }) { "각 자동차 이름은 5자를 초과할 수 없습니다." }
+        require(splitCarNames.any { it.isNotEmpty() }) { "각 자동차 이름은 공백이 불가합니다." }
+        require(splitCarNames.size == splitCarNames.toSet().size) { "각 자동차 이름은 중복이 불가합니다." }
     }
 }
