4213d9ec3a71b4c266cb0bb0d215825d08883d38
이지윤 <02leejiyun@naver.com>
2025-02-12 17:32:08 +0900

    feat: 시도할 횟수가 숫자가 아닌 경우 예외 처리

--- src/main/kotlin/racingcar/validator/RoundsValidator.kt
+++ src/main/kotlin/racingcar/validator/RoundsValidator.kt
@@ -3,15 +3,21 @@ package racingcar.validator
 class RoundsValidator {
     fun validate(input: String): Int {
         isNotEmpty(input)
+        isNumeric(input)
         return 0
     }
 
-    private fun isNotEmpty(carName: String) {
-        require(carName.isNotBlank()) { EMPTY_INPUT_ERROR }
+    private fun isNotEmpty(rounds: String) {
+        require(rounds.isNotBlank()) { EMPTY_INPUT_ERROR }
+    }
+
+    private fun isNumeric(rounds: String) {
+        require(rounds.toIntOrNull() != null) { NOT_NUMERIC_ERROR }
     }
 
     companion object {
         private const val ERROR = "[ERROR]"
         const val EMPTY_INPUT_ERROR = "$ERROR 빈 값을 입력하셨습니다."
+        const val NOT_NUMERIC_ERROR = "$ERROR 숫자를 입력해야 합니다."
     }
 }
