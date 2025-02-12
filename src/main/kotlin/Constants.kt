4c4b7129b99515953e5a2638b4e3be5e047dda45
yrsel <xxknu88@gmail.com>
2025-02-12 14:45:08 +0900

    feat: 상수 메세지 작성

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,7 @@
+object Constants {
+    const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
+    const val INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+
+    const val OUTPUT_RESULT_MESSAGE = "실행 결과"
+    const val OUTPUT_FINAL_WINNER_MESSAGE_FORMAT = "최종 우승자: %s"
+}
