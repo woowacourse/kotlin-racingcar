38b4c294fdd00ea3e21c8bcf482d19aed3690177
지원 <cheerfulboys@naver.com>
2025-02-12 15:49:01 +0900

    feat: 사용자 입력 부분 구현

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val cars: List<Car> = readCars()
+    val round: Int = readRound()
+}
+
+fun readCars(): List<Car> {
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val userInput: String? = readlnOrNull()
+    val cars: List<Car> = userInput!!.split(",").map { it.trim() }.map { name -> Car(name) }
+    return cars
+}
+
+
+fun readRound(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    val round = readlnOrNull()!!
+    return round.toInt()
+}
