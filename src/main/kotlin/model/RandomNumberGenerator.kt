ef38ec23b7a4d20a1310878b298621af666a57ca
cucumber99 <dlsguq1@gmail.com>
2025-02-12 16:08:33 +0900

    feat: 난수 생성 기능 구현

---
+++ src/main/kotlin/model/RandomNumberGenerator.kt
@@ -0,0 +1,13 @@
+package model
+
+import kotlin.random.Random
+
+class RandomNumberGenerator: NumberGenerator {
+    override fun generate(): Int {
+        return Random.nextInt(MAX_VALUE)
+    }
+
+    companion object {
+        const val MAX_VALUE = 9
+    }
+}
