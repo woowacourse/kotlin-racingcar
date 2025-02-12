0daa0cf739fe739cf0af2aff817aeaed7e8fd57f
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-12 14:52:44 +0900

    feat(InputView): 시도할 횟수 입력 기능 구현

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -7,4 +7,9 @@ class InputView {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         return scanner.nextLine()
     }
+
+    fun readAttempt(): String {
+        println("시도할 횟수는 몇 회인가요?")
+        return scanner.nextLine()
+    }
 }
