4869c1df79bc3147305435c17193f241fda33a9b
HamBeomJoon <hbj0802@naver.com>
2025-02-12 15:53:18 +0900

    refactor: 메소드 분리

--- src/main/kotlin/Racingcar.kt
+++ src/main/kotlin/Racingcar.kt
@@ -5,42 +5,74 @@ import kotlin.random.Random
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    val carsInput = br.readLine().split(",").map { it.trim() }
-    carsInput.forEach {
-        if (it.isEmpty() || it.length > 5) {
+    fun readCars(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val carsInput = br.readLine().split(",").map { it.trim() }
+        carsInput.forEach {
+            if (it.isEmpty() || it.length > 5) {
+                throw IllegalArgumentException()
+            }
+        }
+
+        if (carsInput.size > carsInput.toSet().size) {
             throw IllegalArgumentException()
         }
+
+        return carsInput
     }
 
-    if (carsInput.size > carsInput.toSet().size) {
-        throw IllegalArgumentException()
+    fun generateCarList(carsInput: List<String>): MutableList<Car> {
+        val cars = mutableListOf<Car>()
+        carsInput.map { cars.add(Car(it, 0)) }
+        return cars
     }
 
-    val cars = mutableSetOf<Car>()
-    carsInput.map { cars.add(Car(it, 0)) }
+    fun readCount(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        val count = br.readLine().toIntOrNull() ?: throw IllegalArgumentException()
+        if (count <= 0) throw IllegalArgumentException()
+        println()
+        return count
+    }
 
-    println("시도할 횟수는 몇 회인가요?")
-    val count = br.readLine().toIntOrNull() ?: throw IllegalArgumentException()
-    if (count <= 0) throw IllegalArgumentException()
-    println()
+    fun printRound(car: Car) {
+        println("${car.name} : ${"-".repeat(car.position)}")
+    }
 
-    println("실행 결과")
-    repeat(count) {
+    fun moveCars(cars: List<Car>) {
         cars.forEach {
             val number = Random.nextInt(10)
             if (number >= 4) it.position++
-            println("${it.name} : ${"-".repeat(it.position)}")
-        }
-        println()
-    }
-
-    val winner = mutableListOf<String>()
-    cars.forEach { car ->
-        if (car.position == cars.maxOf { it.position }) {
-            winner.add(car.name)
+            printRound(it)
         }
     }
 
-    println("최종 우승자: ${winner.joinToString(", ")}")
+    fun race(count: Int, cars: List<Car>) {
+        println("실행 결과")
+        repeat(count) {
+            moveCars(cars)
+            println()
+        }
+    }
+
+    fun printWinner(cars: List<Car>) {
+        val winner = mutableListOf<String>()
+        cars.forEach { car ->
+            if (car.position == cars.maxOf { it.position }) {
+                winner.add(car.name)
+            }
+        }
+
+        println("최종 우승자: ${winner.joinToString(", ")}")
+    }
+
+    fun start() {
+        val carInput = readCars()
+        val cars = generateCarList(carInput)
+        val count = readCount()
+        race(count, cars)
+        printWinner(cars)
+    }
+
+    start()
 }
