f202e7c34a4eea7be8bff7ac2ef8ebe1c134dc4a
HamBeomJoon <hbj0802@naver.com>
2025-02-12 14:49:41 +0900

    feat: 경주에 참가할 자동차 이름 입력

---
+++ src/main/kotlin/Racingcar.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Racingcar {
+
+    fun main() {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val carsInput = br.readLine().split(",").map { it.trim() }
+    }
+
+}
