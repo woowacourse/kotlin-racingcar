1b398e0f01ce067557608c7dcb3e4672f58b5144
oungsi2000 <oungsi1000@gmail.com>
2025-02-12 17:32:03 +0900

    feat:전체 기능 완성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,6 +2,7 @@ package racingcar
 
 import racingcar.Messages.*
 import racingcar.InputValidator
+import java.util.*
 
 fun main() {
     Application.run()
@@ -22,6 +23,32 @@ class Application{
             val raceCountInput = readln()
             inputValidator.tryCountValidate(raceCountInput)
             val raceCount: Int = raceCountInput.toInt()
+            val cars = toCars(names)
+
+            result(raceCount, cars)
+            getWinner(cars)
+        }
+
+        private fun toCars(names:List<String>):List<Car> = names.map { Car(it) }
+
+        private fun result(raceCount:Int, cars:List<Car>) {
+            print(GAME_RESULT.message)
+            for (i in 0 until raceCount) {
+                race(cars)
+            }
+            print("\n")
+        }
+
+        private fun getWinner(cars:List<Car>) {
+            val winners = cars.filter {it.distance == cars.maxOf {car->car.distance}}
+            println("최종 우승자 :  ${winners.joinToString(", ") {it.name}}")
+        }
+
+        private fun race(cars:List<Car>) {
+            cars.forEach {
+                it.randomMove()
+                it.printDistanceInfo()
+            }
         }
     }
 }
