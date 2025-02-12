0ba32da683d30651ee39cdd45439b78238947296
지원 <cheerfulboys@naver.com>
2025-02-12 18:00:18 +0900

    refactor: isMovable 메서드를 Car 인스턴스 생성 없이 테스트 하기 위해 리팩토링

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -3,7 +3,8 @@ class Car(val name: String) {
         private set
 
     fun move() {
-        if (isMovable()) {
+        val random: Int = (0..9).random()
+        if (isMovable(random)) {
             distance += 1
         }
         println("$name : ${displayDistance()}")
@@ -17,9 +18,10 @@ class Car(val name: String) {
         return output
     }
 
-    private fun isMovable(): Boolean {
-        // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
-        val random: Int = (0..9).random()
-        return random >= 4
+    companion object {
+        fun isMovable(random: Int): Boolean {
+            // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+            return random >= 4
+        }
     }
 }
