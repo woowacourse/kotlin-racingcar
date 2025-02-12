2f2ce6aac21ec290a8a18935360aa300ff5e306a
HamBeomJoon <hbj0802@naver.com>
2025-02-12 15:11:02 +0900

    feat: 이동 횟수만큼 경주 진행

--- src/main/kotlin/Racingcar.kt
+++ src/main/kotlin/Racingcar.kt
@@ -1,18 +1,28 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.random.Random
 
-class Racingcar {
 
-    fun main() {
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        val carsInput = br.readLine().split(",").map { it.trim() }
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val carsInput = br.readLine().split(",").map { it.trim() }
 
-        val cars = mutableSetOf<Car>()
-        carsInput.map { cars.add(Car(it, 0)) }
+    val cars = mutableSetOf<Car>()
+    carsInput.map { cars.add(Car(it, 0)) }
 
-        println("시도할 횟수는 몇 회인가요?")
-        val count = br.readLine().toInt()
+    println("시도할 횟수는 몇 회인가요?")
+    val count = br.readLine().toInt()
+    println()
+
+    println("실행 결과")
+    repeat(count) {
+        cars.forEach {
+            val number = Random.nextInt(10)
+            if (number >= 4) it.position++
+            println("${it.name} : ${"-".repeat(it.position)}")
+        }
+        println()
     }
 
 }
