c0b70bf64c5ef57f55fabac34ee81e9852fd24f7
DongjooSEO <readyjoo24@gmail.com>
2025-02-12 14:29:30 +0900

    fix: InputView 입력 메소드 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,11 +5,11 @@ import java.io.Console
 class InputView {
     fun inputCarNames(): String {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n")
-        return Console.readLine()
+        return readlnOrNull() ?: ""
     }
 
     fun inputTryNumber(): String {
         println("시도할 횟수는 몇 회인가요?\n")
-        return Console.readLine()
+        return readlnOrNull() ?: ""
     }
 }
