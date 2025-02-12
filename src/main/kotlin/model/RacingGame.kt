f0c188416ca312b794374652e212f7b6a2c4155b
gahyunkim <mapledt001@naver.com>
2025-02-12 17:28:17 +0900

    refactor: 경주 횟수 검증 로직 수정

--- src/main/kotlin/model/RacingGame.kt
+++ src/main/kotlin/model/RacingGame.kt
@@ -1,6 +1,5 @@
 package model
 
-import kotlin.math.max
 import kotlin.random.Random
 
 class RacingGame {
@@ -14,8 +13,7 @@ class RacingGame {
     }
 
     fun tryRacing(rawCount: String): String {
-        require(rawCount.toIntOrNull() != null) { INVALID_COUNT }
-        require(rawCount.toInt() > 0) { NEGATIVE_COUNT }
+        require(rawCount.toIntOrNull()?.let { it > 0 } == true) { INVALID_COUNT }
         var result = "실행 결과"
 
         repeat(rawCount.toInt()) {
@@ -32,13 +30,12 @@ class RacingGame {
 
     fun calculateWinner(): String {
         val maxValue = racingCars.maxOfOrNull { it.getStep().length }
-        return racingCars.filter { it.getStep().length == maxValue }.map{it.getName()}.joinToString(", ")
+        return racingCars.filter { it.getStep().length == maxValue }.map { it.getName() }.joinToString(", ")
     }
 
     private companion object {
         const val INVALID_CAR_SIZE = "레이싱 게임은 두대 이상이어야 합니다."
         const val DUPLICATE_CAR_NAME = "자동차 이름이 중복됩니다."
-        const val INVALID_COUNT = "숫자가 아닙니다."
-        const val NEGATIVE_COUNT = "횟수는 양수여야 합니다."
+        const val INVALID_COUNT = "시도할 횟수는 자연수여야 합니다."
     }
 }
