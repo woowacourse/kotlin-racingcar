b010cfd9d7ad846d8075c392a9b1ba7e3d275b48
cucumber99 <dlsguq1@gmail.com>
2025-02-12 17:11:18 +0900

    feat: 컨트롤러 구현

---
+++ src/main/kotlin/controller/RacingController.kt
@@ -0,0 +1,39 @@
+package controller
+
+import model.AttemptCount
+import model.Cars
+import model.Race
+
+class RacingController(
+    private val userInterface: UserInterface = UserInterface()
+) {
+    fun run() {
+        val cars = getCarNames()
+        val attemptCount = getAttemptCount()
+        val race = Race(cars)
+        playRacing(cars, attemptCount, race)
+        val winners = race.getWinner()
+        userInterface.handleWinnerMessage(winners)
+    }
+
+    private fun getCarNames(): Cars = Cars(userInterface.handleStartMessage())
+
+    private fun getAttemptCount(): AttemptCount = AttemptCount(userInterface.handleAttemptCountMessage())
+
+    private fun playRacing(
+        cars: Cars,
+        attemptCount: AttemptCount,
+        race: Race
+    ) {
+        userInterface.handleStartRoundMessage()
+        repeat(attemptCount.getCount()) {
+            race.playOneRound()
+            userInterface.handleRoundResultMessage(cars)
+        }
+    }
+
+    private fun showWinners(race: Race) {
+        val winners = race.getWinner()
+        userInterface.handleWinnerMessage(winners)
+    }
+}
