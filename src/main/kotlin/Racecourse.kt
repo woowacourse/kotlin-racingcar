38c7a780f65527b7206be39d14ae3ae6b9d634a9
지원 <cheerfulboys@naver.com>
2025-02-12 16:41:15 +0900

    feat: Racecourse 클래스 구현

---
+++ src/main/kotlin/Racecourse.kt
@@ -0,0 +1,19 @@
+class Racecourse(private val cars: List<Car>, private val round: Int) {
+    fun startRace() {
+        repeat(round) {
+            startRound()
+            println()
+        }
+        showWinners()
+    }
+
+    private fun startRound() {
+        cars.forEach { car: Car -> car.move() }
+    }
+
+    private fun showWinners() {
+        val maxDistance = cars.maxOf { car: Car -> car.distance }
+        val winners: List<Car> = cars.filter { car: Car -> car.distance == maxDistance }
+        println("최종 우승자: ${winners.joinToString { car: Car -> car.name }}")
+    }
+}
