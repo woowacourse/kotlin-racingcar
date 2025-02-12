225f448818360842871ffefea8af456071309cfa
yrsel <xxknu88@gmail.com>
2025-02-12 15:56:54 +0900

    feat: 시도횟수 검증 구현

--- src/main/kotlin/InputValidator.kt
+++ src/main/kotlin/InputValidator.kt
@@ -1,20 +1,33 @@
+import Constants.CAR_NAME_DELIMITER
+import Constants.ERROR_DUPLICATED_CAR_NAME_MESSAGE
+import Constants.ERROR_EMPTY_CAR_NAME_MESSAGE
+import Constants.ERROR_EMPTY_TRY_COUNT_MESSAGE
+import Constants.ERROR_IS_NOT_NUMBER_MESSAGE
+import Constants.TRY_COUNT_LOWER_BOUND
+import Constants.TRY_COUNT_UPPER_BOUND
+
 object InputValidator {
 
     fun validateCarName(input: String) {
-        require(input.isNotEmpty()) {Constants.ERROR_EMPTY_CAR_NAME_MESSAGE}
-        val carNames = input.split(",")
+        require(input.isNotEmpty()) { ERROR_EMPTY_CAR_NAME_MESSAGE }
+        val carNames = input.split(CAR_NAME_DELIMITER)
         carNames.forEach {
             validateCarNameLength(it)
         }
-        require(validateDuplicatedCarName(carNames)) {Constants.ERROR_DUPLICATED_CAR_NAME_MESSAGE}
+        require(validateDuplicatedCarName(carNames)) { ERROR_DUPLICATED_CAR_NAME_MESSAGE }
     }
 
     private fun validateCarNameLength(input: String) {
-        require(input.length in 1..5) {Constants.ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE}
+        require(input.length in TRY_COUNT_LOWER_BOUND..TRY_COUNT_UPPER_BOUND) { Constants.ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE }
     }
 
     private fun validateDuplicatedCarName(input: List<String>): Boolean {
         return input.distinct().size != input.size
+    }
+
+    fun validateTryCount(input: String) {
+        require(input.isNotEmpty()) { ERROR_EMPTY_TRY_COUNT_MESSAGE }
+        require(input.toIntOrNull() != null) { ERROR_IS_NOT_NUMBER_MESSAGE }
     }
 
 }
