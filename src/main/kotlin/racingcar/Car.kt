ad943df9e5a60777a095f251884bccc9de82eab5
oungsi2000 <oungsi1000@gmail.com>
2025-02-12 17:32:03 +0900

    feat:전체 기능 완성

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,7 +2,7 @@ package racingcar
 
 import kotlin.random.Random
 
-class Car(private val name:String, private var distance:Int) {
+class Car(val name:String, var distance:Int = 0) {
 	private val seed:Int = 134143524
 	private val random:Random = Random(seed)
 
