a3f6d77f5491fb2f97561d1f78e288aea4e2e092
m6z1 <sonmyungj1zz@gmail.com>
2025-02-12 17:32:27 +0900

    feat: 우승자 판별 객체 생성

---
+++ src/main/kotlin/Judge.kt
@@ -0,0 +1,9 @@
+class Judge {
+    fun selectWinners(cars: List<Car>): List<String> {
+        val maxPosition = cars.maxOf { car -> car.position }
+        return cars
+            .filter { car ->
+                car.position == maxPosition
+            }.map { it.name }
+    }
+}
