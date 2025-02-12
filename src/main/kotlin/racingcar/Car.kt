99719c2b5c5182b69160c41a3cf51e7f7198413f
oungsi2000 <oungsi1000@gmail.com>
2025-02-12 16:59:58 +0900

    feat(Car.kt) : Car 객체 구현

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,5 +1,14 @@
 package racingcar
 
-class Car {
+import kotlin.random.Random
 
+class Car(private val name:String, private var distance:Int) {
+	private val seed:Int = 134143524
+	private val random:Random = Random(seed)
+
+	fun randomMove() {
+		if (random.nextInt(0, 10) >= 4 ) distance++
+	}
+
+	fun printDistanceInfo() = println("$name : ${ "-".repeat(distance) }")
 }
