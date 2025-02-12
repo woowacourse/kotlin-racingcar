dc5df016365483f62bc14514146d155e46f45f30
박지민 <pjm5489@naver.com>
2025-02-12 16:47:18 +0900

    refactor: private 제거

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,7 +1,7 @@
 package racingcar
 
 class Car(val carName: String) {
-    private val moveOrStop = mutableListOf<String>()
+    val moveOrStop = mutableListOf<String>()
 
     fun moves(isMove: Boolean) {
         when (isMove) {
