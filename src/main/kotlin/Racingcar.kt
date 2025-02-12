eebdcc7dff997a3c3be890f904754c18cfce8507
HamBeomJoon <hbj0802@naver.com>
2025-02-12 14:59:36 +0900

    feat: 입력받은 자동차들을 Car 객체에 저장

--- src/main/kotlin/Racingcar.kt
+++ src/main/kotlin/Racingcar.kt
@@ -7,6 +7,10 @@ class Racingcar {
         val br = BufferedReader(InputStreamReader(System.`in`))
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         val carsInput = br.readLine().split(",").map { it.trim() }
+
+        val cars = mutableSetOf<Car>()
+        carsInput.map { cars.add(Car(it, 0)) }
+
     }
 
 }
