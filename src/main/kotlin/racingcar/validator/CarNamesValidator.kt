7add6af799b1497c127c4540e47a556a9c82e591
이지윤 <02leejiyun@naver.com>
2025-02-12 16:23:34 +0900

    feat: 자동차 이름이 중복된 경우 예외 처리

--- src/main/kotlin/racingcar/validator/CarNamesValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNamesValidator.kt
@@ -5,6 +5,7 @@ class CarNamesValidator {
         isNotEmpty(input)
         val carNames = input.split(COMMA)
         isValidLength(carNames)
+        isUniqueName(carNames)
     }
 
     private fun isNotEmpty(carName: String) {
@@ -15,10 +16,15 @@ class CarNamesValidator {
         require(carNames.all { it.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH }) { INVALID_NAME_LENGTH_ERROR }
     }
 
+    private fun isUniqueName(carNames: List<String>) {
+        require(carNames.size == carNames.distinct().size) { NOT_UNIQUE_NAME_ERROR }
+    }
+
     companion object {
         private const val ERROR = "[ERROR]"
         const val EMPTY_INPUT_ERROR = "$ERROR 빈 값을 입력하셨습니다."
-        const val INVALID_NAME_LENGTH_ERROR = "$ERROR 이름은 1~5자이어야 합니다."
+        const val INVALID_NAME_LENGTH_ERROR = "$ERROR 자동차 이름은 1~5자이어야 합니다."
+        const val NOT_UNIQUE_NAME_ERROR = "$ERROR 자동차 이름이 중복됩니다."
 
         const val COMMA = ','
         const val MIN_NAME_LENGTH = 1
