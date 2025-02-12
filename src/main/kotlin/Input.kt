706b4bc1e4f125f5a63f5186bc00cb5d4c5ba9b9
m6z1 <sonmyungj1zz@gmail.com>
2025-02-12 17:41:11 +0900

    feat: 자동차 대수 검증 추가

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -2,7 +2,7 @@ class Input {
     fun readCarNames(): List<Car> {
         val carNames = readln().split(",")
         require(carNames.size == carNames.toSet().size) { "[ERROR] 자동차 이름은 중복될 수 없습니다." }
-
+        require(carNames.size > 1) { "[ERROR] 자동차가 한 대일 경우 경기가 취소됩니다." }
         return carNames.map { name ->
             Car(name)
         }
