0ca47d3e3730269705f0c00a19d5b48874373c61
DongjooSEO <readyjoo24@gmail.com>
2025-02-12 17:15:47 +0900

    refactor: 매직 넘버 상수화

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -5,11 +5,18 @@ import kotlin.random.nextInt
 
 class Car (
     val name: String,
-    var position: Int = 0
+    var position: Int = INIT_POSITION
 ){
     fun move() {
-        if (Random.nextInt(0..9) >= 4) {
+        if (Random.nextInt(MIN_RANGE..MAX_RANGE) >= STANDARD_NUMBER) {
             ++position
         }
+    }
+
+    companion object {
+        const val INIT_POSITION = 0
+        const val MIN_RANGE = 0
+        const val MAX_RANGE = 9
+        const val STANDARD_NUMBER = 4
     }
 }
