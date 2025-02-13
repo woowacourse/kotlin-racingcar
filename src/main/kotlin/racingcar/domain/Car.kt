5874856f360885e3b577b194c44a4ba85efa58db
oungsi2000 <oungsi1000@gmail.com>
2025-02-13 11:51:12 +0900

    feat:테스트 코드 작성

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,11 +1,10 @@
 package racingcar.domain
 
-import kotlin.random.Random
-
-class Car(val name: String, private val random: Random, var distance: Int = 0) {
-    fun randomMove() {
-        if (random.nextInt(0, 10) >= 4) distance++
-    }
+class Car(
+    val name: String,
+    var distance: Int = 0,
+) {
+    fun randomMove() = distance++
 
     fun printDistanceInfo() = println("$name : ${ "-".repeat(distance) }")
 }
