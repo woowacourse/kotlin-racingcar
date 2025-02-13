ed56939ba68e0d3df3588d3ba94197181ee985dd
m6z1 <sonmyungj1zz@gmail.com>
2025-02-13 15:27:48 +0900

    test: 우승자 반환에 대한 테스트 추가

--- src/test/kotlin/JudgeTest.kt
+++ src/test/kotlin/JudgeTest.kt
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -8,12 +11,50 @@ class JudgeTest {
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2, 3])
     fun `랜덤 숫자가 4 미만인 경우 false 를 반환한다`(randomNumber: Int) {
-        Assertions.assertFalse(judge.isCarAbleToMove(randomNumber))
+        assertFalse(judge.isCarAbleToMove(randomNumber))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [4, 5, 6, 7, 8, 9])
     fun `랜덤 숫자가 4 이상인 경우 true 를 반환한다`(randomNumber: Int) {
-        Assertions.assertTrue(judge.isCarAbleToMove(randomNumber))
+        assertTrue(judge.isCarAbleToMove(randomNumber))
+    }
+
+    @Test
+    fun `주어진 자동차 목록에서 가장 많이 전진한 우승자의 이름을 반환한다`() {
+        val car1 = Car("제이슨")
+        val car2 = Car("디노")
+        val car3 = Car("레아")
+        val car4 = Car("제임스")
+
+        repeat(4) { car1.moveForward(true) }
+        repeat(3) { car2.moveForward(true) }
+        repeat(2) { car3.moveForward(true) }
+        repeat(1) { car4.moveForward(true) }
+
+        val cars = listOf(car1, car2, car3, car4)
+
+        val winnerNames = judge.selectWinnerNames(cars)
+
+        Assertions.assertEquals(listOf("제이슨"), winnerNames)
+    }
+
+    @Test
+    fun `주어진 자동차 목록에서 가장 많이 전진한 여러 명의 우승자의 이름들을 반환한다`() {
+        val car1 = Car("제이슨")
+        val car2 = Car("디노")
+        val car3 = Car("레아")
+        val car4 = Car("제임스")
+
+        repeat(4) { car1.moveForward(true) }
+        repeat(4) { car2.moveForward(true) }
+        repeat(4) { car3.moveForward(true) }
+        repeat(4) { car4.moveForward(true) }
+
+        val cars = listOf(car1, car2, car3, car4)
+
+        val winnerNames = judge.selectWinnerNames(cars)
+
+        Assertions.assertEquals(listOf("제이슨", "디노", "레아", "제임스"), winnerNames)
     }
 }
