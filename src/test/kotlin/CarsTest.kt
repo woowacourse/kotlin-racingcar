1955b1788e117ea8189491fdc3fbcadcbf769c88
yrsel <xxknu88@gmail.com>
2025-02-13 11:24:47 +0900

    feat: 최종 우승자 확인 테스트

---
+++ src/test/kotlin/CarsTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CarsTest {
+    @Test
+    fun `우승자를 확인할 수 있다 `() {
+        val cars = getDummy()
+        val expected = "포비, 크론, 디노"
+
+        Assertions.assertEquals(expected, cars.getWinners())
+    }
+}
+
+fun getDummy(): Cars {
+    val cars = Cars("포비,크론,디노")
+    cars.lap(TestMoveNumberGenerator())
+    return cars
+}
