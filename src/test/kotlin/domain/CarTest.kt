268ef512952fb3aaa155433230499260b9560dab
ijh1298 <imjh0417@gmail.com>
2025-02-13 13:57:01 +0900

    test: CarTest move 메소드 동작 테스트 코드 추가

---
+++ src/test/kotlin/domain/CarTest.kt
@@ -0,0 +1,33 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CarTest {
+    private val car = Car("테스트", 0)
+
+    @BeforeEach
+    fun init() {
+        car.position = 0
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        ints = [4, 5, 6, 7, 8, 9],
+    )
+    fun `무작위 값이 4 이상인 경우, 자동차는 전진한다`(num: Int) {
+        car.move(num)
+        assertThat(car.position == 1).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        ints = [0, 1, 2, 3],
+    )
+    fun `무작위 값이 4 미만인 경우, 자동차는 전진하지 않는다`(num: Int) {
+        car.move(num)
+        assertThat(car.position == 0).isTrue()
+    }
+}
