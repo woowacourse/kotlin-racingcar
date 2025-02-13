1b794a885d2afc3c335d55883607cbb105e711d9
gahyunkim <mapledt001@naver.com>
2025-02-13 10:43:48 +0900

    refactor: RacingGame 상수화 적용

--- src/main/kotlin/model/RacingGame.kt
+++ src/main/kotlin/model/RacingGame.kt
@@ -6,9 +6,9 @@ class RacingGame {
     private lateinit var racingCars: List<Car>
 
     fun generateCars(rawCars: String) {
-        racingCars = rawCars.split(",").map { Car(it.trim()) }
+        racingCars = rawCars.split(DELIMITER).map { Car(it.trim()) }
 
-        require(racingCars.size > 1) { INVALID_CAR_SIZE }
+        require(racingCars.size > RACING_CAR_MIN_SIZE) { INVALID_CAR_SIZE }
         require(racingCars.size == racingCars.distinct().size) { DUPLICATE_CAR_NAME }
     }
 
@@ -37,5 +37,7 @@ class RacingGame {
         const val INVALID_CAR_SIZE = "레이싱 게임은 두대 이상이어야 합니다."
         const val DUPLICATE_CAR_NAME = "자동차 이름이 중복됩니다."
         const val INVALID_COUNT = "시도할 횟수는 자연수여야 합니다."
+        const val DELIMITER = ","
+        const val RACING_CAR_MIN_SIZE = 1
     }
 }
