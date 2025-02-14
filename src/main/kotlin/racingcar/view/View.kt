f182cbc735f7f84a6e89347e5b45ef507b3d31a4
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-14 17:24:44 +0900

    refactor: RoundManager, RoundManagerTest 생성

--- src/main/kotlin/racingcar/view/View.kt
+++ src/main/kotlin/racingcar/view/View.kt
@@ -1,10 +1,18 @@
 package racingcar.view
 
 import racingcar.Car
+import racingcar.Racecourse
+import racingcar.RoundManager
 import racingcar.util.ErrorMessage
 
 class View {
-    fun readCars(): List<Car> {
+    fun playRacingGame() {
+        val cars: List<Car> = readCars()
+        val roundManager: RoundManager = readRound()
+        showResult(cars, roundManager)
+    }
+
+    private fun readCars(): List<Car> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
 
         runCatching {
@@ -21,15 +29,15 @@ class View {
         throw IllegalStateException(ErrorMessage.RUNCATCHING_NO_RESULT)
     }
 
-    fun readRound(): Int {
+    private fun readRound(): RoundManager {
         println("시도할 횟수는 몇 회인가요?")
 
         runCatching {
             val userInput: String =
                 readlnOrNull() ?: throw IllegalStateException(ErrorMessage.FAIL_TO_READ_INPUT)
-            parseToRound(userInput)
-        }.onSuccess { round: Int ->
-            return round
+            RoundManager.from(userInput)
+        }.onSuccess { roundManager: RoundManager ->
+            return roundManager
         }.onFailure { error: Throwable ->
             println(error.message)
             return readRound()
@@ -38,11 +46,13 @@ class View {
         throw IllegalStateException(ErrorMessage.RUNCATCHING_NO_RESULT)
     }
 
-    fun parseToRound(value: String): Int {
-        require(value.toIntOrNull() != null && value.toInt() > 0) {
-            ErrorMessage.INVALID_ROUND
-        }
-
-        return value.toInt()
+    private fun showResult(
+        cars: List<Car>,
+        roundManager: RoundManager,
+    ) {
+        println("\n실행결과")
+        val racecourse = Racecourse(cars, roundManager)
+        racecourse.startRace { println() }
+        print("최종 우승자: ${racecourse.winners.joinToString { car: Car -> car.name }}")
     }
 }
