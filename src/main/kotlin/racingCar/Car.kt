d9ffbad19fe6a6d2ae7273c102ad33f38de17fa5
m6z1 <sonmyungj1zz@gmail.com>
2025-02-14 16:09:24 +0900

    refactor: 자동차 이름에 대한 에러메시지 상수 객체 분리

--- src/main/kotlin/racingCar/Car.kt
+++ src/main/kotlin/racingCar/Car.kt
@@ -7,10 +7,8 @@ class Car(
         private set
 
     init {
-        require(name.isNotBlank()) { "[ERROR] 자동차 이름은 공백일 수 없습니다." }
-        require(name.length in MIN_CAR_NAME_LENGTH..MAX_CAR_NAME_LENGTH) {
-            "[ERROR] 자동차 이름은 ${MIN_CAR_NAME_LENGTH}자 이상 ${MAX_CAR_NAME_LENGTH}자 이하여야 합니다."
-        }
+        require(name.isNotBlank()) { CarErrorConstant.BLANK_NAME }
+        require(name.length in MIN_CAR_NAME_LENGTH..MAX_CAR_NAME_LENGTH) { CarErrorConstant.OUT_OF_RANGE_NAME }
     }
 
     fun moveForward(isMoved: Boolean) {
@@ -18,8 +16,8 @@ class Car(
     }
 
     companion object {
-        private const val MIN_CAR_NAME_LENGTH = 1
-        private const val MAX_CAR_NAME_LENGTH = 5
+        const val MIN_CAR_NAME_LENGTH = 1
+        const val MAX_CAR_NAME_LENGTH = 5
         private const val INITIAL_CAR_POSITION = 0
     }
 }
