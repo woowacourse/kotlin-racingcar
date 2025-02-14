ce8be215947be2b0eb2304a511b3fd3377095726
HamBeomJoon <hbj0802@naver.com>
2025-02-14 15:10:47 +0900

    test: 자동차 전진 테스트 로직 수정

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -27,9 +27,9 @@ class CarTest {
     fun `무작위 값이 4 이상이면 자동차가 전진한다`() {
         val number = (Constants.CONDITION_RANDOM_MIN..Constants.CONDITION_RANDOM_MAX).random()
 
-        racingController.moveCars(number, car)
+        car.move(number)
 
-        if (number >= Constants.CONDITION_MOVE_THRESHOLD) {
+        if (number >= Car.CONDITION_MOVE_THRESHOLD) {
             assertThat(car.position).isEqualTo(1)
         } else {
             assertThat(car.position).isEqualTo(0)
