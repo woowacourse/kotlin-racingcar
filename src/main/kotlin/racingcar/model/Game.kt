18b53491d72107691073e160c7ab668c235b662c
이지윤 <02leejiyun@naver.com>
2025-02-14 15:21:20 +0900

    refactor: 게임 객체에서 자동차 이름 중복 여부 검사

--- src/main/kotlin/racingcar/model/Game.kt
+++ src/main/kotlin/racingcar/model/Game.kt
@@ -3,6 +3,10 @@ package racingcar.model
 import racingcar.enums.Move
 
 class Game(private val cars: List<Car>) {
+    init {
+        validateUniqueName(cars.map { it.name })
+    }
+
     fun playRound() {
         cars.forEach { car ->
             val randomNumber = makeRandomNumber()
@@ -23,11 +27,18 @@ class Game(private val cars: List<Car>) {
         return winners
     }
 
+    private fun validateUniqueName(carNames: List<String>) {
+        require(carNames.size == carNames.distinct().size) { NOT_UNIQUE_NAME_ERROR }
+    }
+
     private fun makeRandomNumber(): Int {
         return (MIN_RANDOM_NUMBER..MAX_RANDOM_NUMBER).random()
     }
 
     companion object {
+        private const val ERROR = "[ERROR]"
+        const val NOT_UNIQUE_NAME_ERROR = "$ERROR 자동차 이름이 중복됩니다."
+
         const val MIN_RANDOM_NUMBER = 0
         const val MAX_RANDOM_NUMBER = 9
     }
--- src/main/kotlin/racingcar/validator/CarNamesValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNamesValidator.kt
@@ -4,7 +4,6 @@ class CarNamesValidator {
     fun validate(input: String): List<String> {
         isNotEmpty(input)
         val carNames = input.split(COMMA).map { it.trim() }
-        isUniqueName(carNames)
         return carNames
     }
 
@@ -12,14 +11,9 @@ class CarNamesValidator {
         require(carName.isNotBlank()) { EMPTY_INPUT_ERROR }
     }
 
-    private fun isUniqueName(carNames: List<String>) {
-        require(carNames.size == carNames.distinct().size) { NOT_UNIQUE_NAME_ERROR }
-    }
-
     companion object {
         private const val ERROR = "[ERROR]"
         const val EMPTY_INPUT_ERROR = "$ERROR 빈 값을 입력하셨습니다."
-        const val NOT_UNIQUE_NAME_ERROR = "$ERROR 자동차 이름이 중복됩니다."
 
         const val COMMA = ","
     }
