22feaea447f9d9f41332229ef1516b29161f6021
m6z1 <sonmyungj1zz@gmail.com>
2025-02-14 20:58:09 +0900

    refactor: 최대 거리 찾는 로직과 우승자 찾는 로직 함수 분리

--- src/main/kotlin/racingCar/Judge.kt
+++ src/main/kotlin/racingCar/Judge.kt
@@ -4,12 +4,14 @@ class Judge {
     fun isCarAbleToMove(randomNumber: Int): Boolean = randomNumber >= MIN_RANDOM_AVAILABILITY_CONDITION
 
     fun selectWinnerNames(raceCars: List<Car>): List<String> {
-        val maxPosition = raceCars.maxOf { raceCar -> raceCar.position }
+        val maxPosition = findMaxPosition(raceCars)
         return raceCars
             .filter { raceCar ->
                 raceCar.position == maxPosition
             }.map { winner -> winner.name }
     }
+
+    private fun findMaxPosition(raceCars: List<Car>) = raceCars.maxOf { raceCar -> raceCar.position }
 
     companion object {
         private const val MIN_RANDOM_AVAILABILITY_CONDITION = 4
