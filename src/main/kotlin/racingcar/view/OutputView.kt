d3f8009b51b973dd4a030891aeb1f521e210bd5a
HamBeomJoon <hbj0802@naver.com>
2025-02-14 16:57:56 +0900

    refactor: 라운드 결과 출력 함수 수정

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -5,8 +5,10 @@ import racingcar.util.Constants
 import racingcar.util.Messages
 
 object OutputView {
-    fun printRound(car: Car) {
-        println("${car.name} : ${Constants.INDICATOR_CAR_POSITION.repeat(car.position)}")
+    fun printRound(cars: List<Car>) {
+        cars.forEach { car ->
+            println("${car.name} : ${Constants.INDICATOR_CAR_POSITION.repeat(car.position)}")
+        }
     }
 
     fun printResultHeader() {
