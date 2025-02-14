a69e25d8d2791e55b1da93882a13889cf1b41a60
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-14 13:50:56 +0900

    chore: move package

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+import racingcar.Car
+import racingcar.Racecourse
+
+fun main() {
+    val cars: List<Car> = readCars()
+    val round: Int = readRound()
+    println("\n실행결과")
+    val racecourse = Racecourse(cars, round)
+    racecourse.startRace()
+}
+
+fun readCars(): List<Car> {
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val userInput: String? = readlnOrNull()
+    if (checkCarNameValid(userInput)) {
+        return userInput!!.toCars()
+    }
+    println("자동차 이름은 5자를 초과할 수 없습니다. 다시 입력해주세요.")
+    return readCars()
+}
+
+fun checkCarNameValid(userInput: String?): Boolean {
+    if (userInput == null) return false
+    val cars = userInput.toCars()
+    return cars.all { car -> car.name.length <= 5 }
+}
+
+fun String.toCars(): List<Car> =
+    split(",")
+        .map { it.trim() }
+        .filter { it.isNotBlank() }
+        .map { name -> Car(name) }
+
+fun readRound(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    val userInput: String? = readlnOrNull()
+    if (checkRoundValid(userInput)) {
+        return userInput!!.toInt()
+    }
+    println("올바른 숫자를 입력하세요.")
+    return readRound()
+}
+
+fun checkRoundValid(userInput: String?): Boolean {
+    if (userInput == null) return false
+    if (userInput.toIntOrNull() == null) return false
+    if (userInput.toInt() <= 0) return false
+    return true
+}
---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,56 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import racingcar.Car
+import racingcar.Racecourse
+
+class ApplicationTest {
+    @Test
+    fun `주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다`() {
+        val car = Car("Test")
+        car.move()
+        assertThat(car.distance).isIn(car.distance, car.distance + 1)
+    }
+
+    @Test
+    fun `자동차 이름은 5자를 초과할 수 없다`() {
+        assertThat(checkCarNameValid("5자 이상의 자동차 이름")).isFalse()
+    }
+
+    @Test
+    fun `자동차 이름은 쉼표(,)를 기준으로 구분한다`() {
+        val userInputExample = "자동차,이름은,쉼표로,구분한다"
+        val cars = userInputExample.toCars()
+        assertThat(cars.map { car: Car -> car.name }).isEqualTo(listOf("자동차", "이름은", "쉼표로", "구분한다"))
+    }
+
+    @Test
+    fun `시도횟수는 1 이상의 숫자여야 한다`() {
+        assertThat(checkRoundValid(null)).isFalse()
+        assertThat(checkRoundValid("")).isFalse()
+        assertThat(checkRoundValid("0")).isFalse()
+        assertThat(checkRoundValid("-1")).isFalse()
+        assertThat(checkRoundValid("1.1")).isFalse()
+        assertThat(checkRoundValid("asd")).isFalse()
+        assertThat(checkRoundValid("${Int.MAX_VALUE + 1}")).isFalse()
+        assertThat(checkRoundValid("10")).isTrue()
+    }
+
+    @Test
+    fun `전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다`() {
+        (0..3).forEach { random -> assertThat(Car.isMovable(random)).isFalse() }
+        (4..9).forEach { random -> assertThat(Car.isMovable(random)).isTrue() }
+    }
+
+    @Test
+    fun `자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다`() {
+        val cars: List<Car> =
+            listOf(
+                Car(name = "a", initialDistance = 5),
+                Car(name = "b", initialDistance = 0),
+                Car(name = "c", initialDistance = 0),
+            )
+        val racecourse = Racecourse(cars, 0)
+        val expectedWinners = listOf(Car(name = "a", initialDistance = 5))
+        assertThat(racecourse.winners).isEqualTo(expectedWinners)
+    }
+}
