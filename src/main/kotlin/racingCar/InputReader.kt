b2051531286b126e30f528b7673983be57d042b4
m6z1 <sonmyungj1zz@gmail.com>
2025-02-14 16:49:16 +0900

    feat: 자동차 이름 입력값 빈칸 제거

--- src/main/kotlin/racingCar/InputReader.kt
+++ src/main/kotlin/racingCar/InputReader.kt
@@ -2,7 +2,7 @@ package racingCar
 
 class InputReader {
     fun readRaceCars(): List<Car> {
-        val carNames = readln().split(RACE_CAR_NAME_DIVIDER)
+        val carNames = readln().split(RACE_CAR_NAME_DIVIDER).map { it.trim() }
         require(carNames.size == carNames.toSet().size) { "[ERROR] 자동차 이름은 중복될 수 없습니다." }
         require(carNames.size >= MIN_RACE_CAR_COUNT) { "[ERROR] 자동차가 ${MIN_RACE_CAR_COUNT}대 이상일 경우에만 경기가 시작됩니다." }
         return carNames.map { name ->
@@ -11,7 +11,7 @@ class InputReader {
     }
 
     fun readRaceCount(): Int {
-        val count = readln().toInt()
+        val count = readln().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 자동차 경주 횟수는 ")
         require(count >= MIN_RACE_COUNT) { "[ERROR] 자동차 경주는 ${MIN_RACE_COUNT}회 이상 이루어져야 합니다." }
         return count
     }
