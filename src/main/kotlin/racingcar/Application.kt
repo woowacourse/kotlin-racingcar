2a6c2869deb555c94a6b58c4dc950586405c1d77
moondev03 <moondev03@gmail.com>
2025-02-14 15:16:03 +0900

    refactor: InputView & OutputView Object로 변경

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,15 +1,7 @@
 package racingcar
 
 import racingcar.controller.RacingController
-import racingcar.view.InputView
-import racingcar.view.OutputView
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-
-    RacingController(
-        inputView = inputView,
-        outputView = outputView,
-    ).run()
+    RacingController().run()
 }
