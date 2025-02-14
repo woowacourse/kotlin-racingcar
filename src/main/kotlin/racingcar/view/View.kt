36d38306036b0463af01166f10dcf4b68e75f08a
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-14 16:40:47 +0900

    refactor: make ErrorMessage, View

---
+++ src/main/kotlin/racingcar/view/View.kt
@@ -0,0 +1,48 @@
+package racingcar.view
+
+import racingcar.Car
+import racingcar.util.ErrorMessage
+
+class View {
+    fun readCars(): List<Car> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+
+        runCatching {
+            val userInput: String =
+                readlnOrNull() ?: throw IllegalArgumentException(ErrorMessage.FAIL_TO_READ_INPUT)
+            Car.createCars(userInput)
+        }.onSuccess { cars: List<Car> ->
+            return cars
+        }.onFailure { error: Throwable ->
+            println(error.message)
+            return readCars()
+        }
+
+        throw IllegalStateException(ErrorMessage.RUNCATCHING_NO_RESULT)
+    }
+
+    fun readRound(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+
+        runCatching {
+            val userInput: String =
+                readlnOrNull() ?: throw IllegalStateException(ErrorMessage.FAIL_TO_READ_INPUT)
+            parseToRound(userInput)
+        }.onSuccess { round: Int ->
+            return round
+        }.onFailure { error: Throwable ->
+            println(error.message)
+            return readRound()
+        }
+
+        throw IllegalStateException(ErrorMessage.RUNCATCHING_NO_RESULT)
+    }
+
+    fun parseToRound(value: String): Int {
+        require(value.toIntOrNull() != null && value.toInt() > 0) {
+            ErrorMessage.INVALID_ROUND
+        }
+
+        return value.toInt()
+    }
+}
--- src/test/kotlin/test/RoundTest.kt
+++ src/test/kotlin/test/RoundTest.kt
@@ -2,13 +2,14 @@ package test
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import racingcar.parseToRound
+import racingcar.view.View
 
 class RoundTest {
     @Test
     fun `시도횟수는 1 이상의 숫자여야 한다`() {
-        Assertions.assertThatThrownBy({ parseToRound("0") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
-        Assertions.assertThatThrownBy({ parseToRound("-1") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
-        Assertions.assertThatThrownBy({ parseToRound("One") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
+        val view = View()
+        Assertions.assertThatThrownBy({ view.parseToRound("0") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
+        Assertions.assertThatThrownBy({ view.parseToRound("-1") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
+        Assertions.assertThatThrownBy({ view.parseToRound("One") }).hasMessage("시도할 횟수는 1 이상의 자연수여야 합니다. 다시 입력해주세요.")
     }
 }
