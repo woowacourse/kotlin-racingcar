32353ff35a83f7382ce86e020f20b48c4d8c03e3
ijh1298 <imjh0417@gmail.com>
2025-02-14 22:49:19 +0900

    refactor: RandomGenerator 난수 생성 메소드 수정

--- src/main/kotlin/utils/RandomGenerator.kt
+++ src/main/kotlin/utils/RandomGenerator.kt
@@ -1,13 +1,10 @@
 package utils
 
-import kotlin.random.Random
-import kotlin.random.nextInt
-
 object RandomGenerator {
-    fun generateRandomNumber(): Int {
-        return Random.nextInt(MIN_RANGE..MAX_RANGE)
+    fun getRandomNumber(
+        start: Int,
+        end: Int,
+    ): Int {
+        return (start..end).random()
     }
-
-    private const val MIN_RANGE = 0
-    private const val MAX_RANGE = 9
 }
