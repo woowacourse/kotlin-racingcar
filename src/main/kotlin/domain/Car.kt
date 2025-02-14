09f383f21f08349f297ad62e5ea402520348cc78
ijh1298 <imjh0417@gmail.com>
2025-02-14 17:11:42 +0900

    refactor: Car 클래스 move 메소드 이동 조건 제거

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -7,14 +7,11 @@ class Car(
     var position: Int = position
         private set
 
-    fun move(randomNum: Int) {
-        if (randomNum >= STANDARD_NUMBER) {
-            ++position
-        }
+    fun move() {
+        ++position
     }
 
     companion object {
         const val INIT_POSITION = 0
-        const val STANDARD_NUMBER = 4
     }
 }
