87a4d874927333fd876399b6c2b2eeb7760e9052
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-15 07:27:03 +0900

    test(CarTest)): 자동차가 4이상일 경우 위치를 상승시키는 테스트로 변경

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,16 +1,23 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     @ParameterizedTest
-    @ValueSource(ints = [4, 6, 8, 10, 21])
-    fun `자동차 이동 테스트`(tryNumber: Int) {
-        val car = Car("123")
-        car.move(tryNumber)
-        val actual = 1
-        assertThat(actual).isEqualTo(car.position)
+    @MethodSource("getMoveNumbers")
+    fun `자동차가 이동을 시도하는 숫자가 4이상일 경우에 위치를 상승시키는 테스트`(
+        tryNumbers: List<Int>,
+        expect: Int,
+    ) {
+        val car = Car("동전")
+        val numbersExtractor = NumbersExtractor(tryNumbers)
+        repeat(numbersExtractor.currentNumbersSize) {
+            car.move(numbersExtractor)
+        }
+        assertThat(expect).isEqualTo(car.position)
     }
 
     @ParameterizedTest
@@ -18,6 +25,17 @@ class CarTest {
     fun `자동차 이름 테스트`(name: String) {
         assertThrows<IllegalArgumentException> {
             Car(name)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun getMoveNumbers(): List<Arguments> {
+            return listOf(
+                Arguments.arguments(listOf(1, 2, 3, 4, 5, 6), 3),
+                Arguments.arguments(listOf(0, 2, 8, 9, 1), 2),
+                Arguments.arguments(listOf(0, 19, 22, 33, 82, 1), 4),
+            )
         }
     }
 }
