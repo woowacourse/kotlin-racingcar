c4a3256507662b5f2460ff791fd6400bc9294cc9
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-14 17:30:09 +0900

    chore: View의 인터페이스  최소화를 반영

--- src/main/kotlin/racingcar/Main.kt
+++ src/main/kotlin/racingcar/Main.kt
@@ -4,9 +4,5 @@ import racingcar.view.View
 
 fun main() {
     val view = View()
-
-    val cars: List<Car> = view.readCars()
-    val round: Int = view.readRound()
-
-    view.showResult(cars, round)
+    view.playRacingGame()
 }
