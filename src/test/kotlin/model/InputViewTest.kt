6096a00a4c80a54ad7941140846705830d274f7f
moondev03 <moondev03@gmail.com>
2025-02-14 17:20:19 +0900

    feat: 자동차 이름 & 경주 횟수 입력 검증 테스트 추가
    - setConsoleInput() 호출 위치 수정

--- src/test/kotlin/model/InputViewTest.kt
+++ src/test/kotlin/model/InputViewTest.kt
@@ -1,5 +1,6 @@
 package model
 
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -8,20 +9,39 @@ import java.io.ByteArrayInputStream
 
 class InputViewTest {
     @ParameterizedTest
-    @ValueSource(strings = ["", " ", "car12345", "1", "car, car", "car1, car12345"])
-    fun `입력 값으로부터 자동차 이름 검증 예외 테스트`(input: String) {
-        assertThrows<IllegalArgumentException> {
+    @ValueSource(strings = ["car1, car2", "car1, car2, car3", "car1, car2,", ",car1,car2"])
+    fun `자동차 이름 입력 검증 테스트`(input: String) {
+        println(input.split(",").size)
+        setConsoleInput(input)
+        assertDoesNotThrow {
             InputView.inputCarNames()
-            setConsoleInput(input)
         }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["", " ", "0", "-1"])
-    fun `입력 값으로부터 경주 실행 횟수 검증 예외 테스트`(input: String) {
+    @ValueSource(strings = ["", " ", ",", ",,", " , , "])
+    fun `자동차 이름 입력 검증 예외 테스트`(input: String) {
+        setConsoleInput(input)
+        assertThrows<IllegalArgumentException> {
+            InputView.inputCarNames()
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3", "4"])
+    fun `경주 실행 횟수 입력 검증 테스트`(input: String) {
+        setConsoleInput(input)
+        assertDoesNotThrow {
+            InputView.inputRacingCount()
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "0", "-1", "1.1", "1,2"])
+    fun `경주 실행 횟수 입력 검증 예외 테스트`(input: String) {
+        setConsoleInput(input)
         assertThrows<IllegalArgumentException> {
             InputView.inputRacingCount()
-            setConsoleInput(input)
         }
     }
 
