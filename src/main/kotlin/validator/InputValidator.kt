e9f5dcd24344fad44094c98a7b7df2f480ec534f
ijh1298 <imjh0417@gmail.com>
2025-02-14 22:50:00 +0900

    refactor: InputValidator 사용하지 않는 메소드 제거

--- src/main/kotlin/validator/InputValidator.kt
+++ src/main/kotlin/validator/InputValidator.kt
@@ -1,15 +1,6 @@
 package validator
 
 object InputValidator {
-    fun validateCarName(splitCarName: String) {
-        require(splitCarName.length <= 5) { "각 자동차 이름은 5자를 초과할 수 없습니다." }
-        require(splitCarName.isNotBlank()) { "각 자동차 이름은 공백이 불가합니다." }
-    }
-
-    fun validateDuplicatedName(splitCarNames: List<String>) {
-        require(splitCarNames.size == splitCarNames.toSet().size) { "자동차 이름은 중복될 수 없습니다." }
-    }
-
     fun validateTryNumber(inputTryNumber: String): Int {
         require(inputTryNumber.all { it.isDigit() }) { "시도 횟수는 숫자로 입력해주세요." }
         require(inputTryNumber.toInt() > 0) { "시도 횟수는 0보다 커야 합니다." }
