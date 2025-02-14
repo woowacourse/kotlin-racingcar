88b36898b2f4fb918d07e6df95daf46d3816f748
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-14 16:46:08 +0900

    refactor: Main의 입출력 작업 모두 View 클래스로 옮김

--- src/main/kotlin/racingcar/Main.kt
+++ src/main/kotlin/racingcar/Main.kt
@@ -4,9 +4,9 @@ import racingcar.view.View
 
 fun main() {
     val view = View()
+
     val cars: List<Car> = view.readCars()
     val round: Int = view.readRound()
-    println("\n실행결과")
-    val racecourse = Racecourse(cars, round)
-    racecourse.startRace()
+
+    view.showResult(cars, round)
 }
