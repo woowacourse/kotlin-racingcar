c844434b246963ed740ffc6cfddda470b9242405
chanho0908 <uni070@naver.com>
2025-02-14 20:26:41 +0900

    test : 게임 진행 횟수 유효성 검사 테스트

---
+++ src/test/kotlin/racingcar/RoundsTest.kt
@@ -0,0 +1,34 @@
+package racingcar
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import racingcar.model.Rounds
+
+class RoundsTest {
+    @Test
+    fun `빈값이 전달되면 에러가 발생한다`() {
+        Assertions.assertThatThrownBy {
+            Rounds("")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 빈 값을 입력하셨습니다.")
+    }
+
+    @Test
+    fun `시도 횟수 입력값이 음수일 때 예외 테스트`() {
+        Assertions.assertThatThrownBy {
+            Rounds("-1")
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 1에서 10 사이의 수를 입력해야 합니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["가나다, abc, ###, 2147483648"])
+    fun `시도 횟수 입력값이 숫자가 아닐 때 테스트`(value: String) {
+        Assertions.assertThatThrownBy {
+            Rounds(value)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 숫자를 입력해야 합니다.")
+    }
+}
