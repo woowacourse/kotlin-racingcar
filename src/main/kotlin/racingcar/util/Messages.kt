a16d73c37924610f6bd4f2ab6e063fad1927716c
HamBeomJoon <hbj0802@naver.com>
2025-02-14 15:59:19 +0900

    fix: 유효하지 않은 자동차 이름 메세지 수정

--- src/main/kotlin/racingcar/util/Messages.kt
+++ src/main/kotlin/racingcar/util/Messages.kt
@@ -6,7 +6,7 @@ object Messages {
     const val MESSAGE_OUTPUT_RACE_RESULT = "실행 결과"
     const val MESSAGE_OUTPUT_WINNER = "최종 우승자: "
 
-    const val MESSAGE_INVALID_CAR_NAME = "유효하지 않은 자동차 이름입니다."
+    const val MESSAGE_INVALID_CAR_NAME = "자동차 이름은 1글자 이상 5글자 이하여야 합니다."
     const val MESSAGE_DUPLICATE_CAR_NAME = "자동차 이름에 중복이 있습니다."
     const val MESSAGE_RACE_COUNT_NOT_INTEGER = "경주 횟수가 정수가 아닙니다."
     const val MESSAGE_RACE_COUNT_TOO_SMALL = "경주 횟수가 1 이상이어야 합니다."
