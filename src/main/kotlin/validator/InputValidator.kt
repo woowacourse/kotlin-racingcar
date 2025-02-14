d20f0aab6c774db456852d70f052a0337ec0186b
DongjooSEO <readyjoo24@gmail.com>
2025-02-14 16:14:08 +0900

    refactor: 입력 값 유효성 검사 시에, 잘못 입력된 값이 출력되도록 수정

--- src/main/kotlin/validator/InputValidator.kt
+++ src/main/kotlin/validator/InputValidator.kt
@@ -2,8 +2,8 @@ package validator
 
 object InputValidator {
     fun validateCarName(splitCarName: String) {
-        require(splitCarName.length <= 5) { "각 자동차 이름은 5자를 초과할 수 없습니다." }
-        require(splitCarName.isNotBlank()) { "각 자동차 이름은 공백이 불가합니다." }
+        require(splitCarName.length <= 5) { "잘못 입력된 자동차 이름 -> $splitCarName : 자동차 이름은 5자를 초과할 수 없습니다." }
+        require(splitCarName.isNotBlank()) { "잘못 입력된 자동차 이름 -> $splitCarName : 자동차 이름은 공백이 불가합니다." }
     }
 
     fun validateDuplicatedName(splitCarNames: List<String>) {
@@ -11,8 +11,8 @@ object InputValidator {
     }
 
     fun validateTryNumber(inputTryNumber: String): Int {
-        require(inputTryNumber.all { it.isDigit() }) { "시도 횟수는 숫자로 입력해주세요." }
-        require(inputTryNumber.toInt() > 0) { "시도 횟수는 0보다 커야 합니다." }
+        require(inputTryNumber.all { it.isDigit() }) { "잘못 입력된 시도 횟수 -> $inputTryNumber : 시도 횟수는 숫자로 입력해주세요." }
+        require(inputTryNumber.toInt() > 0) { "잘못 입력된 시도 횟수 -> $inputTryNumber : 시도 횟수는 0보다 커야 합니다." }
         return inputTryNumber.toInt()
     }
 }
