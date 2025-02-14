851c57849c58ab258fdf864b7910a33808c2e4d0
이지윤 <02leejiyun@naver.com>
2025-02-14 15:17:50 +0900

    refactor: 자동차 객체에서 이름 유효성 검사

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -6,10 +6,34 @@ class Car(val name: String) {
     var position: Int = 0
         private set
 
+    init {
+        validateNameLength(name)
+        validateNameFormat(name)
+    }
+
     fun move(moveState: Move) {
         when (moveState) {
             Move.MOVABLE -> position++
             Move.UNMOVABLE -> return
         }
+    }
+
+    private fun validateNameLength(name: String) {
+        require(name.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH) { INVALID_NAME_LENGTH_ERROR }
+    }
+
+    private fun validateNameFormat(name: String) {
+        require(name.matches(regex)) { INVALID_NAME_FORMAT_ERROR }
+    }
+
+    companion object {
+        private const val ERROR = "[ERROR]"
+        const val INVALID_NAME_LENGTH_ERROR = "$ERROR 자동차 이름은 1~5자이어야 합니다."
+        const val INVALID_NAME_FORMAT_ERROR = "$ERROR 자동차 이름은 영문 또는 숫자로 이루어져야 합니다."
+
+        const val MIN_NAME_LENGTH = 1
+        const val MAX_NAME_LENGTH = 5
+
+        private val regex = Regex("^[a-zA-Z0-9]*\$")
     }
 }
--- src/main/kotlin/racingcar/validator/CarNamesValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNamesValidator.kt
@@ -4,22 +4,12 @@ class CarNamesValidator {
     fun validate(input: String): List<String> {
         isNotEmpty(input)
         val carNames = input.split(COMMA).map { it.trim() }
-        isValidLength(carNames)
-        isValidFormat(carNames)
         isUniqueName(carNames)
         return carNames
     }
 
     private fun isNotEmpty(carName: String) {
         require(carName.isNotBlank()) { EMPTY_INPUT_ERROR }
-    }
-
-    private fun isValidLength(carNames: List<String>) {
-        require(carNames.all { it.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH }) { INVALID_NAME_LENGTH_ERROR }
-    }
-
-    private fun isValidFormat(carNames: List<String>) {
-        require(carNames.all { it.matches(regex) }) { INVALID_NAME_FORMAT_ERROR }
     }
 
     private fun isUniqueName(carNames: List<String>) {
@@ -29,14 +19,8 @@ class CarNamesValidator {
     companion object {
         private const val ERROR = "[ERROR]"
         const val EMPTY_INPUT_ERROR = "$ERROR 빈 값을 입력하셨습니다."
-        const val INVALID_NAME_LENGTH_ERROR = "$ERROR 자동차 이름은 1~5자이어야 합니다."
-        const val INVALID_NAME_FORMAT_ERROR = "$ERROR 자동차 이름은 영문 또는 숫자로 이루어져야 합니다."
         const val NOT_UNIQUE_NAME_ERROR = "$ERROR 자동차 이름이 중복됩니다."
 
         const val COMMA = ","
-        const val MIN_NAME_LENGTH = 1
-        const val MAX_NAME_LENGTH = 5
-
-        private val regex = Regex("^[a-zA-Z0-9]*\$")
     }
 }
