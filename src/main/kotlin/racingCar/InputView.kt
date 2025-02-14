8d40e1ca3cc56a4c54925bde9d43b517ec190d45
m6z1 <sonmyungj1zz@gmail.com>
2025-02-14 20:45:48 +0900

    feat: 경주 횟수 유효성 로직 추가

--- src/main/kotlin/racingCar/InputView.kt
+++ src/main/kotlin/racingCar/InputView.kt
@@ -11,7 +11,7 @@ class InputView {
     }
 
     fun readRaceCount(): Int {
-        val count = readln().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 자동차 경주 횟수는 ")
+        val count = readln().trim().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 자동차 경주 횟수는 숫자로 입력해야 합니다.")
         require(count >= MIN_RACE_COUNT) { "[ERROR] 자동차 경주는 ${MIN_RACE_COUNT}회 이상 이루어져야 합니다." }
         return count
     }
