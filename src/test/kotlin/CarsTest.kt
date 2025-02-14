bd730aab4cead06dcfe1c6d50423b97af5dd8faa
yrsel <xxknu88@gmail.com>
2025-02-15 02:29:12 +0900

    refactor: 우승자 테스트하기 위해 테스트용 숫자 생성기 추가 구현

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -1,12 +1,14 @@
 import domain.cars.Cars
+import domain.numbergenerator.NumberGenerator
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.collections.ArrayDeque
 
 class CarsTest {
     @Test
     fun `우승자를 확인할 수 있다 `() {
         val cars = getDummy()
-        val expected = "포비, 크론, 디노"
+        val expected = "디노"
 
         assertEquals(expected, cars.getWinners())
     }
@@ -14,6 +16,20 @@ class CarsTest {
 
 fun getDummy(): Cars {
     val cars = Cars("포비,크론,디노")
-    cars.lap(TestMoveNumberGenerator())
+    cars.lap(TestWinnerNumberGenerator())
     return cars
+}
+
+private class TestWinnerNumberGenerator : NumberGenerator {
+    private val queue = ArrayDeque<Int>()
+
+    init {
+        queue.add(1)
+        queue.add(2)
+        queue.add(7)
+    }
+
+    override fun generate(): Int {
+        return queue.removeFirstOrNull() ?: 0
+    }
 }
