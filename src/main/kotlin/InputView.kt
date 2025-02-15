db100bf877728e164afe4d6a7427cae0135435d4
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-16 00:04:39 +0900

    feat(InputView): 자동차 이름이 5자 이상일 경우 예외처리 추가

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -5,6 +5,10 @@ class InputView {
 
     fun readCarsName(): List<String> {
         println(INPUT_CAR_NAME)
+        val carNames = scanner.nextLine().split(CAR_DELIMITER)
+        carNames.forEach { carName ->
+            require(carName.length <= CAR_NAME_LENGTH) { CAR_NAME_ERROR }
+        }
         return scanner.nextLine().split(CAR_DELIMITER)
     }
 
@@ -14,9 +18,11 @@ class InputView {
     }
 
     companion object {
+        private const val CAR_NAME_ERROR = "[ERROR]: 자동차 이름은 5자를 내외입니다."
         private const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         private const val INPUT_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
         private const val ATTEMPT_INPUT_ERROR = "[ERROR]: 시도 횟수를 입력해주세요."
+        private const val CAR_NAME_LENGTH = 5
         private const val CAR_DELIMITER = ","
     }
 }
