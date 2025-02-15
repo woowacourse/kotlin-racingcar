4d7d5b9f73e7546533a7b38d4b224f0d7c59a05e
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-15 20:00:11 +0900

    refactor: getDummy 함수 제거

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -1,19 +1,14 @@
 import domain.cars.Cars
+import domain.numbergenerator.TestNumberGenerator
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CarsTest {
     @Test
     fun `우승자를 확인할 수 있다 `() {
-        val cars = getDummy()
-        val expected = "포비, 크론, 디노"
-
+        val cars = Cars("포비,크론,디노")
+        cars.moveAll(TestNumberGenerator(mutableListOf(1, 2, 5))) // 우승자는 디노
+        val expected = "디노"
         Assertions.assertEquals(expected, cars.getWinners())
     }
-}
-
-fun getDummy(): Cars {
-    val cars = Cars("포비,크론,디노")
-    cars.lap(TestMoveNumberGenerator())
-    return cars
 }
