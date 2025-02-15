351b91b9c2ae265e7a4c7c5dafbdb5f0a128bb0e
ijh1298 <imjh0417@gmail.com>
2025-02-15 14:54:48 +0900

    refactor: View와 RacingController 분리, main 함수에서 View, Controller 호출하여 인자를 넘기도록 수정

--- src/main/kotlin/controller/RacingController.kt
+++ src/main/kotlin/controller/RacingController.kt
@@ -2,42 +2,15 @@ package controller
 
 import domain.Car
 import utils.MoveConditionGenerator.getMoveConditionByNumber
-import validator.InputValidator
-import view.InputView
-import view.OutputView
 
-class RacingController(
-    private val inputView: InputView,
-    private val outputView: OutputView,
-) {
-    fun run() {
-        val carNames: List<String> = getCarNames()
-        val cars: List<Car> = carNames.map { Car(it) }
-        val tryNumber: Int = getTryNumber()
-
-        outputView.printStatus()
-        repeat(tryNumber) {
-            playRace(cars)
-            outputView.printCars(cars)
-        }
-        outputView.printResult(getWinners(cars))
-    }
-
-    private fun getCarNames(): List<String> {
-        return inputView.inputCarNames().split(DELIMITER).map { it.trim() }
-    }
-
-    private fun getTryNumber(): Int {
-        return InputValidator.validateTryNumber(inputView.inputTryNumber())
-    }
-
-    private fun playRace(cars: List<Car>) {
+class RacingController() {
+    fun playRace(cars: List<Car>) {
         for (car in cars) {
             car.moveOrStop(getMoveConditionByNumber((MIN_RANGE..MAX_RANGE).random()))
         }
     }
 
-    private fun getWinners(cars: List<Car>): List<String> {
+    fun getWinners(cars: List<Car>): List<String> {
         return cars.filter { car -> car.position == cars.maxOf { it.position } }.map { it.name }
     }
 
