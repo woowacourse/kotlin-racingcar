1fdecbcce348b2f50de17f499cba8b0c5848d341
cucumber99 <dlsguq1@gmail.com>
2025-02-15 16:37:52 +0900

    refactor: 입력 뷰 수정
    입력 유효성을 검증합니다.

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,7 +2,14 @@ package view
 
 class InputView {
     fun getUserInput(): String {
-        val input = readln().trim()
+        val input = readln()
+        validateInput(input)
         return input
+    }
+
+    private fun validateInput(input: String) {
+        require(input.trim().isNotEmpty()) {
+            "입력이 존재하지 않습니다."
+        }
     }
 }
