8e9e1e12b24facfcbd36a081fd6ade53bf7457af
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-15 16:22:56 +0900

    test(Car): 함수명 세부표현으로 수정

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -10,17 +10,17 @@ class CarTest {
     @MethodSource("getMoveNumbers")
     fun `자동차가 이동을 시도하는 숫자가 4이상일 경우에 위치를 상승시키는 테스트`(
         tryNumbers: List<Int>,
-        expect: Int,
+        actual: Int,
     ) {
         val car = Car("동전")
         val numbersExtractor = NumbersExtractor(tryNumbers)
         repeat(numbersExtractor.currentNumbersSize) { car.move(numbersExtractor) }
-        assertThat(expect).isEqualTo(car.position)
+        assertThat(actual).isEqualTo(car.position)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["abcdef", "익명123", "12345"])
-    fun `자동차 이름 테스트`(name: String) {
+    fun `자동차 이름이 5자 초과일 경우 예외 발생 테스트`(name: String) {
         assertThrows<IllegalArgumentException> {
             Car(name)
         }
