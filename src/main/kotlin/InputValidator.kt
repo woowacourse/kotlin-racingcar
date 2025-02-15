2f433ac76f10ec1bd7a74028edcaa51162ab77cd
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-15 17:02:42 +0900

    refactor: 함수명 변경 - validateCarName -> validateName

--- src/main/kotlin/InputValidator.kt
+++ src/main/kotlin/InputValidator.kt
@@ -1,26 +1,27 @@
 import domain.cars.Cars.Companion.CAR_NAME_DELIMITER
 
 class InputValidator {
-    fun validateCarName(input: String) {
+    fun validateName(input: String) {
         require(input.isNotBlank()) { ERROR_EMPTY_CAR_NAME_MESSAGE }
         val carNames = input.split(CAR_NAME_DELIMITER)
         carNames.forEach {
-            validateCarNameLength(it)
+            validateNameLength(it)
         }
-        require(validateDuplicatedCarName(carNames)) { ERROR_DUPLICATED_CAR_NAME_MESSAGE }
+        require(validateDuplicatedName(carNames)) { ERROR_DUPLICATED_CAR_NAME_MESSAGE }
     }
 
-    private fun validateCarNameLength(input: String) {
+    private fun validateNameLength(input: String) {
         require(input.length in TRY_COUNT_LOWER_BOUND..TRY_COUNT_UPPER_BOUND) { ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE }
     }
 
-    private fun validateDuplicatedCarName(input: List<String>): Boolean {
+    private fun validateDuplicatedName(input: List<String>): Boolean {
         return input.distinct().size == input.size
     }
 
     fun validateTryCount(input: String) {
         require(input.isNotBlank()) { ERROR_EMPTY_TRY_COUNT_MESSAGE }
         require(input.toIntOrNull() != null) { ERROR_IS_NOT_NUMBER_MESSAGE }
+        require(input.toIntOrNull() in TRY_COUNT_LOWER_BOUND..TRY_COUNT_UPPER_BOUND) { ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE }
     }
 
     companion object {
@@ -30,6 +31,7 @@ class InputValidator {
         const val ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE = "$ERROR 자동차 이름의 최대 길이를 초과했습니다."
         const val ERROR_EMPTY_TRY_COUNT_MESSAGE = "$ERROR 시도횟수는 공백일 수 없습니다."
         const val ERROR_IS_NOT_NUMBER_MESSAGE = "$ERROR 시도횟수는 숫자여야만 합니다."
+        const val ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE = "$ERROR 시도횟수는 1에서 1,000 사이의 숫자여야 합니다."
 
         const val TRY_COUNT_LOWER_BOUND = 1
         const val TRY_COUNT_UPPER_BOUND = 5
--- src/test/kotlin/InputValidatorTest.kt
+++ src/test/kotlin/InputValidatorTest.kt
@@ -4,46 +4,58 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class InputValidatorTest {
+    private val inputValidator = InputValidator()
+
     @Test
     fun `자동차의 이름이 공백이면 예외가 발생한다`() {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateCarName(" ")
-        }.withMessage(Constants.ERROR_EMPTY_CAR_NAME_MESSAGE)
+            inputValidator.validateName(" ")
+        }.withMessage(ERROR_EMPTY_CAR_NAME_MESSAGE)
     }
 
     @Test
     fun `자동차의 이름이 중복되면 예외가 발생한다`() {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateCarName("pobi,pobi")
-        }.withMessage(Constants.ERROR_DUPLICATED_CAR_NAME_MESSAGE)
+            inputValidator.validateName("pobi,pobi")
+        }.withMessage(ERROR_DUPLICATED_CAR_NAME_MESSAGE)
     }
 
     @Test
     fun `자동차 이름의 길이가 5자를 넘어가면 예외가 발생한다`() {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateCarName("cronnnnn")
-        }.withMessage(Constants.ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE)
+            inputValidator.validateName("cronnnnn")
+        }.withMessage(ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE)
     }
 
     @Test
     fun `시도 횟수가 공백이면 예외가 발생한다`() {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateTryCount(" ")
-        }.withMessage(Constants.ERROR_EMPTY_TRY_COUNT_MESSAGE)
+            inputValidator.validateTryCount(" ")
+        }.withMessage(ERROR_EMPTY_TRY_COUNT_MESSAGE)
     }
 
     @Test
     fun `시도 횟수가 숫자가 아니면 예외가 발생한다`() {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateTryCount("aa")
-        }.withMessage(Constants.ERROR_IS_NOT_NUMBER_MESSAGE)
+            inputValidator.validateTryCount("aa")
+        }.withMessage(ERROR_IS_NOT_NUMBER_MESSAGE)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["0", "1001"])
     fun `시도 횟수가 1에서 1000사이의 숫자가 아니면 예외가 발생한다`(input: String) {
         Assertions.assertThatIllegalArgumentException().isThrownBy {
-            InputValidator.validateTryCount(input)
-        }.withMessage(Constants.ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE)
+            inputValidator.validateTryCount(input)
+        }.withMessage(ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE)
+    }
+
+    companion object {
+        private const val ERROR = "[ERROR]"
+        const val ERROR_EMPTY_CAR_NAME_MESSAGE = "$ERROR 자동차 이름은 공백일 수 없습니다."
+        const val ERROR_DUPLICATED_CAR_NAME_MESSAGE = "$ERROR 중복된 자동차 이름이 있습니다."
+        const val ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE = "$ERROR 자동차 이름의 최대 길이를 초과했습니다."
+        const val ERROR_EMPTY_TRY_COUNT_MESSAGE = "$ERROR 시도횟수는 공백일 수 없습니다."
+        const val ERROR_IS_NOT_NUMBER_MESSAGE = "$ERROR 시도횟수는 숫자여야만 합니다."
+        const val ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE = "$ERROR 시도횟수는 1에서 1,000 사이의 숫자여야 합니다."
     }
 }
