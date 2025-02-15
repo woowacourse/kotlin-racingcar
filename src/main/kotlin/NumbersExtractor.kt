311aad5ef301960ed0655e7e23cc569866d59989
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-15 16:41:50 +0900

    refactor: 추출 숫자가 비었을 때 출력하는 예외 메시지 상수화

--- src/main/kotlin/NumbersExtractor.kt
+++ src/main/kotlin/NumbersExtractor.kt
@@ -5,7 +5,11 @@ class NumbersExtractor(numbers: List<Int>) : NumberStrategy {
     val currentNumbersSize get() = numbers.size
 
     override fun getNumber(): Int {
-        require(numbers.isNotEmpty()) { "[ERROR] 모든 숫자를 추출 하였습니다" }
+        require(numbers.isNotEmpty()) { EXTRACTOR_EMPTY_ERROR }
         return numbers.poll()
+    }
+
+    companion object {
+        private const val EXTRACTOR_EMPTY_ERROR = "[ERROR] 모든 숫자를 추출 하였습니다 더 이상 추출 할 수 없습니다."
     }
 }
