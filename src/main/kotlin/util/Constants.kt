f5db6d223045f4e50e9810616f19f4f466c3af91
DoableTuple <doabletuple@gmail.com>
2025-02-15 22:14:57 +0900

    refactor: 경주 횟수 검증 관련 상수를 InputView 내부로 이동

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -1,7 +1,6 @@
 package util
 
 object Constants {
-    const val DELIMITER_CAR_INPUT = ","
     const val INDICATOR_CAR_POSITION = "-"
     const val DELIMITER_WINNER_OUTPUT = ", "
 }
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,28 +1,27 @@
 package view
 
-import util.Constants
-
 object InputView {
     fun readCarNames(): List<String> {
-        val carNamesInput = readLine(MESSAGE_ENTER_CAR_NAMES)
-        val carNames = carNamesInput.split(Constants.DELIMITER_CAR_INPUT).map { it.trim() }
+        val carNamesInput = getInput(MESSAGE_ENTER_CAR_NAMES)
+        val carNames = carNamesInput.split(DELIMITER_CAR_INPUT).map { it.trim() }
         return carNames
     }
 
     fun readRounds(): Int {
-        val countInput = readLine(MESSAGE_ENTER_ROUNDS_NUMBER)
+        val countInput = getInput(MESSAGE_ENTER_ROUNDS_NUMBER)
         val count = countInput.toIntOrNull()
         require(count != null) { MESSAGE_ROUNDS_NOT_INTEGER }
         return count
     }
 
-    private fun readLine(prompt: String): String {
+    private fun getInput(prompt: String): String {
         println(prompt)
         return readlnOrNull() ?: ""
     }
 
+    private const val DELIMITER_CAR_INPUT = ","
+
     private const val MESSAGE_ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     private const val MESSAGE_ENTER_ROUNDS_NUMBER = "시도할 횟수는 몇 회인가요?"
-
     private const val MESSAGE_ROUNDS_NOT_INTEGER = "입력한 경주 횟수는 정수가 아닙니다."
 }
