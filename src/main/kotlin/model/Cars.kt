160fc34face6db5f8419c008de3db76589bb4219
cucumber99 <dlsguq1@gmail.com>
2025-02-15 16:36:58 +0900

    feat: Cars 클래스 수정
    라운드 진행과 우승자 산출을 수행합니다.

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -1,21 +1,30 @@
 package model
 
-class Cars(private val input: String) {
-    lateinit var parsedCars: List<Car>
+class Cars(
+    private val carNames: List<String>,
+    private val numberGenerator: NumberGenerator = RandomNumberGenerator(),
+) {
+    val parsedCars: List<Car> = carNames.map { Car(it) }
 
     init {
         validateCars()
-        parseCars()
+    }
+
+    fun playOneRound() {
+        parsedCars.forEach { car ->
+            car.moveForward(numberGenerator.generate())
+        }
+    }
+
+    fun getWinner(): List<String> {
+        val maxPosition = parsedCars.maxOf { it.position }
+        val winners = parsedCars.filter { it.position == maxPosition }.map { it.name }
+
+        return winners
     }
 
     private fun validateCars() {
-        require(input.isNotEmpty()) { "자동차 이름을 입력해주세요." }
-        val cars = input.split(",")
-        require(cars.toSet().size == cars.size) { "자동차 이름 중 중복이 존재합니다." }
-        require(cars.size > 1) { "자동차가 2대 이상일 때 경주가 가능합니다." }
-    }
-
-    private fun parseCars() {
-        parsedCars = input.split(",").map { Car(it) }
+        require(carNames.distinct().size == carNames.size) { "자동차 이름 중 중복이 존재합니다." }
+        require(carNames.size > 1) { "자동차가 2대 미만 입력되었습니다." }
     }
 }
