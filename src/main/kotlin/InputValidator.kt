cbb6a532d49b0429c2147b723479f91430ea0dca
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-15 16:52:20 +0900

    refactor: 함수명 및 상수 위치 변경

--- src/main/kotlin/InputValidator.kt
+++ src/main/kotlin/InputValidator.kt
@@ -1,15 +1,6 @@
-import Constants.CAR_NAME_DELIMITER
-import Constants.CAR_NAME_LOWER_BOUND
-import Constants.CAR_NAME_UPPER_BOUND
-import Constants.ERROR_DUPLICATED_CAR_NAME_MESSAGE
-import Constants.ERROR_EMPTY_CAR_NAME_MESSAGE
-import Constants.ERROR_EMPTY_TRY_COUNT_MESSAGE
-import Constants.ERROR_IS_NOT_NUMBER_MESSAGE
-import Constants.ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE
-import Constants.TRY_COUNT_LOWER_BOUND
-import Constants.TRY_COUNT_UPPER_BOUND
+import domain.cars.Cars.Companion.CAR_NAME_DELIMITER
 
-object InputValidator {
+class InputValidator {
     fun validateCarName(input: String) {
         require(input.isNotBlank()) { ERROR_EMPTY_CAR_NAME_MESSAGE }
         val carNames = input.split(CAR_NAME_DELIMITER)
@@ -20,7 +11,7 @@ object InputValidator {
     }
 
     private fun validateCarNameLength(input: String) {
-        require(input.length in CAR_NAME_LOWER_BOUND..CAR_NAME_UPPER_BOUND) { Constants.ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE }
+        require(input.length in TRY_COUNT_LOWER_BOUND..TRY_COUNT_UPPER_BOUND) { ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE }
     }
 
     private fun validateDuplicatedCarName(input: List<String>): Boolean {
@@ -30,6 +21,17 @@ object InputValidator {
     fun validateTryCount(input: String) {
         require(input.isNotBlank()) { ERROR_EMPTY_TRY_COUNT_MESSAGE }
         require(input.toIntOrNull() != null) { ERROR_IS_NOT_NUMBER_MESSAGE }
-        require(input.toIntOrNull() in TRY_COUNT_LOWER_BOUND..TRY_COUNT_UPPER_BOUND) { ERROR_OUT_OF_BOUND_TRY_COUNT_MESSAGE }
+    }
+
+    companion object {
+        private const val ERROR = "[ERROR]"
+        const val ERROR_EMPTY_CAR_NAME_MESSAGE = "$ERROR 자동차 이름은 공백일 수 없습니다."
+        const val ERROR_DUPLICATED_CAR_NAME_MESSAGE = "$ERROR 중복된 자동차 이름이 있습니다."
+        const val ERROR_OUT_OF_BOUND_CAR_NAME_MESSAGE = "$ERROR 자동차 이름의 최대 길이를 초과했습니다."
+        const val ERROR_EMPTY_TRY_COUNT_MESSAGE = "$ERROR 시도횟수는 공백일 수 없습니다."
+        const val ERROR_IS_NOT_NUMBER_MESSAGE = "$ERROR 시도횟수는 숫자여야만 합니다."
+
+        const val TRY_COUNT_LOWER_BOUND = 1
+        const val TRY_COUNT_UPPER_BOUND = 5
     }
 }
