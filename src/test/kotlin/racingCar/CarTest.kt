84909ed59fd04ad68cfa2f566de53fbf2a0a873b
m6z1 <sonmyungj1zz@gmail.com>
2025-02-16 01:34:00 +0900

    test: 중복된 테스트 변수를 전역 변수로 선언

--- src/test/kotlin/racingCar/CarTest.kt
+++ src/test/kotlin/racingCar/CarTest.kt
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CarTest {
+    private val car = Car("뭉치")
+
     @Test
     fun `자동차의 초기 위치는 0이다`() {
-        val car = Car("공백")
-        val initialPosition = 0
-        Assertions.assertEquals(initialPosition, car.position)
+        // given:
+        // when:
+        val actual = 0
+        // then:
+        Assertions.assertEquals(actual, car.position)
     }
 
     @Test
     fun `1번 전진한 후 자동차는 이전 위치보다 1 앞서있다`() {
         // given:
-        val car = Car("뭉치")
         val previousPosition = car.position
         // when:
         car.moveForward()
@@ -25,7 +28,6 @@ class CarTest {
     @Test
     fun `전진을 못하면 자동차는 이전 위치와 같다`() {
         // given:
-        val car = Car("뭉치")
         val previousPosition = car.position
         // when:
         // then:
