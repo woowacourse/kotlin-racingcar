1977ff0eb0b1347752bcd8288517b896634cbc9b
cucumber99 <dlsguq1@gmail.com>
2025-02-15 16:29:24 +0900

    refactor: Car 클래스 리팩토링
    자동차 데이터를 직접 반환합니다.

--- src/main/kotlin/model/Car.kt
+++ src/main/kotlin/model/Car.kt
@@ -2,27 +2,37 @@ package model
 
 class Car(
     val name: String,
-    private var _position: Int = INIT_POSITION,
+    position: Int = INIT_POSITION,
 ) {
+    var position: Int = position
+        private set
+
     init {
         validateCar(name)
     }
 
-    val position: Int
-        get() = _position
-
     fun moveForward(number: Int) {
-        if (number >= MINIMUM_MOVE_POINT) _position++
+        if (number >= MINIMUM_MOVE_POINT) position++
+    }
+
+    override fun toString(): String {
+        return buildString {
+            append(name)
+            append(DELIMITER)
+            append(POSITION_SIGN.repeat(position))
+        }
     }
 
     private fun validateCar(name: String) {
-        require(name.trim().isNotEmpty()) { "자동차 이름이 올바르지 않습니다." }
-        require(name.length <= MAX_NAME_LENGTH) { "자동차 이름은 5자를 초과할 수 없습니다." }
+        require(name.trim().isNotEmpty()) { "자동차 이름에 빈 값이 입력되었습니다." }
+        require(name.length <= MAX_NAME_LENGTH) { "자동차 이름이 5자를 초과하였습니다." }
     }
 
     companion object {
         const val MAX_NAME_LENGTH = 5
         const val MINIMUM_MOVE_POINT = 4
         const val INIT_POSITION = 0
+        private const val DELIMITER = " : "
+        private const val POSITION_SIGN = "-"
     }
 }
