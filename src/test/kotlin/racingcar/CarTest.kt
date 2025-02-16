f82f2644239d7d28f4b9d818ff0b93150a64a0e2
이지윤 <02leejiyun@naver.com>
2025-02-17 04:56:30 +0900

    refactor: 테스트 코드 네이밍 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -15,14 +15,14 @@ class CarTest {
     }
 
     @Test
-    fun `전진이 가능하면 자동차의 위치가 1 증가한다`() {
+    fun `자동차가 1번 전진하면 자동차의 위치는 1이다`() {
         val moveState = MoveState.create(5)
         car.move(moveState)
         car.position.shouldBe(1)
     }
 
     @Test
-    fun `전진이 가능하지 않으면 자동차의 위치가 유지된다`() {
+    fun `자동차가 전진하지 않으면 자동차의 위치는 0이다`() {
         val moveState = MoveState.create(3)
         car.move(moveState)
         car.position.shouldBe(0)
