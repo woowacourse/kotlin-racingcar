12cef971c93a97de19c28b45e64d3f2040e74a0a
hwannow <vnfdlvgid061@naver.com>
2025-02-16 14:25:38 +0900

    refactor : Validator 객체 추가

--- src/main/kotlin/controller/RacingViewController.kt
+++ src/main/kotlin/controller/RacingViewController.kt
@@ -1,16 +1,18 @@
 package controller
 
-import constant.ErrorConstant
 import constant.MessageConstant
 import model.Car
+import validator.InputValidator
 import view.InputView
 import view.OutputView
 
 class RacingViewController(private val inputView: InputView, private val outputView: OutputView) {
+    private val validator: InputValidator = InputValidator()
+
     fun getCarName(): List<String> {
         val input = inputView.getUserInput(MessageConstant.INPUT_NAME)
         val carNames = splitToComma(input)
-        validateName(carNames)
+        validator.validateName(carNames)
         return carNames
     }
 
@@ -19,26 +21,16 @@ class RacingViewController(private val inputView: InputView, private val outputV
         return carNames
     }
 
-    private fun validateName(name: List<String>) {
-        if (name.isEmpty()) throw java.lang.IllegalArgumentException(ErrorConstant.ERROR_NO_NAME)
-        if (name.toSet().size != name.size) throw IllegalArgumentException(ErrorConstant.ERROR_SAME_NAME)
-    }
-
     fun getTryCount(): Int {
         val input = inputView.getUserInput(MessageConstant.INPUT_COUNT)
         val count = parseInt(input)
-        validateCount(count)
+        validator.validateCount(count)
         return count!!
     }
 
     private fun parseInt(input: String): Int? {
         val count = input.toIntOrNull()
         return count
-    }
-
-    private fun validateCount(count: Int?) {
-        if (count == null) throw IllegalArgumentException(ErrorConstant.ERROR_UNDER_ZERO)
-        if (count <= 0) throw IllegalArgumentException(ErrorConstant.ERROR_UNDER_ZERO)
     }
 
     fun printCurrentPosition(cars: List<Car>) {
---
+++ src/main/kotlin/validator/InputValidator.kt
@@ -0,0 +1,15 @@
+package validator
+
+import constant.ErrorConstant
+
+class InputValidator {
+    fun validateName(name: List<String>) {
+        if (name.isEmpty()) throw IllegalArgumentException(ErrorConstant.ERROR_NO_NAME)
+        if (name.toSet().size != name.size) throw IllegalArgumentException(ErrorConstant.ERROR_SAME_NAME)
+    }
+
+    fun validateCount(count: Int?) {
+        if (count == null) throw IllegalArgumentException(ErrorConstant.ERROR_NOT_NUMBER)
+        if (count <= 0) throw IllegalArgumentException(ErrorConstant.ERROR_UNDER_ZERO)
+    }
+}
