a61fe2564d5c6606d4060092497ea9067ab4162c
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-16 20:28:51 +0900

    refactor(Car): Car 클래스에서 검증 단계 삭제
    - Model에서 검증이 아닌 Controller 단계에서 검증
    - 단일 책임의 원칙에 따라 검증 단계는 분리

--- src/main/kotlin/model/Car.kt
+++ src/main/kotlin/model/Car.kt
@@ -1,27 +1,16 @@
 package model
 
-class Car(
+data class Car(
     val name: String,
-    private var _position: Int = INIT_POSITION,
 ) {
-    init {
-        validateCar(name)
-    }
-
-    val position: Int
-        get() = _position
+    var position: Int = INIT_POSITION
+        private set
 
     fun moveForward(number: Int) {
-        if (number >= MINIMUM_MOVE_POINT) _position++
-    }
-
-    private fun validateCar(name: String) {
-        require(name.trim().isNotEmpty()) { "자동차 이름이 올바르지 않습니다." }
-        require(name.length <= MAX_NAME_LENGTH) { "자동차 이름은 5자를 초과할 수 없습니다." }
+        if (number >= MINIMUM_MOVE_POINT) position++
     }
 
     companion object {
-        const val MAX_NAME_LENGTH = 5
         const val MINIMUM_MOVE_POINT = 4
         const val INIT_POSITION = 0
     }
