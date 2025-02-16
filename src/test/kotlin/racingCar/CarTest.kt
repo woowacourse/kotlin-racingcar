3b382570d704a7b6c2928050e0f6b42cfd9cc62a
m6z1 <sonmyungj1zz@gmail.com>
2025-02-17 00:42:33 +0900

    test: 자동차 이름 에러 테스트 추가
    - step1에서 객체 파일 제거하다가 같이 제거하여 다시 추가합니다.

--- src/test/kotlin/racingCar/CarTest.kt
+++ src/test/kotlin/racingCar/CarTest.kt
@@ -2,9 +2,31 @@ package racingCar
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     private val car = Car("뭉치")
+
+    @ParameterizedTest
+    @ValueSource(strings = [" ", "", "  "])
+    fun `자동차 이름이 공백일 때 에러를 발생시킨다`(carName: String) {
+        assertThrows<IllegalArgumentException> { Car(carName) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["123456", ""])
+    fun `자동차 이름의 길이가 0자 이거나 5자 초과할 때 에러를 발생시킨다`(carName: String) {
+        assertThrows<IllegalArgumentException> { Car(carName) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["뭉치", "코난", "철수"])
+    fun `자동차 이름이 조건을 충족할 때 에러를 발생시키지 않는다`(name: String) {
+        assertDoesNotThrow { Car(name) }
+    }
 
     @Test
     fun `자동차의 초기 위치는 0이다`() {
