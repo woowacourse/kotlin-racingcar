fb8fb9e01ad81e29cd0586a6b871669966fff0a3
yrsel <xxknu88@gmail.com>
2025-02-17 17:41:39 +0900

    refactor: 코드 구조 변경에 따른 테스트 코드 변경

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,5 +1,6 @@
 import domain.cars.Car
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -28,16 +29,17 @@ class CarTest {
 
         Assertions.assertThat(car.distance).isEqualTo(expected)
     }
-//
-//    @Test
-//    fun `숫자가 4 이상이면 전진하고 위치를 출력한다`() {
-//        val car = Car("포비")
-//        val expected = "포비 : -"
-//
-//        car.moveOrStop(TestMoveNumberGenerator())
-//
-//        Assertions.assertThat(car.toString()).isEqualTo(expected)
-//    }
+
+    @Test
+    fun `숫자가 4 이상이면 전진하고 위치를 출력한다`() {
+        val car = Car("포비")
+        val expected = "포비 : -"
+        val movableNumber = 4
+
+        car.moveOrStop(movableNumber)
+
+        Assertions.assertThat(car.toString()).isEqualTo(expected)
+    }
 //
 //    @Test
 //    fun `숫자가 4 미만이면 전진하지 않고 위치를 출력한다`() {
