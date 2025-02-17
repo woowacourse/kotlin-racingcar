db966e1db9c2f37b01618b384bc0e68d3be633bb
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-17 17:23:53 +0900

    feat: 우승자가 여러 명인 경우 테스트

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test
 
 class CarsTest {
     @Test
-    fun `우승자를 확인할 수 있다 `() {
+    fun `우승자가 한 명인 경우를 확인할 수 있다`() {
         val cars = Cars("포비,크론,디노")
         cars.moveAll(TestNumberGenerator(mutableListOf(1, 2, 5)))
         val expected = "디노"
+        assertEquals(expected, cars.getWinners())
+    }
+
+    @Test
+    fun `우승자가 여러 명인 경우를 확인할 수 있다`() {
+        val cars = Cars("포비,크론,디노")
+        cars.moveAll(TestNumberGenerator(mutableListOf(1, 4, 5)))
+        val expected = "크론, 디노"
         assertEquals(expected, cars.getWinners())
     }
 
