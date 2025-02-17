674b17d226375024c94a3cd00338a3a5332f3d3f
ijh1298 <imjh0417@gmail.com>
2025-02-17 17:54:10 +0900

    refactor: MoveConditionGenerator 삭제

--- src/main/kotlin/utils/MoveConditionGenerator.kt
+++
@@ -1,9 +0,0 @@
-package utils
-
-object MoveConditionGenerator {
-    fun getMoveConditionByNumber(number: Int): Boolean {
-        return number < MOVE_CONDITION
-    }
-
-    private const val MOVE_CONDITION = 4
-}
