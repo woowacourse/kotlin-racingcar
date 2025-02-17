9e49c81b34bcd42ac67a7d8464be25f51780613d
yrsel <xxknu88@gmail.com>
2025-02-17 17:06:49 +0900

    rename: toCarList() -> toCarsArgument()
    함수명에 자료형을 제거하고 하는 역할을 명확하게 인식할 수 있도록 변경하였습니다

--- src/main/kotlin/RacingCarConverter.kt
+++ src/main/kotlin/RacingCarConverter.kt
@@ -2,7 +2,7 @@ import Constants.CAR_NAME_DELIMITER
 import domain.cars.Car
 
 object RacingCarConverter {
-    fun toCarList(input: String): List<Car> {
+    fun toCarsArgument(input: String): List<Car> {
         return input.split(CAR_NAME_DELIMITER).map { Car(it.trim()) }
     }
 }
