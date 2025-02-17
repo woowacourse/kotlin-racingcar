01d8b7ff142d523a65be650a662e56cba3b42808
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-17 17:13:10 +0900

    refactor: 폴더 구조 변경

--- src/test/kotlin/racingCar/model/CarTest.kt
+++ src/test/kotlin/racingCar/model/CarTest.kt
@@ -1,4 +1,4 @@
-package model
+package racingCar.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
--- src/test/kotlin/racingCar/model/CarsTest.kt
+++ src/test/kotlin/racingCar/model/CarsTest.kt
@@ -1,4 +1,4 @@
-package model
+package racingCar.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
