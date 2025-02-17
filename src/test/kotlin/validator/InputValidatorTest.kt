5eb97fac944ee4fc5fe7ebd9c4897790fc764679
DongjooSEO <readyjoo24@gmail.com>
2025-02-17 17:31:12 +0900

    feat: 테스트 시 에러 메세지 검사 추가

--- src/test/kotlin/validator/InputValidatorTest.kt
+++ src/test/kotlin/validator/InputValidatorTest.kt
@@ -1,5 +1,6 @@
 package validator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -9,18 +10,32 @@ class InputValidatorTest {
     @Test
     fun `자동차 이름은 중복 될 수 없다`() {
         val names = listOf("tama", "tama")
-        assertThrows<IllegalArgumentException> {
-            InputValidator.validateDuplicatedName(names)
-        }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                InputValidator.validateDuplicatedName(names)
+            }
+        assertThat(exception.message).isEqualTo("자동차 이름은 중복될 수 없습니다.")
     }
 
     @ParameterizedTest
     @ValueSource(
-        strings = ["0", " ", "seven"],
+        strings = [" ", "seven", "-3"],
     )
     fun `시도 횟수는 정수 형태로 입력되어야 한다`(tryNumber: String) {
-        assertThrows<IllegalArgumentException> {
-            InputValidator.validateTryNumber(tryNumber)
-        }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                InputValidator.validateTryNumber(tryNumber)
+            }
+        assertThat(exception.message).isEqualTo("잘못 입력된 시도 횟수 -> $tryNumber : 시도 횟수는 숫자로 입력해주세요.")
+    }
+
+    @Test
+    fun `시도 횟수는 0보다 커야한다`() {
+        val tryNumber = "0"
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                InputValidator.validateTryNumber(tryNumber)
+            }
+        assertThat(exception.message).isEqualTo("잘못 입력된 시도 횟수 -> $tryNumber : 시도 횟수는 0보다 커야 합니다.")
     }
 }
