2e5b368e725ea6b18420d505db5c2e320d4a130a
cucumber99 <dlsguq1@gmail.com>
2025-02-17 17:40:19 +0900

    refactor: 자동차 출력 방식 변경
    toString을 이용해 Cars 객체가 직접 자동차 목록을 반환합니다.

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -23,8 +23,23 @@ class Cars(
         return winners
     }
 
+    override fun toString(): String {
+        return buildString {
+            parsedCars.forEach { car ->
+                append(car.name)
+                append(" : ")
+                append("-".repeat(car.position))
+                append("\n")
+            }
+        }
+    }
+
     private fun validateCars() {
-        require(carNames.distinct().size == carNames.size) { "자동차 이름 중 중복이 존재합니다." }
-        require(carNames.size > 1) { "자동차가 2대 미만 입력되었습니다." }
+        require(carNames.distinct().size == carNames.size) { "자동차 이름 중 중복이 존재합니다.: $carNames" }
+        require(carNames.size >= MINIMUM_CAR_COUNT) { "자동차가 2대 미만 입력되었습니다.: $carNames" }
+    }
+
+    companion object {
+        private const val MINIMUM_CAR_COUNT = 2
     }
 }
