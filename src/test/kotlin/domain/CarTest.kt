37311937eb79d168e190688a0305db02f5bf2356
ijh1298 <imjh0417@gmail.com>
2025-02-17 17:59:38 +0900

    test: CarTest 조건 판단 수정

--- src/test/kotlin/domain/CarTest.kt
+++ src/test/kotlin/domain/CarTest.kt
@@ -3,7 +3,6 @@ package domain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import utils.MoveConditionGenerator.getMoveConditionByNumber
 
 class CarTest {
     @ParameterizedTest
@@ -12,8 +11,9 @@ class CarTest {
     )
     fun `무작위 값이 4 이상인 경우, 자동차는 전진한다`(num: Int) {
         val car = Car("테스트", 0)
-        val condition = getMoveConditionByNumber(num)
-        car.moveOrStop(condition)
+        if (car.isMovable(num)) {
+            car.move()
+        }
         assertThat(car.position == 1).isTrue()
     }
 
@@ -23,8 +23,9 @@ class CarTest {
     )
     fun `무작위 값이 4 미만인 경우, 자동차는 전진하지 않는다`(num: Int) {
         val car = Car("테스트", 0)
-        val condition = getMoveConditionByNumber(num)
-        car.moveOrStop(condition)
+        if (car.isMovable(num)) {
+            car.move()
+        }
         assertThat(car.position == 0).isTrue()
     }
 }
