14e5fcd69943952491bab2557de09ee0097d0f76
HamBeomJoon <hbj0802@naver.com>
2025-02-17 17:35:08 +0900

    feat: 상수 줄바꿈 문자 추가

--- src/main/kotlin/racingcar/util/Messages.kt
+++ src/main/kotlin/racingcar/util/Messages.kt
@@ -3,7 +3,7 @@ package racingcar.util
 object Messages {
     const val MESSAGE_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
     const val MESSAGE_INPUT_RACE_COUNT = "시도할 횟수는 몇 회인가요?"
-    const val MESSAGE_OUTPUT_RACE_RESULT = "실행 결과"
+    const val MESSAGE_OUTPUT_RACE_RESULT = "\n실행 결과"
     const val MESSAGE_OUTPUT_WINNER = "최종 우승자: "
 
     const val MESSAGE_INVALID_CAR_NAME = "자동차 이름은 1글자 이상 5글자 이하여야 합니다."
