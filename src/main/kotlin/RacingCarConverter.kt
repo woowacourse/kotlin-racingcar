038f3259f86652884ac35478fc7632ea1f285cf5
yrsel <xxknu88@gmail.com>
2025-02-17 17:03:09 +0900

    rename: CarsArgumentConverter -> RacingCarConverter

--- src/main/kotlin/RacingCarConverter.kt
+++ src/main/kotlin/RacingCarConverter.kt
@@ -1,7 +1,7 @@
 import Constants.CAR_NAME_DELIMITER
 import domain.cars.Car
 
-object CarsArgumentConverter {
+object RacingCarConverter {
     fun toCarList(input: String): List<Car> {
         return input.split(CAR_NAME_DELIMITER).map { Car(it.trim()) }
     }
