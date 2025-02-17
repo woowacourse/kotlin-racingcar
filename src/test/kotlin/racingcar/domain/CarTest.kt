9ed9f8a90041e519dfa0b4d5ba76ff5d4779ec6a
박지민 <pjm5489@naver.com>
2025-02-17 16:15:46 +0900

    refactor: 전진횟수 테스트 코드 수정

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -9,7 +9,7 @@ class CarTest {
     @Test
     fun `랜덤 숫자가 4 이상이면 전진한다`() {
         car.moves(5)
-        val expected = listOf("-")
+        val expected = 1
         val result = car.position
 
         assertEquals(expected, result)
@@ -17,11 +17,12 @@ class CarTest {
 
     @Test
     fun `자동차에 저장된 전진 횟수를 계산한다`() {
-        val expected = 1
-        car.position.add("-")
-        car.position.add("")
-        val result = car.moveCount()
-
+        val expected = 2
+        val numbers = listOf(1, 2, 4, 5)
+        numbers.forEach { number ->
+            car.moves(number)
+        }
+        val result = car.position
         assertEquals(expected, result)
     }
 }
