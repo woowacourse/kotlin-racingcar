49cbfdee3ad396b9c8c7b125a53cc9de37931bee
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-17 15:29:28 +0900

    refactor: fix error test

--- src/test/kotlin/test/RacecourseTest.kt
+++ src/test/kotlin/test/RacecourseTest.kt
@@ -4,13 +4,14 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import racingcar.domain.Car
 import racingcar.domain.Racecourse
+import racingcar.domain.RoundManager
 
 class RacecourseTest {
     @Test
     fun `자동차 경주 게임의 우승자는 가장 멀리 간 사람이다`() {
         val cars: List<Car> = Car.createCars("a,b,c")
         cars.first().move(9)
-        val racecourse = Racecourse(cars, 0)
+        val racecourse = Racecourse(cars, RoundManager.from("0"))
         Assertions.assertThat(racecourse.winners).isEqualTo(listOf(cars.first()))
     }
 }
