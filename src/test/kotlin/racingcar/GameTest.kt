32b47c204f208f523dd9352526a9eecb8a9a32c8
chanho0908 <uni070@naver.com>
2025-02-17 22:03:04 +0900

    test : 랜덤 숫자 생성 클래스 인터페이스 분리로 인한 수정

--- src/test/kotlin/racingcar/GameTest.kt
+++ src/test/kotlin/racingcar/GameTest.kt
@@ -4,41 +4,60 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import racingcar.enums.MoveState
 import racingcar.model.Car
 import racingcar.model.Game
-import racingcar.model.RandomNumberFactory
+import racingcar.model.RoundResult
+import racingcar.model.random.RandomNumberGenerator
+import racingcar.model.random.RandomNumberGeneratorImpl
 
 class GameTest {
-    private lateinit var randomNumberFactory: FakeRandomNumberFactory
+    private lateinit var moveAbleRandomNumberGenerator: RandomNumberGenerator
+    private lateinit var unMoveAbleRandomNumberGenerator: RandomNumberGenerator
     private lateinit var cars: List<Car>
 
     @BeforeEach
     fun setUp() {
-        randomNumberFactory = FakeRandomNumberFactory()
+        moveAbleRandomNumberGenerator = RandomNumberGeneratorImpl(MOVEABLE_RANGE_MIN, MOVEABLE_RANGE_MAX)
+        unMoveAbleRandomNumberGenerator = RandomNumberGeneratorImpl(UNMOVEABLE_RANGE_MIN, UNMOVEABLE_RANGE_MAX)
         cars = listOf(Car("peto"), Car("por"))
     }
 
-    fun `getRoundResult - 게임 중간 라운드 결과를 출력한다`() {
-        val game = Game(cars, randomNumberFactory.movableRandomNumberFactory())
+    @Test
+    fun `getRoundResult - 랜덤 숫자가 4미만 이여서 이동할 수 없을 때 게임 중간 라운드 결과를 출력한다`() {
+        val game = Game(cars, unMoveAbleRandomNumberGenerator)
 
         game.playRound()
 
         val result = game.getRoundResult()
 
-        val expected = "peto : -\npor : -"
+        val expected = listOf<RoundResult>(RoundResult("peto", 0), RoundResult("por", 0))
 
         assertEquals(expected, result)
     }
 
     @Test
-    fun `getWinners - 게임 최종 우승자를 출력한다`() {
-        val game = Game(cars, randomNumberFactory.movableRandomNumberFactory())
+    fun `getRoundResult - 랜덤 숫자가 4이상 이여서 이동할 수 있을 때 게임 중간 라운드 결과를 출력한다`() {
+        val game = Game(cars, moveAbleRandomNumberGenerator)
 
         game.playRound()
 
+        val result = game.getRoundResult()
+
+        val expected = listOf<RoundResult>(RoundResult("peto", 1), RoundResult("por", 1))
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `getWinners - 최종 우승자는 1칸 이동한 peto다`() {
+        val cars: List<Car> = listOf(Car("Peto"), Car("Por"), Car("nana"))
+        cars.first().moveCarWhenMovable(MoveState.MOVABLE)
+
+        val game = Game(cars, moveAbleRandomNumberGenerator)
         val result = game.getWinners()
 
-        val expected = "peto, por"
+        val expected = listOf("Peto")
 
         assertEquals(expected, result)
     }
@@ -48,8 +67,16 @@ class GameTest {
         val duplicatedCars = listOf(Car("pobi"), Car("pobi"))
 
         Assertions.assertThatThrownBy {
-            Game(duplicatedCars, RandomNumberFactory { 0 })
+            Game(duplicatedCars, moveAbleRandomNumberGenerator)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("[ERROR] 자동차 이름이 중복됩니다.")
+    }
+
+    companion object {
+        const val UNMOVEABLE_RANGE_MIN = 0
+        const val UNMOVEABLE_RANGE_MAX = 3
+
+        const val MOVEABLE_RANGE_MIN = 4
+        const val MOVEABLE_RANGE_MAX = 9
     }
 }
