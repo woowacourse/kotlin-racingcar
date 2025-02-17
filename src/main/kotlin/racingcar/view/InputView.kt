4cda4de04ccc9d8bbaed9b52df1638e06cae0cbb
HamBeomJoon <hbj0802@naver.com>
2025-02-17 17:33:15 +0900

    refactor: 사용자 입력방법 수정

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -2,12 +2,8 @@ package racingcar.view
 
 import racingcar.util.Constants
 import racingcar.util.Messages
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 object InputView {
-    private val br = BufferedReader(InputStreamReader(System.`in`))
-
     fun validateCarName(input: List<String>) {
         require(input.all { it.isNotEmpty() && it.length <= Constants.CONDITION_CAR_MAX_LENGTH }) {
             IllegalArgumentException(Messages.MESSAGE_INVALID_CAR_NAME)
@@ -31,7 +27,7 @@ object InputView {
 
     fun readCars(): List<String> {
         println(Messages.MESSAGE_INPUT_CAR_NAMES)
-        val carsInput = br.readLine().split(Constants.DELIMITER_CAR_INPUT).map { it.trim() }
+        val carsInput = readln().split(Constants.DELIMITER_CAR_INPUT).map { it.trim() }
         validateCarName(carsInput)
 
         return carsInput
@@ -39,10 +35,6 @@ object InputView {
 
     fun readCount(): Int {
         println(Messages.MESSAGE_INPUT_RACE_COUNT)
-        val count = br.readLine()
-        val validCount = validateCount(count)
-        println()
-
-        return validCount
+        return validateCount(readln())
     }
 }
