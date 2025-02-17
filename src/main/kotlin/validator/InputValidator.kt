89d3f80620dfdbee114908289abff5184fcfaa0d
ijh1298 <imjh0417@gmail.com>
2025-02-17 17:22:10 +0900

    refactor: 매직 넘버 상수화

--- src/main/kotlin/validator/InputValidator.kt
+++ src/main/kotlin/validator/InputValidator.kt
@@ -2,13 +2,16 @@ package validator
 
 object InputValidator {
     fun validateCarName(inputCarName: String) {
-        require(inputCarName.length <= 5) { "각 자동차 이름은 5자를 초과할 수 없습니다." }
+        require(inputCarName.length <= NAME_LENGTH_LIMIT) { "각 자동차 이름은 5자를 초과할 수 없습니다." }
         require(inputCarName.isNotBlank()) { "각 자동차 이름은 공백이 불가합니다." }
     }
 
     fun validateTryNumber(inputTryNumber: String): Int {
         require(inputTryNumber.all { it.isDigit() }) { "시도 횟수는 숫자로 입력해주세요." }
-        require(inputTryNumber.toInt() > 0) { "시도 횟수는 0보다 커야 합니다." }
+        require(inputTryNumber.toInt() > MIN_TRY_NUMBER) { "시도 횟수는 0보다 커야 합니다." }
         return inputTryNumber.toInt()
     }
+
+    private const val NAME_LENGTH_LIMIT = 5
+    private const val MIN_TRY_NUMBER = 0
 }
