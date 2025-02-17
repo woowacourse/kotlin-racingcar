2d0352210ed57691493e5681c29eaff4c1fd23dc
moondev03 <moondev03@gmail.com>
2025-02-17 15:31:34 +0900

    refactor(RacingController): RacingGame 객체를 외부에서 생성하도록 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,7 +1,12 @@
 package racingcar
 
 import racingcar.controller.RacingController
+import racingcar.model.RacingGame
+import racingcar.model.RandomNumberGenerator
 
 fun main() {
-    RacingController().run()
+    val randomNumber = RandomNumberGenerator()
+    val racingGame = RacingGame(randomNumber)
+
+    RacingController(racingGame).run()
 }
