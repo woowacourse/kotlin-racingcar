b1ec09c60a0c7f9d7ecdfce31c5615cc13006eba
junseo511 <prime54@g.skku.edu>
2025-02-17 16:14:02 +0900

    refactor(Car): 자동차 객체 생성자의 접근제한자 변경

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,7 +1,11 @@
-class Car private constructor(
+class Car(
     val name: String,
     position: Int = INITIAL_CAR_POSITION,
 ) {
+    init {
+        validateCarName(name)
+    }
+
     var position: Int = position
         private set
 
@@ -9,26 +13,18 @@ class Car private constructor(
         if (isMoved) position++
     }
 
+    private fun validateCarName(carName: String) {
+        require(carName.isNotBlank()) { "[ERROR] 자동차 이름은 공백일 수 없습니다. (입력한 이름: $carName)" }
+        require(carName.length in MIN_CAR_NAME_LENGTH..MAX_CAR_NAME_LENGTH) {
+            "[ERROR] 자동차 이름은 ${MIN_CAR_NAME_LENGTH}자 이상 ${MAX_CAR_NAME_LENGTH}자 이하여야 합니다. (입력한 이름: $carName)"
+        }
+    }
+
     companion object {
         private const val INITIAL_CAR_POSITION = 0
         private const val MIN_CAR_NAME_LENGTH = 1
         private const val MAX_CAR_NAME_LENGTH = 5
         private const val MIN_RANDOM_AVAILABILITY_CONDITION = 4
-
-        operator fun invoke(
-            name: String,
-            position: Int = INITIAL_CAR_POSITION,
-        ): Car {
-            validateCarName(name)
-            return Car(name, position)
-        }
-
-        private fun validateCarName(carName: String) {
-            require(carName.isNotBlank()) { "[ERROR] 자동차 이름은 공백일 수 없습니다. (입력한 이름: $carName)" }
-            require(carName.length in MIN_CAR_NAME_LENGTH..MAX_CAR_NAME_LENGTH) {
-                "[ERROR] 자동차 이름은 ${MIN_CAR_NAME_LENGTH}자 이상 ${MAX_CAR_NAME_LENGTH}자 이하여야 합니다. (입력한 이름: $carName)"
-            }
-        }
 
         fun isCarAbleToMove(randomNumber: Int): Boolean = randomNumber >= MIN_RANDOM_AVAILABILITY_CONDITION
 
