15b94542875b2ec2e3495d04c1df593a2c95f287
ijh1298 <imjh0417@gmail.com>
2025-02-17 17:55:11 +0900

    refactor: Car 클래스 내 move 메소드 변경, isMovable 메소드 추가

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -7,13 +7,16 @@ class Car(
     var position: Int = position
         private set
 
-    fun moveOrStop(condition: Boolean) {
-        if (condition) {
-            ++position
-        }
+    fun isMovable(number: Int): Boolean {
+        return number < MOVE_CONDITION
+    }
+
+    fun move() {
+        ++position
     }
 
     companion object {
         const val INIT_POSITION = 0
+        const val MOVE_CONDITION = 4
     }
 }
