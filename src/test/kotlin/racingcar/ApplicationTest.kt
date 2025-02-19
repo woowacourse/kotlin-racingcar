7451b059e5e46a5ef226091d459fa23dbc97becf
지원 <cheerfulboys@naver.com>
2025-02-19 09:57:48 +0900

    refactor: InputView 한 번만 생성하도록 수정

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -7,14 +7,16 @@ import org.junit.jupiter.params.provider.ValueSource
 import racingcar.view.InputView
 
 class ApplicationTest {
+    val inputView = InputView()
+
     @Test
     fun `자동차 이름은 5자를 초과할 수 없다`() {
-        assertThat(InputView().checkCarNameValid("5자 이상의 자동차 이름")).isFalse()
+        assertThat(inputView.checkCarNameValid("5자 이상의 자동차 이름")).isFalse()
     }
 
     @ValueSource(strings = ["", "0", "-1", "1.1", "asd", "${Int.MAX_VALUE + 1}"])
     @ParameterizedTest
     fun `시도횟수는 1 이상의 숫자여야 한다`(round: String) {
-        assertThat(InputView().checkRoundValid(round)).isFalse()
+        assertThat(inputView.checkRoundValid(round)).isFalse()
     }
 }
