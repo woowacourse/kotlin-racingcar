b4184fa8aeba2b220e6ec99449958135ab6fd8e6
박지민 <pjm5489@naver.com>
2025-03-01 17:36:50 +0900

    refactor: controller와 domain 역할 분리

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,38 +1,32 @@
 package racingcar.controller
 
+import racingcar.domain.Car
+import racingcar.domain.CarNameValidator
 import racingcar.domain.Race
-import racingcar.utils.InputValidator
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
-class RacingGameController {
-    private val inputView = InputView()
-    private val outputView = OutputView()
-    private val inputValidator = InputValidator()
-
+class RacingGameController(private val inputView: InputView, private val outputView: OutputView) {
     fun run() {
-        val rawCarNames = inputView.insertCarNames()
-        inputValidator.validateCarName(rawCarNames)
-        val rawTryCount = inputView.insertTryCount()
-        inputValidator.validateTryCount(rawTryCount)
-        val gameResult = playing(rawCarNames, rawTryCount)
-        printGameResult(gameResult)
+        val carNames = inputView.insertCarNames()
+        CarNameValidator().validateCarName(carNames)
+        val cars = carNames.map { Car(it) }
+
+        val tryCount = inputView.insertTryCount() ?: throw IllegalArgumentException("[ERROR] 시도횟수를 다시 입력해주세요.")
+
+        outputView.printResultMessage()
+        printGameResult(Race(cars), tryCount)
     }
 
-    private fun playing(
-        rawCarNames: String,
-        rawTryCount: String,
-    ): Race {
-        val race = Race(rawCarNames, rawTryCount)
-        return race
-    }
-
-    private fun printGameResult(gameResult: Race) {
-        val roundPositions = gameResult.getPositions()
-        outputView.printRoundResult(
-            gameResult.cars.map { it.carName },
-            roundPositions,
-        )
-        outputView.printWinners(gameResult.getWinners())
+    private fun printGameResult(
+        race: Race,
+        tryCount: Int,
+    ) {
+        val carNames = race.cars.map { it.carName }
+        repeat(tryCount) {
+            val roundPositions = race.getPositions()
+            outputView.printRoundResult(carNames, roundPositions)
+        }
+        outputView.printWinners(race.getWinners())
     }
 }
