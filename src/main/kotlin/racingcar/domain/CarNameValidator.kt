a3fcb85280d963ed03204a924acefad44cd5e7fc
박지민 <pjm5489@naver.com>
2025-03-01 17:38:30 +0900

    refactor:시도횟수 예외처리 로직 제거

--- src/main/kotlin/racingcar/domain/CarNameValidator.kt
+++ src/main/kotlin/racingcar/domain/CarNameValidator.kt
@@ -1,21 +1,12 @@
-package racingcar.utils
+package racingcar.domain
 
-class InputValidator {
-    fun validateCarName(rawCarName: String) {
-        val carNames = rawCarName.split(COMMA)
+class CarNameValidator {
+    fun validateCarName(carNames: List<String>) {
         if (isDuplicateCarNames(carNames) ||
             isEmptyCarNames(carNames) ||
             carNameLengthCheck(carNames)
         ) {
             throw IllegalArgumentException(CARNAME_ERROR_MESSAGE)
-        }
-    }
-
-    fun validateTryCount(rawTryCount: String) {
-        if (isTryCountNumeric(rawTryCount) || isEmptyTryCount(rawTryCount)) {
-            throw IllegalArgumentException(
-                TRYCOUNT_ERROR_MESSAGE,
-            )
         }
     }
 
@@ -37,21 +28,8 @@ class InputValidator {
         return false
     }
 
-    private fun isTryCountNumeric(rawTryCount: String): Boolean {
-        if (rawTryCount.toIntOrNull() == null) return true
-        if (rawTryCount.toInt() <= 0) return true
-
-        return false
-    }
-
-    private fun isEmptyTryCount(rawTryCount: String): Boolean {
-        return rawTryCount.isBlank()
-    }
-
     companion object {
-        private const val COMMA = ","
         private const val CARNAME_ERROR_MESSAGE = "[ERROR] 자동차 이름을 다시 입력해주세요."
-        private const val TRYCOUNT_ERROR_MESSAGE = "[ERROR] 시도횟수를 다시 입력해주세요."
         private const val CARNAME_MAX_LENGTH = 5
     }
 }
