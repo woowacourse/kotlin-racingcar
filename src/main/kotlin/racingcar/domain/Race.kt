5e9c4ef1bdd83349c63406ad87d3c2fe3383fbb2
박지민 <pjm5489@naver.com>
2025-03-01 17:34:56 +0900

    refactor: 한 라운드의 결과만 반환하게 변경

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -1,18 +1,13 @@
 package racingcar.domain
 
-class Race(rawCarNames: String, rawTryCount: String) {
-    val tryCount = rawTryCount.toInt()
-    val cars = getCars(rawCarNames)
-
-    fun getPositions(): List<List<Int>> {
+class Race(val cars: List<Car>) {
+    fun getPositions(): List<Int> {
         val generateRandomNumber = GenerateRandomNumber()
-        val roundResults = mutableListOf<List<Int>>()
-        repeat(tryCount) {
-            cars.forEach { car ->
-                val randomNumber = generateRandomNumber.randomNumber()
-                car.moves(randomNumber)
-            }
-            roundResults.add(cars.map { it.position })
+        val roundResults = mutableListOf<Int>()
+        cars.forEach { car ->
+            val randomNumber = generateRandomNumber.randomNumber()
+            car.moves(randomNumber)
+            roundResults.add(car.position)
         }
         return roundResults
     }
@@ -24,13 +19,5 @@ class Race(rawCarNames: String, rawTryCount: String) {
                 car.position == maxNumber
             }.map { it.carName }
         return winners
-    }
-
-    private fun getCars(rawCarNames: String): List<Car> {
-        return rawCarNames.split(COMMA).map { Car(it.trim()) }
-    }
-
-    companion object {
-        private const val COMMA = ","
     }
 }
